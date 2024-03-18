244e9b3247c78c785c82e9129059f14b24aaeeca
Jim Su <jimsu@protonmail.com>
2024-03-17 22:31:23 -0400

    Add xterm terminal

--- frontend/src/components/Terminal.tsx
+++ frontend/src/components/Terminal.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Terminal as XtermTerminal } from "@xterm/xterm";
+import { AttachAddon } from "xterm-addon-attach";
+import { FitAddon } from "xterm-addon-fit";
+import "@xterm/xterm/css/xterm.css";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function Terminal(): JSX.Element {
+  const terminalRef = useRef<HTMLDivElement>(null);
   const terminalOutput = `> chatbot-ui@2.0.0 prepare
 > husky install
 
@@ -18,12 +23,45 @@ npm notice
 npm notice New minor version of npm available! 10.7.3 -> 10.9.0
 ...`;
 
+  useEffect(() => {
+    const terminal = new XtermTerminal({
+      fontFamily: "Menlo, Monaco, 'Courier New', monospace",
+      fontSize: 14,
+    });
+
+    const fitAddon = new FitAddon();
+    terminal.loadAddon(fitAddon);
+
+    terminal.open(terminalRef.current as HTMLDivElement);
+
+    // Without this timeout, `fitAddon.fit()` throws the error
+    // "this._renderer.value is undefined"
+    setTimeout(() => {
+      fitAddon.fit();
+    }, 1);
+
+    if (!process.env.REACT_APP_TERMINAL_WS_URL) {
+      throw new Error(
+        "The environment variable REACT_APP_TERMINAL_WS_URL is not set. Please set it to the WebSocket URL of the terminal server.",
+      );
+    }
+    const attachAddon = new AttachAddon(
+      new WebSocket(process.env.REACT_APP_TERMINAL_WS_URL as string),
+    );
+    terminal.loadAddon(attachAddon);
+
+    return () => {
+      terminal.dispose();
+    };
+  }, []);
+
   return (
-    <div className="terminal">
-      <SyntaxHighlighter language="bash" style={atomDark}>
-        {terminalOutput}
-      </SyntaxHighlighter>
-    </div>
+    // <div className="terminal">
+    //   <SyntaxHighlighter language="bash" style={atomDark}>
+    //     {terminalOutput}
+    //   </SyntaxHighlighter>
+    // </div>
+    <div ref={terminalRef} style={{ width: "100%", height: "100%" }} />
   );
 }
 
