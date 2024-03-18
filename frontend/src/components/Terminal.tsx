7d39d9d4a9afeb3de3720e40e99a84401bc795ba
Jim Su <jimsu@protonmail.com>
2024-03-17 22:32:34 -0400

    Remove unused code blocks

--- frontend/src/components/Terminal.tsx
+++ frontend/src/components/Terminal.tsx
@@ -3,25 +3,9 @@ import { Terminal as XtermTerminal } from "@xterm/xterm";
 import { AttachAddon } from "xterm-addon-attach";
 import { FitAddon } from "xterm-addon-fit";
 import "@xterm/xterm/css/xterm.css";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function Terminal(): JSX.Element {
   const terminalRef = useRef<HTMLDivElement>(null);
-  const terminalOutput = `> chatbot-ui@2.0.0 prepare
-> husky install
-
-husky - Git hooks installed
-
-added 1455 packages, and audited 1456 packages in 1m
-
-295 packages are looking for funding
-  run \`npm fund\` for details
-  
-found 0 vulnerabilities
-npm notice
-npm notice New minor version of npm available! 10.7.3 -> 10.9.0
-...`;
 
   useEffect(() => {
     const terminal = new XtermTerminal({
@@ -55,14 +39,7 @@ npm notice New minor version of npm available! 10.7.3 -> 10.9.0
     };
   }, []);
 
-  return (
-    // <div className="terminal">
-    //   <SyntaxHighlighter language="bash" style={atomDark}>
-    //     {terminalOutput}
-    //   </SyntaxHighlighter>
-    // </div>
-    <div ref={terminalRef} style={{ width: "100%", height: "100%" }} />
-  );
+  return <div ref={terminalRef} style={{ width: "100%", height: "100%" }} />;
 }
 
 export default Terminal;
