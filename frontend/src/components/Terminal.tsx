cf97b66ff90f1521476bba1b4ca80187d8a47039
Jim Su <jimsu@protonmail.com>
2024-03-17 23:33:48 -0400

    Frontend: Implement real terminal with xterm.js (#39)
    * Add xterm terminal
    
    * Remove unused code blocks
    
    * Update README.md with terminal documentation
    
    * Update frontend/README.md
    
    Co-authored-by: Graham Neubig <neubig@gmail.com>
    
    ---------
    
    Co-authored-by: Graham Neubig <neubig@gmail.com>

--- frontend/src/components/Terminal.tsx
+++ frontend/src/components/Terminal.tsx
@@ -1,30 +1,45 @@
-import React from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import React, { useEffect, useRef } from "react";
+import { Terminal as XtermTerminal } from "@xterm/xterm";
+import { AttachAddon } from "xterm-addon-attach";
+import { FitAddon } from "xterm-addon-fit";
+import "@xterm/xterm/css/xterm.css";
 
 function Terminal(): JSX.Element {
-  const terminalOutput = `> chatbot-ui@2.0.0 prepare
-> husky install
+  const terminalRef = useRef<HTMLDivElement>(null);
 
-husky - Git hooks installed
+  useEffect(() => {
+    const terminal = new XtermTerminal({
+      fontFamily: "Menlo, Monaco, 'Courier New', monospace",
+      fontSize: 14,
+    });
 
-added 1455 packages, and audited 1456 packages in 1m
+    const fitAddon = new FitAddon();
+    terminal.loadAddon(fitAddon);
 
-295 packages are looking for funding
-  run \`npm fund\` for details
-  
-found 0 vulnerabilities
-npm notice
-npm notice New minor version of npm available! 10.7.3 -> 10.9.0
-...`;
+    terminal.open(terminalRef.current as HTMLDivElement);
 
-  return (
-    <div className="terminal">
-      <SyntaxHighlighter language="bash" style={atomDark}>
-        {terminalOutput}
-      </SyntaxHighlighter>
-    </div>
-  );
+    // Without this timeout, `fitAddon.fit()` throws the error
+    // "this._renderer.value is undefined"
+    setTimeout(() => {
+      fitAddon.fit();
+    }, 1);
+
+    if (!process.env.REACT_APP_TERMINAL_WS_URL) {
+      throw new Error(
+        "The environment variable REACT_APP_TERMINAL_WS_URL is not set. Please set it to the WebSocket URL of the terminal server.",
+      );
+    }
+    const attachAddon = new AttachAddon(
+      new WebSocket(process.env.REACT_APP_TERMINAL_WS_URL as string),
+    );
+    terminal.loadAddon(attachAddon);
+
+    return () => {
+      terminal.dispose();
+    };
+  }, []);
+
+  return <div ref={terminalRef} style={{ width: "100%", height: "100%" }} />;
 }
 
 export default Terminal;
