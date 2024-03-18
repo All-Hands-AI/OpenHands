20721b6689111cc70e178d137ee3d01193b99908
z-zeechung <zhouzeechung@qq.com>
2024-03-18 12:23:01 +0800

    -

--- llm_api/multillm.py
+++ llm_api/multillm.py
@@ -2,19 +2,20 @@ from typing import List, Optional
 import os
 from llm_api.llm_api import llm_api
 from langchain.llms.base import LLM
+from litellm.utils import ModelResponse
 
 
 class ChatMultiLLM(LLM):
 
-    def __init__(self, temperature=None, top_p=None, presence_penalty=None, frequency_penalty=None, n=None, max_tokens=None, stream=None):
+    def __init__(self, temperature=None, top_p=None, presence_penalty=None, frequency_penalty=None, max_tokens=None):
         super().__init__()
         self.__temperature = temperature
         self.__top_p = top_p
         self.__presence_penalty = presence_penalty
         self.__frequency_penalty = frequency_penalty
-        self.__n = n
+        #self.__n = n
         self.__max_tokens = max_tokens
-        self.__stream = stream
+        #self.__stream = stream
         
     @property
     def _llm_type(self) -> str:
@@ -22,17 +23,20 @@ class ChatMultiLLM(LLM):
 
     def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
 
-        return llm_api.request_model(
+        response = llm_api.request_model(
             messages=[{"role": "user", "content": prompt}],
             temperature=self.__temperature,
             top_p=self.__top_p,
-            n=self.__n,
-            stream=self.__stream,
+            #n=self.__n,
+            #stream=self.__stream,
             stop=stop,
             max_tokens=self.__max_tokens,
             presence_penalty=self.__presence_penalty,
             frequency_penalty=self.__frequency_penalty,
         )
+
+        response:ModelResponse = response[0]
+        return response.choices[0].message.get("content", "default")
 
 
 '''
