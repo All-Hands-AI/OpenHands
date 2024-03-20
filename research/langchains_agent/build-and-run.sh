23308f6b430ee371a03f208919c9b3cd4a1e13fa
Xingyao Wang <xingyao6@illinois.edu>
2024-03-20 11:00:27 -0500

    get langchains_agent working after abstraction

--- research/langchains_agent/build-and-run.sh
+++ research/langchains_agent/build-and-run.sh
@@ -4,6 +4,16 @@ set -eo pipefail
 rm -rf `pwd`/workspace
 mkdir -p `pwd`/workspace
 
+pushd research/langchains_agent
 docker build -t control-loop .
-docker run -e DEBUG=$DEBUG -e OPENAI_API_KEY=$OPENAI_API_KEY -v `pwd`/workspace:/workspace control-loop python /app/main.py -d /workspace -t "${1}"
+popd
+docker run \
+    -e DEBUG=$DEBUG \
+    -e OPENAI_API_KEY=$OPENAI_API_KEY \
+    -u `id -u`:`id -g` \
+    -v `pwd`/workspace:/workspace \
+    -v `pwd`:/app:ro \
+    -e PYTHONPATH=/app \
+    control-loop \
+    python /app/opendevin/main.py -d /workspace -t "${1}" 
 
