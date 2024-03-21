f8f53b00bbbf6a413dfce8e69ca7a0c2a8ee1641
Jim Su <jimsu@protonmail.com>
2024-03-21 18:44:36 -0400

    Set up Redux, extract component state into Redux store, and break WebSocket out of Terminal component only

--- frontend/src/components/Browser.tsx
+++ frontend/src/components/Browser.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Browser.css";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 type UrlBarProps = {
   url: string;
@@ -17,12 +19,12 @@ function Screenshot({ src }: ScreenshotProps): JSX.Element {
   return <img className="screenshot" src={src} alt="screenshot" />;
 }
 
-type BrowserProps = {
-  url: string;
-  screenshotSrc: string;
-};
+function Browser(): JSX.Element {
+  const url = useSelector((state: RootState) => state.browser.url);
+  const screenshotSrc = useSelector(
+    (state: RootState) => state.browser.screenshotSrc,
+  );
 
-function Browser({ url, screenshotSrc }: BrowserProps): JSX.Element {
   return (
     <div className="browser">
       <UrlBar url={url} />
