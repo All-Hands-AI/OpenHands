f974b8da166929dcfb975c88d56688a694c9c290
Xingyao Wang <xingyao6@illinois.edu>
2024-03-23 10:36:50 -0500

    add agent actions

--- opendevin/action/agent.py
+++ opendevin/action/agent.py
@@ -6,8 +6,22 @@ from ..controller import AgentController
 
 @dataclass
 class AgentRecallAction(Action):
-    pass
+    query: str
 
     def run(self, controller: AgentController) -> str:
-        return controller.agent.search_memory(self.args['query'])
+        return controller.agent.search_memory(self.query)
 
+
+@dataclass
+class AgentThinkAction(Action):
+    thought: str
+
+    def run(self, controller: AgentController) -> str:
+        return self.thought
+
+
+@dataclass
+class AgentFinishAction(Action):
+
+    def run(self, controller: AgentController) -> str:
+        return "finish"
