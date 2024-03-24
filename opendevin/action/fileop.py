4f22b3dba5baf7d36d807cafade56a0ad7cacce1
Xingyao Wang <xingyao6@illinois.edu>
2024-03-24 00:40:25 -0500

    make file read/write action compatible w/ docker directory

--- opendevin/action/fileop.py
+++ opendevin/action/fileop.py
@@ -7,17 +7,22 @@ from .base import ExecutableAction
 @dataclass
 class FileReadAction(ExecutableAction):
     path: str
+    workspace_dir: str = ""  # TODO: maybe handle this in a more elegant way
 
     def run(self, *args, **kwargs) -> str:
-        with open(self.path, 'r') as file:
+        # remove the leading /workspace/ from the path (inside container)
+        path = os.path.join(self.workspace_dir, self.path.lstrip("/workspace/"))
+        with open(path, 'r') as file:
             return file.read()
 
 @dataclass
 class FileWriteAction(ExecutableAction):
     path: str
     contents: str
+    workspace_dir: str = ""
 
     def run(self, *args, **kwargs) -> str:
-        with open(self.path, 'w') as file:
+        path = os.path.join(self.workspace_dir, self.path.lstrip("/workspace/"))
+        with open(path, 'w') as file:
             file.write(self.contents)
         return ""
--- opendevin/controller/__init__.py
+++ opendevin/controller/__init__.py
@@ -4,6 +4,8 @@ from opendevin.state import State
 from opendevin.agent import Agent
 from opendevin.action import (
     Action,
+    FileReadAction,
+    FileWriteAction,
     AgentFinishAction,
 )
 from opendevin.observation import (
@@ -27,15 +29,16 @@ class AgentController:
     ):
         self.agent = agent
         self.max_iterations = max_iterations
+        self.workdir = workdir
         self.command_manager = CommandManager(workdir)
         self.state_updated_info: List[Action | Observation] = []
 
     def get_current_state(self) -> State:
+        # update observations & actions
         state = State(
             background_commands_obs=self.command_manager.get_background_obs(),
+            updated_info=self.state_updated_info,
         )
-        # update observations & actions
-        state.updated_info = self.state_updated_info
         self.state_updated_info = []
         return state
 
@@ -46,14 +49,24 @@ class AgentController:
             state: State = self.get_current_state()
             action: Action = self.agent.step(state)
             self.state_updated_info.append(action)
-
+            print("ACTION", action, flush=True)
+            
             if isinstance(action, AgentFinishAction):
                 break
+            if isinstance(action, (FileReadAction, FileWriteAction)):
+                action_cls = action.__class__
+                _kwargs = action.__dict__
+                _kwargs["workspace_dir"] = self.workdir
+                action = action_cls(**_kwargs)
+                print(action, flush=True)
             print("---", flush=True)
 
-            if action.runnable:
+
+            if action.executable:
                 observation: Observation = action.run(self)
                 self.state_updated_info.append(observation)
                 print(observation, flush=True)
-            
+            else:
+                print("ACTION NOT EXECUTABLE", flush=True)
+
             print("==============", flush=True)
