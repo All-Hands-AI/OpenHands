fb10f2d4cfdb36ba113fb49a5f30d13310ec06a2
Xingyao Wang <xingyao6@illinois.edu>
2024-03-24 09:55:55 -0500

    fix instruction setting

--- opendevin/agent.py
+++ opendevin/agent.py
@@ -21,6 +21,7 @@ class Agent(ABC):
 
     def __init__(self, model_name: str):
         self.model_name = model_name
+        self.instruction: str = ""  # need to be set before step
         self._complete = False
 
     @property
