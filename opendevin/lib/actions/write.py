57fb107d95510c40ea0f7ec91444c60f41b06c94
Robert Brennan <accounts@rbren.io>
2024-03-24 19:02:46 -0400

    Update write.py

--- opendevin/lib/actions/write.py
+++ opendevin/lib/actions/write.py
@@ -1,8 +1,8 @@
 from .util import resolve_path
 
-def write(base_path, path, contents):
+def write(base_path, file_path, contents):
     file_path = resolve_path(base_path, file_path)
-    with open(path, 'w') as file:
+    with open(file_path, 'w') as file:
         file.write(contents)
     return ""
 
