d4eed4ce8e05c9ed12718284a6f02596333ad859
Robert Brennan <contact@rbren.io>
2024-03-25 17:42:30 -0400

    add status code

--- opendevin/action/browse.py
+++ opendevin/action/browse.py
@@ -14,6 +14,7 @@ class BrowseURLAction(ExecutableAction):
             response = requests.get(self.url)
             return BrowserOutputObservation(
                 content=response.text,
+                status_code=response.status_code,
                 url=self.url
             )
         except requests.exceptions.RequestException as e:
