bf3d3a0a492ae09eb8dafa110d3b2635b2bff3fd
ifuryst <ifuryst@gmail.com>
2024-03-26 22:02:41 +0800

    feat: add message related schema to FE and BE.

---
+++ frontend/src/types/Message.tsx
@@ -0,0 +1,24 @@
+export interface ActionMessage {
+  // The action to be taken
+  action: string;
+
+  // The arguments for the action
+  args: Record<string, string>;
+
+  // A friendly message that can be put in the chat log
+  message: string;
+}
+
+export interface ObservationMessage {
+  // The type of observation
+  observation: string;
+
+  // The observed data
+  content: string;
+
+  // Additional structured data
+  extras: Record<string, string>;
+
+  // A friendly message that can be put in the chat log
+  message: string;
+}
