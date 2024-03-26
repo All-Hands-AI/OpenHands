d3ac882ed15cbd60451178339e51934ef9e67ecc
Xingyao Wang <xingyao6@illinois.edu>
2024-03-26 02:10:38 -0500

    fix ruff

--- agenthub/codeact_agent/__init__.py
+++ agenthub/codeact_agent/__init__.py
@@ -2,8 +2,6 @@ import os
 import re
 from typing import List, Mapping
 
-from termcolor import colored
-
 from opendevin.agent import Agent
 from opendevin.state import State
 from opendevin.action import (
