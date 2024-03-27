32ba8dd101eaf5f033024c44931c110edbb2a19f
Mohamed A. Fouad <111041768+moresearch@users.noreply.github.com>
2024-03-27 10:52:48 -0400

    Update download_test_data.py
    I think princeton-nlp/SWE-bench_Lite_oracle is more suitable for MVP.
    
    Source: https://www.swebench.com/lite.html

--- evaluation/SWE-bench/scripts/download_test_data.py
+++ evaluation/SWE-bench/scripts/download_test_data.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 import pandas as pd
 
-dataset = load_dataset("princeton-nlp/SWE-bench")
+dataset = load_dataset("princeton-nlp/SWE-bench_Lite_oracle")
 test = dataset["test"].to_pandas()
 test.to_json("data/processed/swe-bench-test.json", orient="records")
