77d54e90002b304621ecb7231c126029e0cdb523
Robert Brennan <contact@rbren.io>
2024-03-26 20:23:33 -0400

    support different embedding models

--- agenthub/langchains_agent/utils/memory.py
+++ agenthub/langchains_agent/utils/memory.py
@@ -1,18 +1,35 @@
-from . import json
+import os
 
 import chromadb
-
 from llama_index.core import Document
 from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.core import VectorStoreIndex
 from llama_index.vector_stores.chroma import ChromaVectorStore
+
+from . import json
+
+embedding_model = os.getenv("LLM_EMBEDDING_MODEL", "openai")
+
+if embedding_model == "llama2":
+    from llama_index.embeddings.ollama import OllamaEmbedding
+    embed_model = OllamaEmbedding(
+        model_name=embedding_model,
+        base_url=os.getenv("LLM_EMBEDDING_MODEL_BASE_URL", "http://localhost:8000"),
+        ollama_additional_kwargs={"mirostat": 0},
+    )
+else:
+    from llama_index.embeddings.openai import OpenAIEmbedding
+    embed_model = OpenAIEmbedding(
+        base_url=os.getenv("LLM_EMBEDDING_MODEL_BASE_URL"),
+    )
+
 
 class LongTermMemory:
     def __init__(self):
         db = chromadb.Client()
         self.collection = db.get_or_create_collection(name="memories")
         vector_store = ChromaVectorStore(chroma_collection=self.collection)
-        self.index = VectorStoreIndex.from_vector_store(vector_store)
+        self.index = VectorStoreIndex.from_vector_store(vector_store, embed_model=embed_model)
         self.thought_idx = 0
 
     def add_event(self, event):
