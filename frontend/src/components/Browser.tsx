3c08a6b2f67125df286d87472a4afd45c0a2475f
808vita <97225946+808vita@users.noreply.github.com>
2024-03-28 14:40:51 +0530

    Clean up comments from Browser.tsx
    Clean up commented code from Browser.tsx

--- frontend/src/components/Browser.tsx
+++ frontend/src/components/Browser.tsx
@@ -3,42 +3,9 @@ import "./Browser.css";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 
-// type UrlBarProps = {
-//   url: string;
-// };
-
-// function UrlBar({ url }: UrlBarProps): JSX.Element {
-//   return (
-//     <div className="mac-url-bar">
-//       <div className="left-icons">
-//         <div className="icon icon-red" />
-//         <div className="icon icon-yellow" />
-//         <div className="icon icon-green" />
-//       </div>
-//       <div className="url">{url}</div>
-//     </div>
-//   );
-// }
-
-// type ScreenshotProps = {
-//   src: string;
-// };
-
-// function Screenshot({ src }: ScreenshotProps): JSX.Element {
-//   return <img className="screenshot" src={src} alt="screenshot" />;
-// }
-
 function Browser(): JSX.Element {
   const url = useSelector((state: RootState) => state.browser.url);
-  // const screenshotSrc = useSelector(
-  //   (state: RootState) => state.browser.screenshotSrc,
-  // );
-
   return (
-    // <div className="browser">
-    //   <UrlBar url={url} />
-    //   <Screenshot src={screenshotSrc} />
-    // </div>
     <div
       className="mockup-browser"
       style={{
@@ -52,7 +19,6 @@ function Browser(): JSX.Element {
       <div className="mockup-browser-toolbar">
         <div className="input">{url}</div>
       </div>
-      {/* <div className="flex justify-center px-4 py-16 bg-base-100 " >Hello World!</div> */}
     </div>
   );
 }
