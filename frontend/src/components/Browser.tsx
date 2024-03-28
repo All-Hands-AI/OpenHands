4bcc4fb56723b7796cb1cd77ead08e991986ce36
808vita <97225946+808vita@users.noreply.github.com>
2024-03-28 15:06:35 +0530

    removed inline style-  Browser.tsx
    Removed inline styles from the div in Browser.tsx ;  moved them to Browser.css under the class name "mockup-browser".

--- frontend/src/components/Browser.tsx
+++ frontend/src/components/Browser.tsx
@@ -6,16 +6,7 @@ import { RootState } from "../store";
 function Browser(): JSX.Element {
   const url = useSelector((state: RootState) => state.browser.url);
   return (
-    <div
-      className="mockup-browser"
-      style={{
-        background: "black",
-        padding: "1rem",
-        height: "90%",
-        margin: "1rem",
-        borderRadius: "1rem",
-      }}
-    >
+    <div className="mockup-browser">
       <div className="mockup-browser-toolbar">
         <div className="input">{url}</div>
       </div>
