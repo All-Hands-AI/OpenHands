cc627d667c06aa9dbac6517b1bb35e5c4cc8972d
xcodebuild <me@xuan.at>
2024-04-01 11:10:56 +0800

    feat: add env var BACKEND_HOST

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
+const BACKEND_HOST = process.env.BACKEND_HOST || "localhost:3000";
+
+// check BACKEND_HOST is something like "localhost:3000" or "example.com"
+if (!BACKEND_HOST.match(/^(localhost|[\w\d-]+(\.[\w\d-]+)+(:\d+)?)/)) {
+  throw new Error(`Invalid BACKEND_HOST ${BACKEND_HOST}, example BACKEND_HOST localhost:3000`);
+}
+
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: "",
@@ -10,12 +17,12 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: `http://${BACKEND_HOST}/`,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path: string) => path.replace(/^\/api/, ""),
       },
       "/ws": {
-        target: "ws://localhost:3000/",
+        target: `ws://${BACKEND_HOST}/`,
         ws: true,
       },
     },
