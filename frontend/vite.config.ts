b2e2146755e8a4c7ed7a80f8eb89ac5e82251b14
Spencer Owen <owenspencer@gmail.com>
2024-04-04 11:00:01 -0600

    Make test less agressive

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,8 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 
 const BACKEND_HOST = process.env.BACKEND_HOST || "127.0.0.1:3000";
 
-// check BACKEND_HOST is something like "example.com"
-if (!BACKEND_HOST.match(/^([\w\d-]+(\.[\w\d-]+)+(:\d+)?)/)) {
+// check BACKEND_HOST is something like "localhost:3000"
+const backendHostRegex = /^(\w+(-\w+)*)(:(\d+))?$/;
   throw new Error(
     `Invalid BACKEND_HOST ${BACKEND_HOST}, example BACKEND_HOST 127.0.0.1:3000`,
   );
