0fdc401f910299dc585af3848dc49c5867d3db00
mashiro <github@te.sb>
2024-04-04 14:55:03 +0800

    chore: use pnpm to manage frontend deps (#659)
    * chore: use pnpm to manage frontend deps
    
    * typo: change content of tips
    
    * docs: update node.js require version & replacement installation link
    
    * feat: detect the Node.js version to ensure corepack is supported
    
    * typo: remove chinese comment
    
    * fix: lint CI config, use pnpm to install dependencies
    
    * fix: nextui style error when using pnpm
    
    * fix: ci setup pnpm cwd
    
    * fix: frontend lint ci install deps crash
    
    * fix: ci lint frontend missing package.json path
    
    * fix: frontend lint ci add cache-dependency-path prop

---
+++ frontend/scripts/detect-node-version.js
@@ -0,0 +1,28 @@
+function parseVersion(version) {
+  return version.split('.').map(Number);
+}
+
+function compareVersions(v1, v2) {
+  const v1Parts = parseVersion(v1);
+  const v2Parts = parseVersion(v2);
+
+  for (let i = 0; i < Math.max(v1Parts.length, v2Parts.length); i++) {
+    const v1Part = v1Parts[i] || 0;
+    const v2Part = v2Parts[i] || 0;
+
+    if (v1Part > v2Part) return 1;
+    if (v1Part < v2Part) return -1;
+  }
+
+  return 0;
+}
+
+const currentVersion = process.version.substring(1);
+const targetVersion = "18.17.1";
+
+if (compareVersions(currentVersion, targetVersion) > 0) {
+  console.log(`Current Node.js version is ${currentVersion}, corepack is supported.`);
+} else {
+  console.error(`Current Node.js version is ${currentVersion}, but corepack is unsupported. Required version: ^${targetVersion}.`);
+  process.exit(1)
+}
