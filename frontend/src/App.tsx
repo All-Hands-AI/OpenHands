8ec24ab7eaa049136708785f40c08810d99ec904
Alex Bäuerle <alex@a13x.io>
2024-04-04 18:45:28 -0700

    feat: start implementing new frontend layout (#737)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 import "./App.css";
+import CogTooth from "./assets/cog-tooth";
 import ChatInterface from "./components/ChatInterface";
 import Errors from "./components/Errors";
 import SettingModal from "./components/SettingModal";
+import Terminal from "./components/Terminal";
 import Workspace from "./components/Workspace";
+
+interface Props {
+  setSettingOpen: (isOpen: boolean) => void;
+}
+
+function LeftNav({ setSettingOpen }: Props): JSX.Element {
+  return (
+    <div className="flex flex-col h-full p-4 bg-bg-dark w-16 items-center shrink-0">
+      <div
+        className="mt-auto cursor-pointer hover:opacity-80"
+        onClick={() => setSettingOpen(true)}
+      >
+        <CogTooth />
+      </div>
+    </div>
+  );
+}
 
 function App(): JSX.Element {
   const [settingOpen, setSettingOpen] = useState(false);
@@ -14,15 +33,22 @@ function App(): JSX.Element {
 
   return (
     <div className="flex h-screen bg-bg-dark text-white">
-      <Errors />
-      <div className="flex-1 rounded-xl m-4 overflow-hidden bg-bg-light">
-        <ChatInterface setSettingOpen={setSettingOpen} />
+      <LeftNav setSettingOpen={setSettingOpen} />
+      <div className="flex flex-col grow gap-3 py-3 pr-3">
+        <div className="flex gap-3 grow">
+          <div className="w-[500px] shrink-0 rounded-xl overflow-hidden border border-border">
+            <ChatInterface />
+          </div>
+          <div className="flex flex-col flex-1 overflow-hidden rounded-xl bg-bg-workspace border border-border">
+            <Workspace />
+          </div>
+        </div>
+        <div className="h-72 shrink-0 bg-bg-workspace rounded-xl border border-border flex flex-col">
+          <Terminal key="terminal" />
+        </div>
       </div>
-      <div className="flex flex-col flex-1 m-4 overflow-hidden rounded-xl bg-bg-light">
-        <Workspace />
-      </div>
-
       <SettingModal isOpen={settingOpen} onClose={handleCloseModal} />
+      <Errors />
     </div>
   );
 }
