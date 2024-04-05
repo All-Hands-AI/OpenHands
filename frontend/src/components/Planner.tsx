a202b2550fe23e38f19e3f60855328c57d79c866
Alex Bäuerle <alex@a13x.io>
2024-04-05 10:26:46 -0700

    fix: show code tab by default, planner not working yet (#775)
    * fix: show code tab by default, planner not working yet
    
    * remove planner content
    
    * remove planner for now

--- frontend/src/components/Planner.tsx
+++ frontend/src/components/Planner.tsx
@@ -1,37 +1,7 @@
 import React from "react";
 
 function Planner(): JSX.Element {
-  return (
-    <div className="h-full w-full bg-bg-workspace">
-      <h3>
-        Current Focus: Set up the development environment according to the
-        project&apos;s instructions.
-      </h3>
-      <ul className="ml-4 mt-3">
-        <li className="space-x-2">
-          <input type="checkbox" checked readOnly />
-          <span>
-            Clone the repository and review the README for project setup
-            instructions.
-          </span>
-        </li>
-        <li className="space-x-2">
-          <input type="checkbox" checked readOnly />
-          <span>
-            Identify the package manager and install necessary dependencies.
-          </span>
-        </li>
-        <li className="space-x-2">
-          <input type="checkbox" />
-          <span>
-            Set up the development environment according to the project&apos;s
-            instructions.
-          </span>
-        </li>
-        {/* Add more tasks */}
-      </ul>
-    </div>
-  );
+  return <div className="h-full w-full bg-bg-workspace">Coming soon...</div>;
 }
 
 export default Planner;
