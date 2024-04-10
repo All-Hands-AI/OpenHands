cc584445c68dd158905793b1e97e1936710e59c9
Robert Brennan <accounts@rbren.io>
2024-04-10 16:39:33 -0500

    implement exec box (#983)
    * implement exec box
    
    * fix autopep8 issue
    
    * fix mypy issue
    
    * fix some bugs
    
    * set default to ssh
    
    * rename sandbox impls
    
    * empty commit
    
    * fix imports
    
    ---------
    
    Co-authored-by: Anas Dorbani <anasdorbani@gmail.com>

--- opendevin/config.py
+++ opendevin/config.py
@@ -21,6 +21,7 @@ DEFAULT_CONFIG: dict = {
     ConfigType.DIRECTORY_REWRITE: '',
     ConfigType.MAX_ITERATIONS: 100,
     ConfigType.AGENT: 'MonologueAgent',
+    ConfigType.SANDBOX_TYPE: 'ssh'
 }
 
 config_str = ''
