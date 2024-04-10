108e2401d890b0aa6b73a0cabeacc0d07f7076d4
Robert Brennan <contact@rbren.io>
2024-04-10 11:50:13 -0400

    fix mypy issue

--- opendevin/sandbox/sandbox.py
+++ opendevin/sandbox/sandbox.py
@@ -211,8 +211,9 @@ class DockerInteractive(CommandExecutor):
             f"Connecting to {username}@{hostname} via ssh."
         )
         logger.info(
-            f"If you encounter any issues, you can try `ssh -v -p 2222 {username}@{
-                hostname}` with the password '{self._ssh_password}' and report the issue on GitHub."
+            # FIXME: mypy and autopep8 fight each other on this line
+            # autopep8: off
+            f"If you encounter any issues, you can try `ssh -v -p 2222 {username}@{hostname}` with the password '{self._ssh_password}' and report the issue on GitHub."
         )
         self.ssh.login(hostname, username, self._ssh_password, port=2222)
 
