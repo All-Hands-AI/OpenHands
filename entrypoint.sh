66915c415cc5bb5b4de7acb43405a4fb9920eb65
Robert Brennan <contact@rbren.io>
2024-04-11 12:53:27 -0400

    rm entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-echo "starting backend..."
-poetry run uvicorn opendevin.server.listen:app --port 3000 --host 0.0.0.0 &
-
-echo "starting frontend..."
-cd frontend
-npm run start -- --host 0.0.0.0
