0f2b77a8e3de7247f18ef69f6b490b9d33e60a28
Lehcode <53556648+lehcode@users.noreply.github.com>
2024-04-12 18:38:41 +0300

    feat: Add FastAPI server and Vite dev server logging for debug and live modes

--- docker/devin/app/entrypoint.sh
+++ docker/devin/app/entrypoint.sh
@@ -14,7 +14,7 @@ if [ -n "${DEBUG}" ]; then
     env | grep JUPYTER_PORT
     echo "Nvidia CUDA properties:"
     nvidia-smi
-    bash $BIN_DIR/env_debug
+    bash "$BIN_DIR/env_debug"
 fi
 
 set -eux
