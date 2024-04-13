7bc554a7a129fa38855a50bfd423cddae0497a74
Boxuan Li <liboxuan@connect.hku.hk>
2024-04-13 00:33:17 -0700

    Ultimate fix

--- opendevin/exceptions.py
+++ opendevin/exceptions.py
@@ -3,7 +3,7 @@ class MaxCharsExceedError(Exception):
         if num_of_chars is not None and max_chars_limit is not None:
             # FIXME: autopep8 and mypy are fighting each other on this line
             # autopep8: off
-            message = f"Number of characters {num_of_chars} exceeds MAX_CHARS limit: {max_chars_limit}"
+            message = f'Number of characters {num_of_chars} exceeds MAX_CHARS limit: {max_chars_limit}'
         else:
             message = 'Number of characters exceeds MAX_CHARS limit'
         super().__init__(message)
