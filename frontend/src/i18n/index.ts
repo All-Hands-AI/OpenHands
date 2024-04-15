bb68565355af51f444cc8b9e0106b782e8e6900a
Jim Su <jimsu@protonmail.com>
2024-04-14 22:59:06 -0400

    Add space

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -14,7 +14,7 @@ export const AvailableLanguages = [
   { label: "Italiano", value: "it" },
   { label: "Português", value: "pt" },
   { label: "Español", value: "es" },
-  { label: "Türkçe", value: "tr"},
+  { label: "Türkçe", value: "tr" },
 ];
 
 i18n
