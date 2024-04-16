5881857d5c49adb5dec6b87fb973ee9729bf1dca
sp.wack <83104063+amanape@users.noreply.github.com>
2024-04-16 19:49:56 +0300

    test(frontend): add unit tests (#1076)
    * test(frontend): add unit tests for getCachedConfig
    
    * test(frontend): add unit tests for getCachedConfig
    
    * add unit test for the useTypingEffect hook
    
    * add unit test for the useInputComposition hook
    
    * create unit test for auth service
    
    * remove outdated and failing component test
    
    * create unit test for session service
    
    * break down saveSettings into smaller functions for testability and create unit test for new mergeAndUpdateSettings
    
    ---------
    
    Co-authored-by: Robert Brennan <accounts@rbren.io>

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -14,7 +14,7 @@ const fetchToken = async (): Promise<ResFetchToken> => {
   return data;
 };
 
-const validateToken = (token: string): boolean => {
+export const validateToken = (token: string): boolean => {
   try {
     const claims = jose.decodeJwt(token);
     return !(claims.sid === undefined || claims.sid === "");
