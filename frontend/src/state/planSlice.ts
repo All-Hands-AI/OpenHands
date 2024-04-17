b040ee56fa214ad15bab3ae38d9a1343219737ef
Alex Bäuerle <alex@a13x.io>
2024-04-17 11:04:52 -0700

    fix type

--- frontend/src/state/planSlice.ts
+++ frontend/src/state/planSlice.ts
@@ -1,12 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Plan } from "../services/planService";
+import { Plan, TaskState } from "../services/planService";
 
 export const planSlice = createSlice({
   name: "plan",
   initialState: {
     plan: {
       mainGoal: undefined,
-      task: { id: "", goal: "", parent: "Task | None", subtasks: [] },
+      task: {
+        id: "",
+        goal: "",
+        parent: "Task | None",
+        subtasks: [],
+        state: TaskState.OPEN_STATE,
+      },
     } as Plan,
   },
   reducers: {
