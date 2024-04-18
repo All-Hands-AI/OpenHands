c2da8f44c22a8db0508c5d9b6ca2200007330bc2
amanape <stephanpsaras@gmail.com>
2024-04-18 23:48:19 +0300

    refactor for better readability and extending

--- frontend/src/components/file-explorer/TreeNode.tsx
+++ frontend/src/components/file-explorer/TreeNode.tsx
@@ -2,6 +2,26 @@ import React from "react";
 import FolderIcon from "../FolderIcon";
 import FileIcon from "../FileIcons";
 
+interface TitleProps {
+  name: string;
+  type: "folder" | "file";
+  isOpen: boolean;
+  onClick: () => void;
+}
+
+function Title({ name, type, isOpen, onClick }: TitleProps) {
+  return (
+    <div
+      onClick={onClick}
+      className="cursor-pointer rounded-[5px] p-1 nowrap flex items-center gap-2 aria-selected:bg-neutral-600 aria-selected:text-white hover:text-white"
+    >
+      {type === "folder" && <FolderIcon isOpen={isOpen} />}
+      {type === "file" && <FileIcon filename={name} />}
+      {name}
+    </div>
+  );
+}
+
 interface TreeNodeProps {
   node: TreeNode;
   path: string;
@@ -27,17 +47,13 @@ function TreeNode({
 
   return (
     <div className="text-sm text-neutral-400">
-      <div
+      <Title
+        name={node.name}
+        type={node.children ? "folder" : "file"}
+        isOpen={isOpen}
         onClick={handleClick}
-        className="cursor-pointer rounded-[5px] p-1 nowrap flex items-center gap-2 aria-selected:bg-neutral-600 aria-selected:text-white hover:text-white"
-      >
-        {node.children ? (
-          <FolderIcon isOpen={isOpen} />
-        ) : (
-          <FileIcon filename={node.name} />
-        )}
-        {node.name}
-      </div>
+      />
+
       {isOpen && node.children && (
         <div className="ml-5">
           {node.children.map((child, index) => (
