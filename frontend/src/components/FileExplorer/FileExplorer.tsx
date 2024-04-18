9688f4d56bce71609e1dd8a17376fa9b01a40e15
amanape <stephanpsaras@gmail.com>
2024-04-18 20:40:10 +0300

    initial commit - file explorer

---
+++ frontend/src/components/FileExplorer/FileExplorer.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { getWorkspace } from "../../services/fileService";
+import ExplorerTree from "./ExplorerTree";
+
+interface FileExplorerProps {
+  onFileClick: (path: string) => void;
+}
+
+function FileExplorer({ onFileClick }: FileExplorerProps) {
+  const [workspace, setWorkspace] = React.useState<TreeNode[]>([]);
+  const [isHidden, setIsHidden] = React.useState(false);
+
+  const getWorkspaceData = async () => {
+    const wsFile = await getWorkspace();
+    setWorkspace([wsFile]);
+  };
+
+  React.useEffect(() => {
+    (async () => {
+      await getWorkspaceData();
+    })();
+  }, []);
+
+  return (
+    <div>
+      <div style={{ display: isHidden ? "none" : "block" }}>
+        <ExplorerTree tree={workspace} onFileClick={onFileClick} />
+      </div>
+
+      <button
+        data-testid="close"
+        type="button"
+        onClick={() => setIsHidden((prev) => !prev)}
+      >
+        {isHidden ? "Open" : "Close"}
+      </button>
+      <button
+        data-testid="refresh"
+        type="button"
+        onClick={async () => getWorkspaceData()}
+      >
+        Refresh
+      </button>
+    </div>
+  );
+}
+
+export default FileExplorer;
