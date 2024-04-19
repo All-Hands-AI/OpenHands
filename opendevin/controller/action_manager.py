76b81ca0ed2946948ea9c19379b60c0859f589bb
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2024-04-19 11:21:58 -0700

    Integrate E2B sandbox as an alternative to a Docker container (#727)
    * add e2b sandbox [wip]
    
    * Install e2b package
    
    * Add basic E2B sandbox integration
    
    * Update dependencies and fix command execution in E2BSandbox
    
    * Udpate e2b
    
    * Add comment
    
    * Lint
    
    * Remove unnecessary type conversion
    
    * Lint
    
    * Fix linting
    
    * Resolve comments
    
    * Update opendevin/action/fileop.py
    
    * Update opendevin/action/fileop.py
    
    * Fix log
    
    * Update E2B readme
    
    * poetry lock
    
    ---------
    
    Co-authored-by: Robert Brennan <accounts@rbren.io>

--- opendevin/controller/action_manager.py
+++ opendevin/controller/action_manager.py
@@ -3,7 +3,7 @@ import traceback
 
 from opendevin import config
 from opendevin.observation import CmdOutputObservation
-from opendevin.sandbox import DockerExecBox, DockerSSHBox, Sandbox, LocalBox
+from opendevin.sandbox import DockerExecBox, DockerSSHBox, Sandbox, LocalBox, E2BBox
 from opendevin.schema import ConfigType
 from opendevin.logger import opendevin_logger as logger
 from opendevin.action import (
@@ -18,7 +18,7 @@ from opendevin.observation import (
 
 class ActionManager:
     id: str
-    shell: Sandbox
+    sandbox: Sandbox
 
     def __init__(
             self,
@@ -27,15 +27,17 @@ class ActionManager:
     ):
         sandbox_type = config.get(ConfigType.SANDBOX_TYPE).lower()
         if sandbox_type == 'exec':
-            self.shell = DockerExecBox(
+            self.sandbox = DockerExecBox(
                 sid=(sid or 'default'), container_image=container_image
             )
         elif sandbox_type == 'local':
-            self.shell = LocalBox()
+            self.sandbox = LocalBox()
         elif sandbox_type == 'ssh':
-            self.shell = DockerSSHBox(
+            self.sandbox = DockerSSHBox(
                 sid=(sid or 'default'), container_image=container_image
             )
+        elif sandbox_type == 'e2b':
+            self.sandbox = E2BBox()
         else:
             raise ValueError(f'Invalid sandbox type: {sandbox_type}')
 
@@ -58,23 +60,23 @@ class ActionManager:
             return self._run_immediately(command)
 
     def _run_immediately(self, command: str) -> CmdOutputObservation:
-        exit_code, output = self.shell.execute(command)
+        exit_code, output = self.sandbox.execute(command)
         return CmdOutputObservation(
             command_id=-1, content=output, command=command, exit_code=exit_code
         )
 
     def _run_background(self, command: str) -> CmdOutputObservation:
-        bg_cmd = self.shell.execute_in_background(command)
-        content = f'Background command started. To stop it, send a `kill` action with id {bg_cmd.id}'
+        bg_cmd = self.sandbox.execute_in_background(command)
+        content = f'Background command started. To stop it, send a `kill` action with id {bg_cmd.pid}'
         return CmdOutputObservation(
             content=content,
-            command_id=bg_cmd.id,
+            command_id=bg_cmd.pid,
             command=command,
             exit_code=0,
         )
 
     def kill_command(self, id: int) -> CmdOutputObservation:
-        cmd = self.shell.kill_background(id)
+        cmd = self.sandbox.kill_background(id)
         return CmdOutputObservation(
             content=f'Background command with id {id} has been killed.',
             command_id=id,
@@ -84,7 +86,7 @@ class ActionManager:
 
     def get_background_obs(self) -> List[CmdOutputObservation]:
         obs = []
-        for _id, cmd in self.shell.background_commands.items():
+        for _id, cmd in self.sandbox.background_commands.items():
             output = cmd.read_logs()
             if output is not None and output != '':
                 obs.append(
