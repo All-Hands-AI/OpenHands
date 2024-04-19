d8aba848f6f8a028b539a99902d4308e53244e86
Robert Brennan <contact@rbren.io>
2024-04-19 09:53:10 -0400

    add instructions parser

---
+++ agenthub/micro/instructions.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+instructions = {}
+
+base_dir = os.path.dirname(os.path.abspath(__file__)) + "/_instructions"
+print(base_dir)
+for root, dirs, files in os.walk(base_dir):
+    if len(files) == 0:
+        continue
+    rel_base = os.path.relpath(root, base_dir)
+    keys = rel_base.split("/")
+    obj = instructions
+    for key in keys:
+        if key not in obj:
+            obj[key] = {}
+        obj = obj[key]
+    for file in files:
+        without_ext = os.path.splitext(file)[0]
+        with open(os.path.join(root, file), "r") as f:
+            obj[without_ext] = f.read()
+
+print(instructions)
