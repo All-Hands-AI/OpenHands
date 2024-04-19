e6d91affc6a2ab3678255c46e6b6c835f2378f56
Alex Bäuerle <alex@a13x.io>
2024-04-19 10:25:40 -0700

    refactor: remove the previously implemented shallow file fetch (#1231)
    Removing because the new filetree implementation is fast enough as is and fetching partial trees adds a lot of complexity that we probably don't need. For example, if we still want to automatically open the tree location of a changed file, we need to make sure all the parent folders are fetched first.

--- frontend/src/services/fileService.ts
+++ frontend/src/services/fileService.ts
@@ -17,21 +17,3 @@ export async function getWorkspace(): Promise<WorkspaceFile> {
   const data = await res.json();
   return data as WorkspaceFile;
 }
-
-export type WorkspaceItem = {
-  name: string;
-  isBranch: boolean;
-  relativePath: string;
-  id: string;
-  parent: string | null;
-  children: string[];
-};
-
-export async function getWorkspaceDepthOne(
-  relpath: string,
-): Promise<WorkspaceItem[]> {
-  const res = await fetch(`/api/list-files?relpath=${relpath}`);
-  const json = await res.json();
-  const files = json.files as WorkspaceItem[];
-  return files;
-}
