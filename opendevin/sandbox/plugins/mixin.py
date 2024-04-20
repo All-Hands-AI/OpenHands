1ada5759429002b54e96d8c348263ab13cd2b890
Xingyao Wang <xingyao6@illinois.edu>
2024-04-20 11:33:35 -0500

    further improve plugin mixin

--- opendevin/sandbox/plugins/mixin.py
+++ opendevin/sandbox/plugins/mixin.py
@@ -1,3 +1,4 @@
+import os
 from typing import List, Protocol, Tuple
 from opendevin.logger import opendevin_logger as logger
 from opendevin.sandbox.plugins.requirement import PluginRequirement
@@ -14,9 +15,15 @@ class PluginMixin:
     """Mixin for Sandbox to support plugins."""
 
     def init_plugins(self: HasExecuteProtocol,
-                     requirements: List[PluginRequirement]):
+                     requirements: List[PluginRequirement],
+                     plugin_dir: str = '/opendevin/plugins'
+                     ):
         """Load a plugin into the sandbox."""
         for requirement in requirements:
             # Execute the bash script
-            logger.info(f'Initalizing plugin {requirement.name} by executing [{requirement.bash_script_path}]')
-            self.execute(requirement.bash_script_path)
+            abs_path_to_bash_script = os.path.join(plugin_dir, requirement.bash_script_path)
+            logger.info(f'Initalizing plugin {requirement.name} by executing [{abs_path_to_bash_script}] in the sandbox.')
+            exit_code, output = self.execute(abs_path_to_bash_script)
+            if exit_code != 0:
+                raise RuntimeError(f'Failed to initialize plugin {requirement.name} with exit code {exit_code} and output {output}')
+            logger.info(f'Plugin {requirement.name} initialized successfully\n:{output}')
--- opendevin/sandbox/plugins/requirement.py
+++ opendevin/sandbox/plugins/requirement.py
@@ -5,4 +5,5 @@ from dataclasses import dataclass
 class PluginRequirement:
     """Requirement for a plugin."""
     name: str
+    # NOTE: bash_script_path shoulds be relative to the `plugin` directory
     bash_script_path: str
