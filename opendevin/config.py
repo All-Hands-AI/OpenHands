64c531079634fafe0b0636177e57053cbed84b29
Boxuan Li <liboxuan@connect.hku.hk>
2024-04-19 21:58:07 -0700

    Fix arg parser help message display

--- opendevin/config.py
+++ opendevin/config.py
@@ -58,13 +58,51 @@ def parse_arguments():
         type=str,
         help='The working directory for the agent',
     )
+    parser.add_argument(
+        '-t', '--task', type=str, default='', help='The task for the agent to perform'
+    )
+    parser.add_argument(
+        '-f',
+        '--file',
+        type=str,
+        help='Path to a file containing the task. Overrides -t if both are provided.',
+    )
+    parser.add_argument(
+        '-c',
+        '--agent-cls',
+        default='MonologueAgent',
+        type=str,
+        help='The agent class to use',
+    )
+    parser.add_argument(
+        '-m',
+        '--model-name',
+        default=config.get(ConfigType.LLM_MODEL),
+        type=str,
+        help='The (litellm) model name to use',
+    )
+    parser.add_argument(
+        '-i',
+        '--max-iterations',
+        default=config.get(ConfigType.MAX_ITERATIONS),
+        type=int,
+        help='The maximum number of iterations to run the agent',
+    )
+    parser.add_argument(
+        '-n',
+        '--max-chars',
+        default=config.get(ConfigType.MAX_CHARS),
+        type=int,
+        help='The maximum number of characters to send to and receive from LLM per task',
+    )
     args, _ = parser.parse_known_args()
     if args.directory:
         config[ConfigType.WORKSPACE_BASE] = os.path.abspath(args.directory)
         print(f'Setting workspace base to {config[ConfigType.WORKSPACE_BASE]}')
+    return args
 
 
-parse_arguments()
+args = parse_arguments()
 
 
 def finalize_config():
