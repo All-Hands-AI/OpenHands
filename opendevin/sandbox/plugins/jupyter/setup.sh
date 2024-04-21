bac492a4becd9f6831c0bca950c9cae92f1359cb
Xingyao Wang <xingyao6@illinois.edu>
2024-04-21 01:12:23 -0500

    update build.sh to make logs available in /opendevin/logs

--- opendevin/sandbox/plugins/jupyter/setup.sh
+++ opendevin/sandbox/plugins/jupyter/setup.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-pip install jupyterlab notebook jupyter_kernel_gateway
-
 # ADD /opendevin/plugins to PATH to make `jupyter_cli` available
 echo 'export PATH=$PATH:/opendevin/plugins/jupyter' >> ~/.bashrc
 export PATH=/opendevin/plugins/jupyter:$PATH
@@ -19,23 +17,25 @@ if [ "$USER" = "root" ]; then
     export PATH=$PATH:/root/.local/bin
 fi
 
+# Install dependencies
+pip install jupyterlab notebook jupyter_kernel_gateway
+
+# Create logs directory
+sudo mkdir -p /opendevin/logs && sudo chmod 777 /opendevin/logs
+
 # Run background process to start jupyter kernel gateway
 export JUPYTER_GATEWAY_PORT=18888
-jupyter kernelgateway --KernelGatewayApp.ip=0.0.0.0 --KernelGatewayApp.port=$JUPYTER_GATEWAY_PORT &
-
+jupyter kernelgateway --KernelGatewayApp.ip=0.0.0.0 --KernelGatewayApp.port=$JUPYTER_GATEWAY_PORT > /opendevin/logs/jupyter_kernel_gateway.log 2>&1 &
 export JUPYTER_GATEWAY_PID=$!
 echo "export JUPYTER_GATEWAY_PID=$JUPYTER_GATEWAY_PID" >> ~/.bashrc
 export JUPYTER_GATEWAY_KERNEL_ID="default"
 echo "export JUPYTER_GATEWAY_KERNEL_ID=$JUPYTER_GATEWAY_KERNEL_ID" >> ~/.bashrc
-
 echo "JupyterKernelGateway started with PID: $JUPYTER_GATEWAY_PID"
 
 # Start the jupyter_server
 export JUPYTER_EXEC_SERVER_PORT=18889
 echo "export JUPYTER_EXEC_SERVER_PORT=$JUPYTER_EXEC_SERVER_PORT" >> ~/.bashrc
-/opendevin/plugins/jupyter/execute_server &
-
+/opendevin/plugins/jupyter/execute_server > /opendevin/logs/jupyter_execute_server.log 2>&1 &
 export JUPYTER_EXEC_SERVER_PID=$!
 echo "export JUPYTER_EXEC_SERVER_PID=$JUPYTER_EXEC_SERVER_PID" >> ~/.bashrc
-
 echo "Execution server started with PID: $JUPYTER_EXEC_SERVER_PID"
