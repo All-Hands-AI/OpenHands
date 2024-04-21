456d9cbecdcdb70f6e4ddf2e295db08acb07608c
Xingyao Wang <xingyao6@illinois.edu>
2024-04-20 23:07:25 -0500

    make jupyter requirement a dataclass

--- opendevin/sandbox/plugins/jupyter/__init__.py
+++ opendevin/sandbox/plugins/jupyter/__init__.py
@@ -1,10 +1,9 @@
 import os
+from dataclasses import dataclass
 from opendevin.sandbox.plugins.requirement import PluginRequirement
 
 
+@dataclass
 class JupyterRequirement(PluginRequirement):
-    def __init__(self):
-        super().__init__(
-            'jupyter',
-            os.path.join('jupyter', 'setup.sh')
-        )
+    name: str = 'jupyter'
+    bash_script_path: str = os.path.join('jupyter', 'setup.sh')
