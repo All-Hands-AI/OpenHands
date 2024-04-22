896d91acd7dc005766d8c4e00319867ef91304ff
Robert Brennan <contact@rbren.io>
2024-04-22 10:48:20 -0400

    refactor microagent registration a bit

--- agenthub/__init__.py
+++ agenthub/__init__.py
@@ -1,7 +1,6 @@
+from .micro.registry import all_agents
 from .micro.agent import MicroAgent
 from opendevin.agent import Agent
-import os
-import yaml
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -16,26 +15,14 @@ from . import SWE_agent      # noqa: E402
 __all__ = ['monologue_agent', 'codeact_agent',
            'planner_agent', 'SWE_agent']
 
-# list all dirs in ./micro
-for dir in os.listdir(os.path.dirname(__file__) + '/micro'):
-    base = os.path.dirname(__file__) + '/micro/' + dir
-    if os.path.isfile(base):
-        continue
-    if dir.startswith('_'):
-        continue
-    promptFile = base + '/prompt.md'
-    agentFile = base + '/agent.yaml'
-    if not os.path.isfile(promptFile) or not os.path.isfile(agentFile):
-        raise Exception(
-            f'Missing prompt or agent file in {base}. Please create them.')
-    with open(promptFile, 'r') as f:
-        prompt = f.read()
-    with open(agentFile, 'r') as f:
-        agent = yaml.safe_load(f)
+# all_agents is a dict of name: agent_class
+for agent in all_agents.values():
+    name = agent['name']
+    prompt = agent['prompt']
 
-    anon_class = type(agent['name'], (MicroAgent,), {
+    anon_class = type(name, (MicroAgent,), {
         'prompt': prompt,
         'agentDefinition': agent,
     })
 
-    Agent.register(agent['name'], anon_class)
+    Agent.register(name, anon_class)
