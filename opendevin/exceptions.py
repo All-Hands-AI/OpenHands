cbf84749892f287b0b9854640024258ef208ae48
Engel Nyst <enyst@users.noreply.github.com>
2024-04-24 00:15:47 +0200

    Send the failed decoding back to the LLM

--- opendevin/exceptions.py
+++ opendevin/exceptions.py
@@ -7,11 +7,6 @@ class MaxCharsExceedError(Exception):
         super().__init__(message)
 
 
-class AgentNoActionError(Exception):
-    def __init__(self, message='Agent must return an action'):
-        super().__init__(message)
-
-
 class AgentNoInstructionError(Exception):
     def __init__(self, message='Instruction must be provided'):
         super().__init__(message)
@@ -19,11 +14,6 @@ class AgentNoInstructionError(Exception):
 
 class AgentEventTypeError(Exception):
     def __init__(self, message='Event must be a dictionary'):
-        super().__init__(message)
-
-
-class AgentMalformedActionError(Exception):
-    def __init__(self, message='Malformed response'):
         super().__init__(message)
 
 
@@ -65,4 +55,15 @@ class PlanInvalidStateError(Exception):
             message = f'Invalid state {state}'
         else:
             message = 'Invalid state'
+        super().__init__(message)
+
+
+# These exceptions get sent back to the LLM
+class AgentMalformedActionError(Exception):
+    def __init__(self, message='Malformed response'):
+        super().__init__(message)
+
+
+class AgentNoActionError(Exception):
+    def __init__(self, message='Agent must return an action'):
         super().__init__(message)
