ec8f6c38d3753aef78648f35526dc22833bf11b5
Boxuan Li <liboxuan@connect.hku.hk>
2024-04-24 01:06:30 -0700

    Use existing logs

--- tests/integration/README.md
+++ tests/integration/README.md
@@ -1,1 +1,29 @@
 In order to run integration tests, please ensure your workspace is empty.
+
+Your `config.toml` should look like this:
+
+```toml
+LLM_MODEL="gpt-4-turbo"
+LLM_API_KEY="<your-api-key>"
+LLM_EMBEDDING_MODEL="openai"
+WORKSPACE_MOUNT_PATH="<absolute-path-of-your-workspace>"
+```
+
+You can choose any model you'd like to generate the mock responses.
+You can even handcraft mock responses, especially when you would like to test the behaviour of agent for corner cases. If you use a very weak model (e.g. 8B params), chance is most agents won't be able to finish the task.
+
+```bash
+# Remove logs iff you are okay to lose logs. This helps us locate the prompts and responses quickly, but is NOT a must.
+rm -rf logs
+# Clear the workspace, otherwise OpenDevin might not be able to reproduce your prompts in CI environment. Feel free to change the workspace name and path. Be sure to set `WORKSPACE_MOUNT_PATH` to the same absolute path.
+rm -rf workspace
+mkdir workspace
+# Depending on the complexity of the task you want to test, you can change the number of iterations limit. Change agent accordingly. If you are adding a new test, try generating mock responses for every agent.
+poetry run python ./opendevin/main.py -i 10 -t "Write a shell script 'hello.sh' that prints 'hello'." -c "MonologueAgent" -d "./workspace"
+```
+
+After running the above commands, you should be able to locate the real prompts
+and responses logged. The log folder follows `logs/llm/%y-%m-%d_%H-%M` format.
+
+Now, move all files under that folder to `tests/integration/mock/<AGENT>/<test-name>` folder. For example, moving all files from `logs/llm/24-04-23_21-55/` folder to
+`tests/integration/mock/MonologueAgent/test_write_simple_script` folder.
--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,4 +1,4 @@
-import json
+import re
 import os
 
 import pytest
@@ -9,33 +9,50 @@ mock_dir = os.path.join(script_dir, 'mock', os.environ.get('AGENT'))
 
 
 def filter_out_symbols(input):
-    return ' '.join([char for char in input if char.isalpha()])
+    return ' '.join([char for char in input if char.isalnum()])
+
+
+def get_log_id(prompt_log_name):
+    match = re.search(r'prompt_(\d+).log', prompt_log_name)
+    if match:
+        return match.group(1)
 
 
 def get_mock_response(messages):
     # Find mock response based on prompt. Prompts are stored under nested
-    # folders under mock folder. If prompt_{id}.json matches,
-    # then the mock response we're looking for is at `resp_{id}.json`.
-    prompt = filter_out_symbols(json.dumps(messages))
+    # folders under mock folder. If prompt_{id}.log matches,
+    # then the mock response we're looking for is at response_{id}.log.
+    prompt = filter_out_symbols(messages)
     for root, _, files in os.walk(mock_dir):
         for file in files:
-            if file.startswith('prompt_') and file.endswith('.json'):
+            if file.startswith('prompt_') and file.endswith('.log'):
                 file_path = os.path.join(root, file)
-                # Open the prompt file and compare its contents to kwargs_json
+                # Open the prompt file and compare its contents
                 with open(file_path, 'r') as f:
-                    file_content = filter_out_symbols(json.dumps(json.loads(f.read())['messages']))
+                    file_content = filter_out_symbols(f.read())
+                    # print('\n\n\nfile:')
+                    # print(file_content)
                     if file_content == prompt:
                         # If a match is found, construct the corresponding response file path
-                        resp_file_path = os.path.join(root, f'resp_{file[len("prompt_"): -5]}.json')
+                        log_id = get_log_id(file_path)
+                        resp_file_path = os.path.join(root, f'response_{log_id}.log')
                         # Read the response file and return its content
                         with open(resp_file_path, 'r') as resp_file:
-                            return json.loads(resp_file.read())
+                            return resp_file.read()
+                            # lines = resp_file.read()
+                            # if lines[0].strip() == '```json':
+                            #     lines = lines[1:]  # Remove the first line
+                            # if lines[-1].strip() == '```':
+                            #     lines = lines[:-1]  # Remove the last line
+                            # return json.loads(resp_file.read())
 
 
 def mock_completion(*args, **kwargs):
-    # Convert kwargs to JSON string for comparison
-    messages = kwargs.get('messages', [])
-    mock_response = get_mock_response(messages)
+    messages = kwargs['messages']
+    message_str = ''
+    for message in messages:
+        message_str += message['content']
+    mock_response = get_mock_response(message_str)
     assert mock_response is not None, 'Mock response for prompt is not found'
     response = completion(**kwargs, mock_response=mock_response)
     return response
