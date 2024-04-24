a2e628c578e7e824d41884f6a2ce5c3a6b3cc61d
Xingyao Wang <xingyao6@illinois.edu>
2024-04-24 18:06:39 +0800

    fix swe-tool plugins; add missing _split_string

--- opendevin/sandbox/plugins/swe_agent_commands/setup_cursor_mode.sh
+++ opendevin/sandbox/plugins/swe_agent_commands/setup_cursor_mode.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pip install flake8
+
 # Cursor Mode from SWE-Bench
 # https://github.com/princeton-nlp/SWE-agent/blob/ca54d5556b9db4f4f2be21f09530ce69a72c0305/config/configs/default_sys-env_cursors_window100-detailed_cmd_format-last_5_history-1_demos.yaml#L108-L111
 echo '
@@ -27,6 +29,13 @@ state() {
 };
 ' >> ~/.bashrc
 
+# make _split_string (py) available
+echo 'export PATH=$PATH:/opendevin/plugins/swe_agent_commands' >> ~/.bashrc
+
 echo 'source /opendevin/plugins/swe_agent_commands/cursors_defaults.sh' >> ~/.bashrc
 echo 'source /opendevin/plugins/swe_agent_commands/cursors_edit_linting.sh' >> ~/.bashrc
 echo 'source /opendevin/plugins/swe_agent_commands/search.sh' >> ~/.bashrc
+
+echo 'export SWE_CMD_WORK_DIR="/opendevin/plugins/swe_agent_commands/workdir"' >> ~/.bashrc
+sudo mkdir -p /opendevin/plugins/swe_agent_commands/workdir
+sudo chmod 777 /opendevin/plugins/swe_agent_commands/workdir
