bac3784336da8006c19b536b95e8dcb666722c3a
Boxuan Li <liboxuan@connect.hku.hk>
2024-04-24 19:36:55 -0700

    conftest: look up under test name folder only

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,11 +1,11 @@
 import re
 import os
+from functools import partial
 
 import pytest
 from litellm import completion
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
-mock_dir = os.path.join(script_dir, 'mock', os.environ.get('AGENT'))
 
 
 def filter_out_symbols(input):
@@ -18,10 +18,11 @@ def get_log_id(prompt_log_name):
         return match.group(1)
 
 
-def get_mock_response(messages):
+def get_mock_response(test_name, messages):
     # Find mock response based on prompt. Prompts are stored under nested
     # folders under mock folder. If prompt_{id}.log matches,
     # then the mock response we're looking for is at response_{id}.log.
+    mock_dir = os.path.join(script_dir, 'mock', os.environ.get('AGENT'), test_name)
     prompt = filter_out_symbols(messages)
     for root, _, files in os.walk(mock_dir):
         for file in files:
@@ -30,8 +31,6 @@ def get_mock_response(messages):
                 # Open the prompt file and compare its contents
                 with open(file_path, 'r') as f:
                     file_content = filter_out_symbols(f.read())
-                    # print('\n\n\nfile:')
-                    # print(file_content)
                     if file_content == prompt:
                         # If a match is found, construct the corresponding response file path
                         log_id = get_log_id(file_path)
@@ -39,25 +38,20 @@ def get_mock_response(messages):
                         # Read the response file and return its content
                         with open(resp_file_path, 'r') as resp_file:
                             return resp_file.read()
-                            # lines = resp_file.read()
-                            # if lines[0].strip() == '```json':
-                            #     lines = lines[1:]  # Remove the first line
-                            # if lines[-1].strip() == '```':
-                            #     lines = lines[:-1]  # Remove the last line
-                            # return json.loads(resp_file.read())
 
 
-def mock_completion(*args, **kwargs):
+def mock_completion(*args, test_name, **kwargs):
     messages = kwargs['messages']
     message_str = ''
     for message in messages:
         message_str += message['content']
-    mock_response = get_mock_response(message_str)
+    mock_response = get_mock_response(test_name, message_str)
     assert mock_response is not None, 'Mock response for prompt is not found'
     response = completion(**kwargs, mock_response=mock_response)
     return response
 
 
 @pytest.fixture(autouse=True)
-def patch_completion(monkeypatch):
-    monkeypatch.setattr('opendevin.llm.llm.litellm_completion', mock_completion)
+def patch_completion(monkeypatch, request):
+    test_name = request.node.name
+    monkeypatch.setattr('opendevin.llm.llm.litellm_completion', partial(mock_completion, test_name=test_name))
