cd9133851012807d2a85040b5f6a775da301c01b
Alex Bäuerle <alex@a13x.io>
2024-04-26 09:29:47 -0700

    update video url

--- docs/src/components/Demo/Demo.tsx
+++ docs/src/components/Demo/Demo.tsx
@@ -19,7 +19,7 @@ export function Demo() {
         ref={videoRef}
       >
         <source
-          src="https://github-production-user-asset-6210df.s3.amazonaws.com/38853559/318638664-71a472cc-df34-430c-8b1d-4d7286c807c9.webm?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240426%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240426T001612Z&X-Amz-Expires=300&X-Amz-Signature=8ab9974b557a380fcd8667beb362af7d5584357c182ae850e4e6244da53bf5fb&X-Amz-SignedHeaders=host&actor_id=5690524&key_id=0&repo_id=771302083"
+          src="https://private-user-images.githubusercontent.com/38853559/318638664-71a472cc-df34-430c-8b1d-4d7286c807c9.webm?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTQwOTEyNTAsIm5iZiI6MTcxNDA5MDk1MCwicGF0aCI6Ii8zODg1MzU1OS8zMTg2Mzg2NjQtNzFhNDcyY2MtZGYzNC00MzBjLThiMWQtNGQ3Mjg2YzgwN2M5LndlYm0_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNDI2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDQyNlQwMDIyMzBaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02YTQxNmE4ZjIzMDFkZTQ0ZTUxYjUzNGI5MmFhMDdhMzU4MmM1YTQyMTU4MmRkNDZmZmZkMWYyY2EzMzc1ZjVjJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.4dj-ZQvPwW_76Mh81XXD7CkZcXrs-lsh-R6BrmA-_gY"
           type="video/webm"
         ></source>
       </video>
