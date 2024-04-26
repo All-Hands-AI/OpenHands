b5ea645f8e4eaf9cba614c66f86f706b323a3438
Boxuan Li <liboxuan@connect.hku.hk>
2024-04-26 00:02:51 -0700

    local sandbox: Create workspace base during init

--- opendevin/sandbox/docker/local_box.py
+++ opendevin/sandbox/docker/local_box.py
@@ -1,10 +1,12 @@
 import subprocess
 import atexit
+import os
 from typing import Tuple, Dict
 from opendevin.sandbox.sandbox import Sandbox
 from opendevin.sandbox.process import Process
 from opendevin.sandbox.docker.process import DockerProcess
 from opendevin import config
+from opendevin.schema.config import ConfigType
 
 # ===============================================================================
 #  ** WARNING **
@@ -24,6 +26,7 @@ from opendevin import config
 
 class LocalBox(Sandbox):
     def __init__(self, timeout: int = 120):
+        os.makedirs(config.get(ConfigType.WORKSPACE_BASE), exist_ok=True)
         self.timeout = timeout
         self.background_commands: Dict[int, Process] = {}
         self.cur_background_id = 0
