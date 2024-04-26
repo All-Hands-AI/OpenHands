a6673e02e00b023aa38467c3df24a199e5f4b4cb
Xingyao Wang <xingyao6@illinois.edu>
2024-04-26 20:11:40 +0800

    support SyntaxHighlighter and markdown for jupyter visualization

--- frontend/src/components/Jupyter.tsx
+++ frontend/src/components/Jupyter.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "#/store";
-import Highlight from 'react-highlight'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import Markdown from 'react-markdown';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { Cell } from "#/state/jupyterSlice";
 
 interface IJupyterCell {
@@ -19,24 +21,29 @@ function JupyterCell({ cell }: IJupyterCell): JSX.Element {
           className={"scrollbar-custom scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20 overflow-auto px-5"}
           style={{ padding: 0, marginBottom: 0, fontSize: "0.75rem" }}
         >
-          <Highlight className="python">{code}</Highlight>
-        </pre>
-      </div>
+          <SyntaxHighlighter language="python" style={atomOneDark}>
+            {code}
+          </SyntaxHighlighter>
+      </pre>
+      </div >
     );
   }
   else {
     return (
       <div className={"rounded-lg bg-gray-800 dark:bg-gray-900 p-2 text-xs"}>
         <div className={"mb-1 text-gray-400"}>STDOUT/STDERR</div>
-          <pre
+        <pre
           className={"scrollbar-custom scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20 overflow-auto px-5 max-h-[60vh] bg-gray-800"}
           style={{ padding: 0, marginBottom: 0, fontSize: "0.75rem" }}
-          >
-            {/* <code
-            class="language-console">{@html highlightedCode || code.replaceAll("<", "&lt;")}</code
-          >< */}
-          <Highlight className="markdown">{code}</Highlight>
-          </pre>
+        >
+          {/* split code by newline and render each line as a plaintext, except it starts with `![image]` so we render it as markdown */}
+          {code.split("\n").map((line, index) => {
+            if (line.startsWith("![image](data:image/png;base64,")) {
+              return <Markdown key={index}>{line.slice(2, -1)}</Markdown>;
+            }
+            return <SyntaxHighlighter key={index} language="plaintext" style={atomOneDark}>{line}</SyntaxHighlighter>;
+          })}
+        </pre>
       </div>
     );
   }
