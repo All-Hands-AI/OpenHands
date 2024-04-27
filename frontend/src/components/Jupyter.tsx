38c1afe9c99752615a1150289033c4fcdcd321dc
Xingyao Wang <xingyao6@illinois.edu>
2024-04-27 18:53:25 +0800

    attempt to fix lint yet agiain

--- frontend/src/components/Jupyter.tsx
+++ frontend/src/components/Jupyter.tsx
@@ -18,26 +18,21 @@ function JupyterCell({ cell }: IJupyterCell): JSX.Element {
       <div className="rounded-lg bg-gray-800 dark:bg-gray-900 p-2 text-xs">
         <div className="mb-1 text-gray-400">EXECUTE</div>
         <pre
-          className=
-          "scrollbar-custom scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20 overflow-auto px-5"
-
+          className="scrollbar-custom scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20 overflow-auto px-5"
           style={{ padding: 0, marginBottom: 0, fontSize: "0.75rem" }}
         >
           <SyntaxHighlighter language="python" style={atomOneDark}>
             {code}
           </SyntaxHighlighter>
         </pre>
-      </div >
+      </div>
     );
-  }
-  else {
+  } else {
     return (
       <div className="rounded-lg bg-gray-800 dark:bg-gray-900 p-2 text-xs">
         <div className="mb-1 text-gray-400">STDOUT/STDERR</div>
         <pre
-          className=
-          "scrollbar-custom scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20 overflow-auto px-5 max-h-[60vh] bg-gray-800"
-
+          className="scrollbar-custom scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-400 dark:scrollbar-thumb-white/10 dark:hover:scrollbar-thumb-white/20 overflow-auto px-5 max-h-[60vh] bg-gray-800"
           style={{ padding: 0, marginBottom: 0, fontSize: "0.75rem" }}
         >
           {/* split code by newline and render each line as a plaintext, except it starts with `![image]` so we render it as markdown */}
@@ -55,7 +50,9 @@ function JupyterCell({ cell }: IJupyterCell): JSX.Element {
             }
             return (
               <div key={index}>
-                <SyntaxHighlighter language="plaintext" style={atomOneDark}>{line}</SyntaxHighlighter>
+                <SyntaxHighlighter language="plaintext" style={atomOneDark}>
+                  {line}
+                </SyntaxHighlighter>
                 <br />
               </div>
             );
@@ -79,6 +76,5 @@ function Jupyter(): JSX.Element {
     </div>
   );
 }
-
 
 export default Jupyter;
