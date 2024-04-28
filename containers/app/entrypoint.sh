2e6d75963fe8a4aecb45390e1f945ca3c4500d6a
Xingyao Wang <xingyao6@illinois.edu>
2024-04-28 07:02:46 -0500

    make the uvicron app run as host user uid for jupyter plugin

--- containers/app/entrypoint.sh
+++ containers/app/entrypoint.sh
@@ -9,19 +9,13 @@ if [ -z "$SANDBOX_USER_ID" ]; then
   echo "SANDBOX_USER_ID is not set"
   exit 1
 fi
-echo "Creating user opendevin with UID $SANDBOX_USER_ID to run the application"
 
-# add the user to sudoers
-useradd -m -u $SANDBOX_USER_ID opendevin && \
-usermod -aG sudo opendevin && \
-echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+# change uid of opendevin user to match the host user
+# but the group id is not changed, so the user can still access everything under /app
+usermod -u $SANDBOX_USER_ID opendevin
 
 # make docker.sock accessible to the user
 chmod 777 /var/run/docker.sock
 
-# switch to the user
-su - opendevin
-
-# start the application
-cd /app
-uvicorn opendevin.server.listen:app --host 0.0.0.0 --port 3000
+# switch to the user and start the server
+su opendevin -c "cd /app && uvicorn opendevin.server.listen:app --host 0.0.0.0 --port 3000"
