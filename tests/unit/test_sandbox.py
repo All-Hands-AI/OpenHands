a920d0052a6eca054867b46a870c2d924f4875cd
Robert Brennan <contact@rbren.io>
2024-04-30 13:48:35 -0400

    adjust whitespace processing

--- tests/unit/test_sandbox.py
+++ tests/unit/test_sandbox.py
@@ -6,6 +6,7 @@ import pytest
 
 from opendevin import config
 from opendevin.sandbox.docker.ssh_box import DockerSSHBox
+from opendevin.sandbox.docker.exec_box import DockerExecBox
 
 
 @pytest.fixture
@@ -27,29 +28,29 @@ def test_ssh_box_run_as_devin(temp_dir):
         },
         clear=True
     ):
-        ssh_box = DockerSSHBox()
+        for box in [DockerSSHBox(), DockerExecBox()]:
 
-        # test the ssh box
-        exit_code, output = ssh_box.execute('ls -l')
-        assert exit_code == 0, 'The exit code should be 0.'
-        assert output.strip() == 'total 0'
+            # test the ssh box
+            exit_code, output = box.execute('ls -l')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert output.strip() == 'total 0'
 
-        exit_code, output = ssh_box.execute('mkdir test')
-        assert exit_code == 0, 'The exit code should be 0.'
-        assert output.strip() == ''
+            exit_code, output = box.execute('mkdir test')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert output.strip() == ''
 
-        exit_code, output = ssh_box.execute('ls -l')
-        assert exit_code == 0, 'The exit code should be 0.'
-        assert 'opendevin' in output, "The output should contain username 'opendevin'"
-        assert 'test' in output, 'The output should contain the test directory'
+            exit_code, output = box.execute('ls -l')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert 'opendevin' in output, "The output should contain username 'opendevin'"
+            assert 'test' in output, 'The output should contain the test directory'
 
-        exit_code, output = ssh_box.execute('touch test/foo.txt')
-        assert exit_code == 0, 'The exit code should be 0.'
-        assert output.strip() == ''
+            exit_code, output = box.execute('touch test/foo.txt')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert output.strip() == ''
 
-        exit_code, output = ssh_box.execute('ls -l test')
-        assert exit_code == 0, 'The exit code should be 0.'
-        assert 'foo.txt' in output, 'The output should contain the foo.txt file'
+            exit_code, output = box.execute('ls -l test')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert 'foo.txt' in output, 'The output should contain the foo.txt file'
 
 
 def test_ssh_box_multi_line_cmd_run_as_devin(temp_dir):
@@ -69,7 +70,7 @@ def test_ssh_box_multi_line_cmd_run_as_devin(temp_dir):
         exit_code, output = ssh_box.execute('pwd\nls -l')
         assert exit_code == 0, 'The exit code should be 0.'
         expected_lines = ['/workspacels -l', 'total 0']
-        assert output.strip().splitlines() == expected_lines
+        assert output == '\r\n'.join(expected_lines)
 
 def test_ssh_box_stateful_cmd_run_as_devin(temp_dir):
     # get a temporary directory
@@ -113,3 +114,26 @@ def test_ssh_box_failed_cmd_run_as_devin(temp_dir):
         # test the ssh box with a command that fails
         exit_code, output = ssh_box.execute('non_existing_command')
         assert exit_code != 0, 'The exit code should not be 0 for a failed command.'
+
+
+def test_sandbox_whitespace(temp_dir):
+    # get a temporary directory
+    with patch.dict(
+        config.config,
+        {
+            config.ConfigType.WORKSPACE_BASE: temp_dir,
+            config.ConfigType.RUN_AS_DEVIN: 'true',
+            config.ConfigType.SANDBOX_TYPE: 'ssh',
+        },
+        clear=True
+    ):
+        for box in [DockerSSHBox(), DockerExecBox()]:
+
+            # test the ssh box
+            exit_code, output = box.execute('echo -e "\n\n\n"')
+            assert exit_code == 0, 'The exit code should be 0.'
+            if isinstance(box, DockerExecBox):
+                assert output == '\n\n\n', 'The output should be the same as the input.'
+            else:
+                # FIXME: why are there extra '>' characters? Can we remove \r?
+                assert output == '> > > \r\n\r\n\r\n'
