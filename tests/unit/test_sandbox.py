1999706197f6e405af7a517643a1a7988763f8ce
Robert Brennan <contact@rbren.io>
2024-04-30 13:55:01 -0400

    adjust tests

--- tests/unit/test_sandbox.py
+++ tests/unit/test_sandbox.py
@@ -32,25 +32,25 @@ def test_ssh_box_run_as_devin(temp_dir):
 
             # test the ssh box
             exit_code, output = box.execute('ls -l')
-            assert exit_code == 0, 'The exit code should be 0.'
+            assert exit_code == 0, 'The exit code should be 0 for ' + box.__class__.__name__
             assert output.strip() == 'total 0'
 
             exit_code, output = box.execute('mkdir test')
-            assert exit_code == 0, 'The exit code should be 0.'
+            assert exit_code == 0, 'The exit code should be 0 for ' + box.__class__.__name__
             assert output.strip() == ''
 
             exit_code, output = box.execute('ls -l')
             assert exit_code == 0, 'The exit code should be 0.'
-            assert 'opendevin' in output, "The output should contain username 'opendevin'"
-            assert 'test' in output, 'The output should contain the test directory'
+            assert 'opendevin' in output, "The output should contain username 'opendevin' for " + box.__class__.__name__
+            assert 'test' in output, 'The output should contain the test directory for ' + box.__class__.__name__
 
             exit_code, output = box.execute('touch test/foo.txt')
-            assert exit_code == 0, 'The exit code should be 0.'
+            assert exit_code == 0, 'The exit code should be 0. for ' + box.__class__.__name__
             assert output.strip() == ''
 
             exit_code, output = box.execute('ls -l test')
-            assert exit_code == 0, 'The exit code should be 0.'
-            assert 'foo.txt' in output, 'The output should contain the foo.txt file'
+            assert exit_code == 0, 'The exit code should be 0. for ' + box.__class__.__name__
+            assert 'foo.txt' in output, 'The output should contain the foo.txt file for ' + box.__class__.__name__
 
 
 def test_ssh_box_multi_line_cmd_run_as_devin(temp_dir):
@@ -131,9 +131,9 @@ def test_sandbox_whitespace(temp_dir):
 
             # test the ssh box
             exit_code, output = box.execute('echo -e "\n\n\n"')
-            assert exit_code == 0, 'The exit code should be 0.'
+            assert exit_code == 0, 'The exit code should be 0 for ' + box.__class__.__name__
             if isinstance(box, DockerExecBox):
-                assert output == '\n\n\n', 'The output should be the same as the input.'
+                assert output == '\n\n\n', 'The output should be the same as the input for ' + box.__class__.__name__
             else:
                 # FIXME: why are there extra '>' characters? Can we remove \r?
-                assert output == '> > > \r\n\r\n\r\n'
+                assert output == '> \r\n> \r\n> ', 'The output should be the same as the input for ' + box.__class__.__name__
