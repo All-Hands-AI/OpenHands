035aa75b914a42a79128ce65981eb5a95a3485fa
Robert Brennan <contact@rbren.io>
2024-04-30 16:31:44 -0400

    fix imports

--- tests/unit/test_sandbox.py
+++ tests/unit/test_sandbox.py
@@ -5,8 +5,8 @@ from unittest.mock import patch
 import pytest
 
 from opendevin import config
-from opendevin.sandbox.docker.ssh_box import DockerSSHBox
 from opendevin.sandbox.docker.exec_box import DockerExecBox
+from opendevin.sandbox.docker.ssh_box import DockerSSHBox
 
 
 @pytest.fixture
