db893ad1ef191edb73cbc0c6b58aad856ad92ba3
Robert Brennan <contact@rbren.io>
2024-04-30 20:16:23 -0400

    fix NullObs

--- opendevin/events/action/agent.py
+++ opendevin/events/action/agent.py
@@ -89,7 +89,7 @@ class AgentDelegateAction(Action):
 
     async def run(self, controller: 'AgentController') -> 'Observation':
         await controller.start_delegate(self)
-        return NullObservation('')
+        return NullObservation()
 
     @property
     def message(self) -> str:
