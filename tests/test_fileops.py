6b033ece669b441ad4c43ff510b0599d2ffd9deb
Robert Brennan <contact@rbren.io>
2024-04-30 22:01:40 -0400

    fix path prefix

--- tests/test_fileops.py
+++ tests/test_fileops.py
@@ -6,19 +6,20 @@ from opendevin import config
 from opendevin.events.action import files
 from opendevin.schema import ConfigType
 
+SANDBOX_PATH_PREFIX = '/workspace'
 
 def test_resolve_path():
     assert files.resolve_path('test.txt', '/workspace') == Path(config.get(ConfigType.WORKSPACE_BASE)) / 'test.txt'
     assert files.resolve_path('subdir/test.txt', '/workspace') == \
         Path(config.get(ConfigType.WORKSPACE_BASE)) / 'subdir' / 'test.txt'
-    assert files.resolve_path(Path(files.SANDBOX_PATH_PREFIX) / 'test.txt', '/workspace') == \
+    assert files.resolve_path(Path(SANDBOX_PATH_PREFIX) / 'test.txt', '/workspace') == \
         Path(config.get(ConfigType.WORKSPACE_BASE)) / 'test.txt'
-    assert files.resolve_path(Path(files.SANDBOX_PATH_PREFIX) / 'subdir' / 'test.txt',
+    assert files.resolve_path(Path(SANDBOX_PATH_PREFIX) / 'subdir' / 'test.txt',
                                '/workspace') == Path(config.get(ConfigType.WORKSPACE_BASE)) / 'subdir' / 'test.txt'
-    assert files.resolve_path(Path(files.SANDBOX_PATH_PREFIX) / 'subdir' / '..' / 'test.txt',
+    assert files.resolve_path(Path(SANDBOX_PATH_PREFIX) / 'subdir' / '..' / 'test.txt',
                                '/workspace') == Path(config.get(ConfigType.WORKSPACE_BASE)) / 'test.txt'
     with pytest.raises(PermissionError):
-        files.resolve_path(Path(files.SANDBOX_PATH_PREFIX) / '..' / 'test.txt', '/workspace')
+        files.resolve_path(Path(SANDBOX_PATH_PREFIX) / '..' / 'test.txt', '/workspace')
     with pytest.raises(PermissionError):
         files.resolve_path(Path('..') / 'test.txt', '/workspace')
     with pytest.raises(PermissionError):
