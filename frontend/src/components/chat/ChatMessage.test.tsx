d8d6aafee4567da76f86592cd7b2600c80fac839
amanape <stephanpsaras@gmail.com>
2024-05-01 12:22:12 +0300

    rename

--- frontend/src/components/chat/ChatMessage.test.tsx
+++ frontend/src/components/chat/ChatMessage.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import React from "react";
-import ChatMessage from "./Message";
+import ChatMessage from "./ChatMessage";
 
 describe("Message", () => {
   it("should render a user message", () => {
