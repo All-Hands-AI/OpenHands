f19333aa6ec6efe9cd23b345d1d096bc1b36b764
Xingyao Wang <xingyao6@illinois.edu>
2024-05-02 23:34:55 +0800

    fix(controller): "Current task state not recognized" (#1517)
    * fix agent task already running bug
    
    * attemp to fix "Current task state not recognized"
    
    * Revert "attemp to fix "Current task state not recognized""
    
    This reverts commit f5cbfe1ebba3f9ed9d9ee8f47d228e2d0117347a.
    
    * attemp to fix "Current task state not recognized"
    
    * fix invalid state for reset
    
    ---------
    
    Co-authored-by: Leo <ifuryst@gmail.com>

--- opendevin/server/agent/agent.py
+++ opendevin/server/agent/agent.py
@@ -18,7 +18,11 @@ from opendevin.server.session import session_manager
 VALID_TASK_STATE_MAP: Dict[TaskStateAction, List[TaskState]] = {
     TaskStateAction.PAUSE: [TaskState.RUNNING],
     TaskStateAction.RESUME: [TaskState.PAUSED],
-    TaskStateAction.STOP: [TaskState.RUNNING, TaskState.PAUSED],
+    TaskStateAction.STOP: [
+        TaskState.RUNNING,
+        TaskState.PAUSED,
+        TaskState.AWAITING_USER_INPUT,
+    ],
 }
 IGNORED_TASK_STATE_MAP: Dict[TaskStateAction, List[TaskState]] = {
     TaskStateAction.PAUSE: [
@@ -26,12 +30,14 @@ IGNORED_TASK_STATE_MAP: Dict[TaskStateAction, List[TaskState]] = {
         TaskState.PAUSED,
         TaskState.STOPPED,
         TaskState.FINISHED,
+        TaskState.AWAITING_USER_INPUT,
     ],
     TaskStateAction.RESUME: [
         TaskState.INIT,
         TaskState.RUNNING,
         TaskState.STOPPED,
         TaskState.FINISHED,
+        TaskState.AWAITING_USER_INPUT,
     ],
     TaskStateAction.STOP: [TaskState.INIT, TaskState.STOPPED, TaskState.FINISHED],
 }
