08a1674943dcfb48723e6e2b47374fdd5b8763e9
huybery <huybery@gmail.com>
2024-05-03 23:20:15 +0800

    fix the issue where session id might be duplicated

--- opendevin/runtime/docker/exec_box.py
+++ opendevin/runtime/docker/exec_box.py
@@ -61,7 +61,7 @@ class DockerExecBox(Sandbox):
             )
             raise ex
 
-        self.instance_id = sid if sid is not None else str(uuid.uuid4())
+        self.instance_id = sid + str(uuid.uuid4()) if sid is not None else str(uuid.uuid4())
 
         # TODO: this timeout is actually essential - need a better way to set it
         # if it is too short, the container may still waiting for previous
