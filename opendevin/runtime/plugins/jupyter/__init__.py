1d92e9a27bf67b856e71aa9d5c8f18d3919024ab
Robert Brennan <accounts@rbren.io>
2024-05-04 01:08:43 -0400

    Fix user permissions in docker (#1565)
    * permissions mostly working
    
    * fix browser

--- opendevin/runtime/plugins/jupyter/__init__.py
+++ opendevin/runtime/plugins/jupyter/__init__.py
@@ -9,6 +9,6 @@ class JupyterRequirement(PluginRequirement):
     name: str = 'jupyter'
     host_src: str = os.path.dirname(
         os.path.abspath(__file__)
-    )  # The directory of this file (sandbox/plugins/jupyter)
+    )  # The directory of this file (opendevin/runtime/plugins/jupyter)
     sandbox_dest: str = '/opendevin/plugins/jupyter'
     bash_script_path: str = 'setup.sh'
