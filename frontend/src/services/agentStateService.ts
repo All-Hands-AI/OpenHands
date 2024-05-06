d0967122f8fc2ae22ee2dcac2c668725e871efd4
Robert Brennan <accounts@rbren.io>
2024-05-06 08:29:48 -0400

    Minor changes to agent state management (#1597)
    * move towards event stream
    
    * refactor agent state changes
    
    * move agent state logic
    
    * fix callbacks
    
    * break on finish
    
    * closer to working
    
    * change frontend to accomodate new flow
    
    * handle start action
    
    * fix locked stream
    
    * revert message
    
    * logspam
    
    * no async on close
    
    * get rid of agent_task
    
    * fix up closing
    
    * better asyncio handling
    
    * sleep to give back control
    
    * fix key
    
    * logspam
    
    * update frontend agent state actions
    
    * fix pause and cancel
    
    * delint
    
    * fix map
    
    * delint
    
    * wait for agent to finish
    
    * fix unit test
    
    * event stream enums
    
    * fix merge issues
    
    * fix lint
    
    * fix test
    
    * fix test
    
    * add user message action
    
    * add user message action
    
    * fix up user messages
    
    * fix main.py flow
    
    * refactor message waiting
    
    * lint
    
    * fix test
    
    * fix test
    
    * simplify if/else
    
    * fix state reset
    
    * logspam
    
    * add error status
    
    * minor changes to control bar
    
    * handle user messages when not awaiting
    
    * restart agent after stopping
    
    * Update opendevin/controller/agent_controller.py
    
    Co-authored-by: Engel Nyst <enyst@users.noreply.github.com>
    
    * delint
    
    * refactor initialize
    
    * delint
    
    * fix dispatch
    
    ---------
    
    Co-authored-by: Engel Nyst <enyst@users.noreply.github.com>

--- frontend/src/services/agentStateService.ts
+++ frontend/src/services/agentStateService.ts
@@ -1,11 +1,19 @@
 import ActionType from "#/types/ActionType";
 import AgentState from "#/types/AgentState";
 import Socket from "./socket";
+import { initializeAgent } from "./agent";
 
-export function changeAgentState(message: AgentState): void {
+const INIT_DELAY = 1000;
+
+export function changeAgentState(state: AgentState): void {
   const eventString = JSON.stringify({
     action: ActionType.CHANGE_AGENT_STATE,
-    args: { agent_state: message },
+    args: { agent_state: state },
   });
   Socket.send(eventString);
+  if (state === AgentState.STOPPED) {
+    setTimeout(() => {
+      initializeAgent();
+    }, INIT_DELAY);
+  }
 }
