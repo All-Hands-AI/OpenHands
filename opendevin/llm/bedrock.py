44241be15ab8749ae6d8309d5f9120225e2f8a5b
ning.zhao <ning.zhao@jiduauto.com>
2024-05-06 16:53:36 +0800

    fix: - auto set environment variable - add criteria for querying the AWS bedrock model

--- opendevin/llm/bedrock.py
+++ opendevin/llm/bedrock.py
@@ -1,3 +1,5 @@
+import os
+
 import boto3
 
 from opendevin.core import config
@@ -8,9 +10,22 @@ AWS_ACCESS_KEY_ID = config.get(ConfigType.AWS_ACCESS_KEY_ID)
 AWS_SECRET_ACCESS_KEY = config.get(ConfigType.AWS_SECRET_ACCESS_KEY)
 AWS_REGION_NAME = config.get(ConfigType.AWS_REGION_NAME)
 
+# It needs to be set as an environment variable, if the variable is configured in the Config file.
+os.environ[ConfigType.AWS_ACCESS_KEY_ID] = AWS_ACCESS_KEY_ID
+os.environ[ConfigType.AWS_SECRET_ACCESS_KEY] = AWS_SECRET_ACCESS_KEY
+os.environ[ConfigType.AWS_REGION_NAME] = AWS_REGION_NAME
+
 
 def list_foundation_models():
     try:
+        # The AWS bedrock model id is not queried, if no AWS parameters are configured.
+        if (
+            AWS_REGION_NAME is None
+            or AWS_ACCESS_KEY_ID is None
+            or AWS_SECRET_ACCESS_KEY is None
+        ):
+            return []
+
         client = boto3.client(
             service_name='bedrock',
             region_name=AWS_REGION_NAME,
@@ -28,6 +43,7 @@ def list_foundation_models():
             ' if you want use bedrock model.',
             err,
         )
+        return []
 
 
 def remove_error_modelId(model_list):
