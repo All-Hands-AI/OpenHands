660a6caf773dd1e56fb0d71b075ee0c8cfb8e3e5
Graham Neubig <neubig@gmail.com>
2024-05-13 14:33:26 -0400

    Update regenerate.sh

--- tests/integration/regenerate.sh
+++ tests/integration/regenerate.sh
@@ -59,13 +59,8 @@ for ((i = 0; i < num_of_tests; i++)); do
       sleep 1
       rm -rf logs
       rm -rf tests/integration/mock/$agent/$test_name/*
-      echo "echo -e \"/exit\n\" | SANDBOX_TYPE=$SANDBOX_TYPE WORKSPACE_BASE=$WORKSPACE_BASE \
-        DEBUG=true REMIND_ITERATIONS=$remind_iterations \
-        WORKSPACE_MOUNT_PATH=$WORKSPACE_MOUNT_PATH AGENT=$agent \
-        poetry run python ./opendevin/core/main.py \
-        -i $MAX_ITERATIONS \
-        -t \"$task Do not ask me for confirmation at any point.\" \
-        -c $agent"
+      # set -x to print the command being executed
+      set -x
       echo -e "/exit\n" | SANDBOX_TYPE=$SANDBOX_TYPE WORKSPACE_BASE=$WORKSPACE_BASE \
         DEBUG=true REMIND_ITERATIONS=$remind_iterations \
         WORKSPACE_MOUNT_PATH=$WORKSPACE_MOUNT_PATH AGENT=$agent \
@@ -73,6 +68,7 @@ for ((i = 0; i < num_of_tests; i++)); do
         -i $MAX_ITERATIONS \
         -t "$task Do not ask me for confirmation at any point." \
         -c $agent
+      set +x
 
       mkdir -p tests/integration/mock/$agent/$test_name/
       mv logs/llm/**/* tests/integration/mock/$agent/$test_name/
