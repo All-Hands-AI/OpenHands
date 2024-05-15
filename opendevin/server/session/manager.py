e9732377982edb377d56245169ac6cbb9f26c7b8
Robert Brennan <contact@rbren.io>
2024-05-15 14:59:41 -0400

    remove session saving

--- opendevin/server/session/manager.py
+++ opendevin/server/session/manager.py
@@ -1,7 +1,4 @@
 import asyncio
-import atexit
-import json
-import os
 import time
 
 from fastapi import WebSocket
@@ -10,9 +7,6 @@ from opendevin.core.logger import opendevin_logger as logger
 
 from .session import Session
 
-CACHE_DIR = os.getenv('CACHE_DIR', 'cache')
-SESSION_CACHE_FILE = os.path.join(CACHE_DIR, 'sessions.json')
-
 
 class SessionManager:
     _sessions: dict[str, Session] = {}
@@ -20,8 +14,6 @@ class SessionManager:
     session_timeout: int = 600
 
     def __init__(self):
-        self._load_sessions()
-        atexit.register(self.close)
         asyncio.create_task(self._cleanup_sessions())
 
     def add_session(self, sid: str, ws_conn: WebSocket) -> Session:
@@ -36,10 +28,6 @@ class SessionManager:
             return None
         return self._sessions.get(sid)
 
-    def close(self):
-        logger.info('Saving sessions...')
-        self._save_sessions()
-
     async def send(self, sid: str, data: dict[str, object]) -> bool:
         """Sends data to the client."""
         if sid not in self._sessions:
@@ -53,33 +41,6 @@ class SessionManager:
     async def send_message(self, sid: str, message: str) -> bool:
         """Sends a message to the client."""
         return await self.send(sid, {'message': message})
-
-    def _save_sessions(self):
-        data = {}
-        for sid, conn in self._sessions.items():
-            data[sid] = {
-                'sid': conn.sid,
-                'last_active_ts': conn.last_active_ts,
-                'is_alive': conn.is_alive,
-            }
-        if not os.path.exists(CACHE_DIR):
-            os.makedirs(CACHE_DIR)
-        with open(SESSION_CACHE_FILE, 'w+') as file:
-            json.dump(data, file)
-
-    def _load_sessions(self):
-        try:
-            with open(SESSION_CACHE_FILE, 'r') as file:
-                data = json.load(file)
-                for sid, sdata in data.items():
-                    conn = Session(sid, None)
-                    ok = conn.load_from_data(sdata)
-                    if ok:
-                        self._sessions[sid] = conn
-        except FileNotFoundError:
-            pass
-        except json.decoder.JSONDecodeError:
-            pass
 
     async def _cleanup_sessions(self):
         while True:
