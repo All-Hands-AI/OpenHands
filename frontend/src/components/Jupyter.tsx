30178769d8391a81f9bfe3edaac964fe2e37efdd
Shimada666 <649940882@qq.com>
2024-05-15 10:46:59 +0800

    Update Jupyter.tsx

--- frontend/src/components/Jupyter.tsx
+++ frontend/src/components/Jupyter.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import Markdown from "react-markdown";
