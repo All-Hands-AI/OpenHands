a3732d980c2d9d714d1d1cf16444182cb0b49e19
Robert Brennan <contact@rbren.io>
2024-05-16 11:36:35 -0400

    fix user message rehydration

--- frontend/src/services/actions.ts
+++ frontend/src/services/actions.ts
@@ -1,5 +1,5 @@
 import { setScreenshotSrc, setUrl } from "#/state/browserSlice";
-import { addAssistantMessage } from "#/state/chatSlice";
+import { addAssistantMessage, addUserMessage } from "#/state/chatSlice";
 import { setCode, setActiveFilepath } from "#/state/codeSlice";
 import { appendInput } from "#/state/commandSlice";
 import { appendJupyterInput } from "#/state/jupyterSlice";
@@ -28,7 +28,11 @@ const messageActions = {
     store.dispatch(setCode(content));
   },
   [ActionType.MESSAGE]: (message: ActionMessage) => {
-    store.dispatch(addAssistantMessage(message.args.content));
+    if (message.source === "user") {
+      store.dispatch(addUserMessage(message.args.content));
+    } else {
+      store.dispatch(addAssistantMessage(message.args.content));
+    }
   },
   [ActionType.FINISH]: (message: ActionMessage) => {
     store.dispatch(addAssistantMessage(message.message));
