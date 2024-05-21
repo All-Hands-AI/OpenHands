ac22d83cda698bd19e8e2dfe680e6ff8226a9a28
yufansong <yufan@risingwave-labs.com>
2024-05-21 22:26:25 +0800

    make lint

--- opendevin/core/metrics.py
+++ opendevin/core/metrics.py
@@ -1,12 +1,12 @@
-
-class Metrics():
+class Metrics:
     """
     Metrics class can record various metrics during running and evaluation.
     Currently we definte the following metrics:
         total_cost: the total cost of the current LLM.
     """
+
     def __init__(self) -> None:
-        self._total_cost = 0
+        self._total_cost = 0.0
         self.reset()
 
     @property
@@ -16,14 +16,14 @@ class Metrics():
     @total_cost.setter
     def total_cost(self, value: float) -> None:
         if value < 0:
-            raise ValueError("Total cost cannot be negative.")
+            raise ValueError('Total cost cannot be negative.')
         self._total_cost = value
-    
+
     def add_cost(self, value: float) -> None:
         if value < 0:
-            raise ValueError("Added cost cannot be negative.")
+            raise ValueError('Added cost cannot be negative.')
         self._total_cost += value
-    
+
     def reset(self):
         """
         Reset all metrics to zero value.
@@ -34,17 +34,14 @@ class Metrics():
         """
         Return the metrics in a dictionary.
         """
-        return {
-            "total_cost": self._total_cost
-        }
-    
+        return {'total_cost': self._total_cost}
+
     def log(self):
         """
         Log the metrics.
         """
         metrics = self.get()
-        logs = ""
+        logs = ''
         for key, value in metrics.items():
-            logs += f"{key}: {value}\n"
+            logs += f'{key}: {value}\n'
         return logs
-    
