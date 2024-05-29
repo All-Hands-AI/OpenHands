fdce0ce80301af6f916c7bed81f19134f4154dcb
Boxuan Li <liboxuan@connect.hku.hk>
2024-05-28 22:39:32 -0700

    Fix prompt regression in jupyter plugin

--- opendevin/runtime/plugins/jupyter/execute_cli.py
+++ opendevin/runtime/plugins/jupyter/execute_cli.py
@@ -8,7 +8,7 @@ import requests
 code = sys.stdin.read()
 
 
-def execute_code(code):
+def execute_code(code, print_output=True):
     PORT = os.environ.get('JUPYTER_EXEC_SERVER_PORT')
     POST_URL = f'http://localhost:{PORT}/execute'
 
@@ -21,7 +21,8 @@ def execute_code(code):
                 POST_URL, json={'kernel_id': kernel_id, 'code': code}
             )
             if '500: Internal Server Error' not in response.text:
-                print(response.text)
+                if print_output:
+                    print(response.text)
                 break
         except requests.exceptions.ConnectionError:
             pass
@@ -31,6 +32,8 @@ def execute_code(code):
 
 
 if jupyter_pwd := os.environ.get('JUPYTER_PWD'):
-    execute_code(f'import os\nos.environ["JUPYTER_PWD"] = "{jupyter_pwd}"\n')
+    execute_code(
+        f'import os\nos.environ["JUPYTER_PWD"] = "{jupyter_pwd}"\n', print_output=False
+    )
 
 execute_code(code)
