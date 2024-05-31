feb5d565fb30a7ee9982bffe4184aa8d6cc51d1f
Xingyao Wang <xingyao6@illinois.edu>
2024-05-31 17:57:47 +0800

    update the branch of git repo

--- evaluation/swe_bench/scripts/docker/pull_all_eval_docker.sh
+++ evaluation/swe_bench/scripts/docker/pull_all_eval_docker.sh
@@ -2,6 +2,6 @@
 
 mkdir evaluation/swe_bench/eval_workspace
 pushd evaluation/swe_bench/eval_workspace
-git clone https://github.com/OpenDevin/SWE-bench-docker.git
+git clone -b xw/od-fix https://github.com/OpenDevin/SWE-bench-docker.git
 cd SWE-bench-docker
 scripts/pull_docker_images.sh docker/ aorwall
