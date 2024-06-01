0208cef9681d7bd0a44af3c89b7349ab847ac322
Rahul Anand <62982824+zeul22@users.noreply.github.com>
2024-06-01 07:24:45 +0530

    Update docs/src/components/HomepageHeader/HomepageHeader.tsx
    Co-authored-by: Graham Neubig <neubig@gmail.com>

--- docs/src/components/HomepageHeader/HomepageHeader.tsx
+++ docs/src/components/HomepageHeader/HomepageHeader.tsx
@@ -1,37 +1,3 @@
-// import Link from "@docusaurus/Link";
-// import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-// import Heading from "@theme/Heading";
-// import { Demo } from "../Demo/Demo";
-// import styles from "./index.module.css";
-
-// export function HomepageHeader() {
-//   const { siteConfig } = useDocusaurusContext();
-//   return (
-//     <div className="h-screen text-white bg-gradient-to-t from-slate-600 to-black">
-//     {/* <div className={styles.headerContainer}> */}
-//       <div className={` flex flex-col gap-2
-//       items-center p-6 font-light w-full`}>
-//         <Heading as="h1" className="
-//         text-5xl
-//         ">
-//           {/* hero__title  */}
-//           {siteConfig.title}
-//         </Heading>
-//         <p className="hero__subtitle ">{siteConfig.tagline}</p>
-//         <div className={`${styles.buttons}`}>
-//           <Link
-//             className="button button--secondary button--lg"
-//             to="/modules/usage/intro"
-//           >
-//             Get Started
-//           </Link>
-//         </div>
-//       <Demo />
-//       </div>
-//     </div>
-//   );
-// }
-
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Heading from "@theme/Heading";
