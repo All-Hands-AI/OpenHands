0f50428158ba81a206e485a76f77b319d46460f2
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-06-01 13:46:18 +0530

    refactored source bashrc logic

--- opendevin/core/main.py
+++ opendevin/core/main.py
@@ -91,12 +91,7 @@ async def main(
         event_stream=event_stream,
     )
     runtime = ServerRuntime(event_stream=event_stream, sandbox=sandbox)
-
-    if runtime.sandbox.is_initial_session:
-        logger.info('Initializing plugins in the sandbox')
-        runtime.init_sandbox_plugins(controller.agent.sandbox_plugins)
-    else:
-        logger.info('Plugins are already initialized in the sandbox')
+    runtime.init_sandbox_plugins(controller.agent.sandbox_plugins)
     runtime.init_runtime_tools(controller.agent.runtime_tools, is_async=False)
 
     await event_stream.add_event(MessageAction(content=task), EventSource.USER)
