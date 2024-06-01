a965315ef9244fb19ab816630352f00f3cb58d2b
Robert Brennan <contact@rbren.io>
2024-06-01 10:38:09 -0400

    fix imports

--- opendevin/runtime/__init__.py
+++ opendevin/runtime/__init__.py
@@ -1,7 +1,7 @@
-from .docker.exec_box import DockerExecBox
-from .docker.local_box import LocalBox
-from .docker.ssh_box import DockerSSHBox
 from .e2b.sandbox import E2BBox
 from .sandbox import Sandbox
+from .sandbox.docker.exec_box import DockerExecBox
+from .sandbox.docker.local_box import LocalBox
+from .sandbox.docker.ssh_box import DockerSSHBox
 
 __all__ = ['Sandbox', 'DockerSSHBox', 'DockerExecBox', 'E2BBox', 'LocalBox']
