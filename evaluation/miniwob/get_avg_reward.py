f1e1dfd7dab019ec4b2fc84ae27d0abeab35e6b0
Frank Xu <frankxu2004@gmail.com>
2024-06-04 06:18:24 -0400

    add avg cost calculation

--- evaluation/miniwob/get_avg_reward.py
+++ evaluation/miniwob/get_avg_reward.py
@@ -16,10 +16,18 @@ if __name__ == '__main__':
     total_num = len(env_ids)
     print('Total number of tasks: ', total_num)
     total_reward = 0
+    total_cost = 0
+    actual_num = 0
     with open(args.output_path, 'r') as f:
         for line in f:
             data = json.loads(line)
+            actual_num += 1
+            total_cost += data['metrics']['accumulated_cost']
             total_reward += data['test_result']
 
     avg_reward = total_reward / total_num
     print('Avg Reward: ', avg_reward)
+
+    avg_cost = total_cost / actual_num
+    print('Avg Cost: ', avg_cost)
+    print('Actual number of tasks finished: ', actual_num)
