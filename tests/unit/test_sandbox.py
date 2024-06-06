8271bec849b11fdb9a58407219bfc66e63d1b2ef
tobitege <tobitege@gmx.de>
2024-06-06 05:20:41 +0200

    removed try/finally

--- tests/unit/test_sandbox.py
+++ tests/unit/test_sandbox.py
@@ -98,38 +98,36 @@ def test_ssh_box_run_as_devin(temp_dir):
         for box in [
             DockerSSHBox()
         ]:  # FIXME: permission error on mkdir test for exec box
-            try:
-                exit_code, output = box.execute('ls -l')
-                assert exit_code == 0, (
-                    'The exit code should be 0 for ' + box.__class__.__name__
-                )
-                assert output.strip() == 'total 0'
+            exit_code, output = box.execute('ls -l')
+            assert exit_code == 0, (
+                'The exit code should be 0 for ' + box.__class__.__name__
+            )
+            assert output.strip() == 'total 0'
 
-                assert config.workspace_base == temp_dir
-                exit_code, output = box.execute('ls -l')
-                assert exit_code == 0, 'The exit code should be 0.'
-                assert output.strip() == 'total 0'
+            assert config.workspace_base == temp_dir
+            exit_code, output = box.execute('ls -l')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert output.strip() == 'total 0'
 
-                exit_code, output = box.execute('mkdir test')
-                assert exit_code == 0, 'The exit code should be 0.'
-                assert output.strip() == ''
+            exit_code, output = box.execute('mkdir test')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert output.strip() == ''
 
-                exit_code, output = box.execute('ls -l')
-                assert exit_code == 0, 'The exit code should be 0.'
-                assert (
-                    'opendevin' in output
-                ), "The output should contain username 'opendevin'"
-                assert 'test' in output, 'The output should contain the test directory'
+            exit_code, output = box.execute('ls -l')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert (
+                'opendevin' in output
+            ), "The output should contain username 'opendevin'"
+            assert 'test' in output, 'The output should contain the test directory'
 
-                exit_code, output = box.execute('touch test/foo.txt')
-                assert exit_code == 0, 'The exit code should be 0.'
-                assert output.strip() == ''
+            exit_code, output = box.execute('touch test/foo.txt')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert output.strip() == ''
 
-                exit_code, output = box.execute('ls -l test')
-                assert exit_code == 0, 'The exit code should be 0.'
-                assert 'foo.txt' in output, 'The output should contain the foo.txt file'
-            finally:
-                box.close()
+            exit_code, output = box.execute('ls -l test')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert 'foo.txt' in output, 'The output should contain the foo.txt file'
+            box.close()
 
 
 def test_ssh_box_multi_line_cmd_run_as_devin(temp_dir):
@@ -140,19 +138,17 @@ def test_ssh_box_multi_line_cmd_run_as_devin(temp_dir):
         config, 'sandbox_type', new='ssh'
     ):
         for box in [DockerSSHBox(), DockerExecBox()]:
-            try:
-                exit_code, output = box.execute('pwd && ls -l')
-                assert exit_code == 0, (
-                    'The exit code should be 0 for ' + box.__class__.__name__
-                )
-                expected_lines = ['/workspace', 'total 0']
-                line_sep = '\r\n' if isinstance(box, DockerSSHBox) else '\n'
-                assert output == line_sep.join(expected_lines), (
-                    'The output should be the same as the input for '
-                    + box.__class__.__name__
-                )
-            finally:
-                box.close()
+            exit_code, output = box.execute('pwd && ls -l')
+            assert exit_code == 0, (
+                'The exit code should be 0 for ' + box.__class__.__name__
+            )
+            expected_lines = ['/workspace', 'total 0']
+            line_sep = '\r\n' if isinstance(box, DockerSSHBox) else '\n'
+            assert output == line_sep.join(expected_lines), (
+                'The output should be the same as the input for '
+                + box.__class__.__name__
+            )
+            box.close()
 
 
 def test_ssh_box_stateful_cmd_run_as_devin(temp_dir):
@@ -165,28 +161,26 @@ def test_ssh_box_stateful_cmd_run_as_devin(temp_dir):
         for box in [
             DockerSSHBox()
         ]:  # FIXME: DockerExecBox() does not work with stateful commands
-            try:
-                exit_code, output = box.execute('mkdir test')
-                assert exit_code == 0, 'The exit code should be 0.'
-                assert output.strip() == ''
+            exit_code, output = box.execute('mkdir test')
+            assert exit_code == 0, 'The exit code should be 0.'
+            assert output.strip() == ''
 
-                exit_code, output = box.execute('cd test')
-                assert exit_code == 0, (
-                    'The exit code should be 0 for ' + box.__class__.__name__
-                )
-                assert output.strip() == '', (
-                    'The output should be empty for ' + box.__class__.__name__
-                )
+            exit_code, output = box.execute('cd test')
+            assert exit_code == 0, (
+                'The exit code should be 0 for ' + box.__class__.__name__
+            )
+            assert output.strip() == '', (
+                'The output should be empty for ' + box.__class__.__name__
+            )
 
-                exit_code, output = box.execute('pwd')
-                assert exit_code == 0, (
-                    'The exit code should be 0 for ' + box.__class__.__name__
-                )
-                assert output.strip() == '/workspace/test', (
-                    'The output should be /workspace for ' + box.__class__.__name__
-                )
-            finally:
-                box.close()
+            exit_code, output = box.execute('pwd')
+            assert exit_code == 0, (
+                'The exit code should be 0 for ' + box.__class__.__name__
+            )
+            assert output.strip() == '/workspace/test', (
+                'The output should be /workspace for ' + box.__class__.__name__
+            )
+            box.close()
 
 
 def test_ssh_box_failed_cmd_run_as_devin(temp_dir):
@@ -197,14 +191,12 @@ def test_ssh_box_failed_cmd_run_as_devin(temp_dir):
         config, 'sandbox_type', new='ssh'
     ):
         for box in [DockerSSHBox(), DockerExecBox()]:
-            try:
-                exit_code, output = box.execute('non_existing_command')
-                assert exit_code != 0, (
-                    'The exit code should not be 0 for a failed command for '
-                    + box.__class__.__name__
-                )
-            finally:
-                box.close()
+            exit_code, output = box.execute('non_existing_command')
+            assert exit_code != 0, (
+                'The exit code should not be 0 for a failed command for '
+                + box.__class__.__name__
+            )
+            box.close()
 
 
 def test_single_multiline_command(temp_dir):
@@ -214,24 +206,22 @@ def test_single_multiline_command(temp_dir):
         config, 'sandbox_type', new='ssh'
     ):
         for box in [DockerSSHBox(), DockerExecBox()]:
-            try:
-                exit_code, output = box.execute('echo \\\n -e "foo"')
-                assert exit_code == 0, (
-                    'The exit code should be 0 for ' + box.__class__.__name__
+            exit_code, output = box.execute('echo \\\n -e "foo"')
+            assert exit_code == 0, (
+                'The exit code should be 0 for ' + box.__class__.__name__
+            )
+            if isinstance(box, DockerExecBox):
+                assert output == 'foo', (
+                    'The output should be the same as the input for '
+                    + box.__class__.__name__
                 )
-                if isinstance(box, DockerExecBox):
-                    assert output == 'foo', (
-                        'The output should be the same as the input for '
-                        + box.__class__.__name__
-                    )
-                else:
-                    # FIXME: why is there a `>` in the output? Probably PS2?
-                    assert output == '> foo', (
-                        'The output should be the same as the input for '
-                        + box.__class__.__name__
-                    )
-            finally:
-                box.close()
+            else:
+                # FIXME: why is there a `>` in the output? Probably PS2?
+                assert output == '> foo', (
+                    'The output should be the same as the input for '
+                    + box.__class__.__name__
+                )
+            box.close()
 
 
 def test_multiline_echo(temp_dir):
@@ -241,24 +231,22 @@ def test_multiline_echo(temp_dir):
         config, 'sandbox_type', new='ssh'
     ):
         for box in [DockerSSHBox(), DockerExecBox()]:
-            try:
-                exit_code, output = box.execute('echo -e "hello\nworld"')
-                assert exit_code == 0, (
-                    'The exit code should be 0 for ' + box.__class__.__name__
+            exit_code, output = box.execute('echo -e "hello\nworld"')
+            assert exit_code == 0, (
+                'The exit code should be 0 for ' + box.__class__.__name__
+            )
+            if isinstance(box, DockerExecBox):
+                assert output == 'hello\nworld', (
+                    'The output should be the same as the input for '
+                    + box.__class__.__name__
                 )
-                if isinstance(box, DockerExecBox):
-                    assert output == 'hello\nworld', (
-                        'The output should be the same as the input for '
-                        + box.__class__.__name__
-                    )
-                else:
-                    # FIXME: why is there a `>` in the output?
-                    assert output == '> hello\r\nworld', (
-                        'The output should be the same as the input for '
-                        + box.__class__.__name__
-                    )
-            finally:
-                box.close()
+            else:
+                # FIXME: why is there a `>` in the output?
+                assert output == '> hello\r\nworld', (
+                    'The output should be the same as the input for '
+                    + box.__class__.__name__
+                )
+            box.close()
 
 
 def test_sandbox_whitespace(temp_dir):
@@ -269,24 +257,21 @@ def test_sandbox_whitespace(temp_dir):
         config, 'sandbox_type', new='ssh'
     ):
         for box in [DockerSSHBox(), DockerExecBox()]:
-            try:
-                # test the ssh box
-                exit_code, output = box.execute('echo -e "\\n\\n\\n"')
-                assert exit_code == 0, (
-                    'The exit code should be 0 for ' + box.__class__.__name__
+            exit_code, output = box.execute('echo -e "\\n\\n\\n"')
+            assert exit_code == 0, (
+                'The exit code should be 0 for ' + box.__class__.__name__
+            )
+            if isinstance(box, DockerExecBox):
+                assert output == '\n\n\n', (
+                    'The output should be the same as the input for '
+                    + box.__class__.__name__
                 )
-                if isinstance(box, DockerExecBox):
-                    assert output == '\n\n\n', (
-                        'The output should be the same as the input for '
-                        + box.__class__.__name__
-                    )
-                else:
-                    assert output == '\r\n\r\n\r\n', (
-                        'The output should be the same as the input for '
-                        + box.__class__.__name__
-                    )
-            finally:
-                box.close()
+            else:
+                assert output == '\r\n\r\n\r\n', (
+                    'The output should be the same as the input for '
+                    + box.__class__.__name__
+                )
+            box.close()
 
 
 def test_sandbox_jupyter_plugin(temp_dir):
@@ -297,18 +282,14 @@ def test_sandbox_jupyter_plugin(temp_dir):
         config, 'sandbox_type', new='ssh'
     ):
         for box in [DockerSSHBox()]:
-            try:
-                box.init_plugins([JupyterRequirement])
-
-                # test the ssh box
-                exit_code, output = box.execute('echo "print(1)" | execute_cli')
-                print(output)
-                assert exit_code == 0, (
-                    'The exit code should be 0 for ' + box.__class__.__name__
-                )
-                assert output == '1\r\n', (
-                    'The output should be the same as the input for '
-                    + box.__class__.__name__
-                )
-            finally:
-                box.close()
+            box.init_plugins([JupyterRequirement])
+            exit_code, output = box.execute('echo "print(1)" | execute_cli')
+            print(output)
+            assert exit_code == 0, (
+                'The exit code should be 0 for ' + box.__class__.__name__
+            )
+            assert output == '1\r\n', (
+                'The output should be the same as the input for '
+                + box.__class__.__name__
+            )
+            box.close()
