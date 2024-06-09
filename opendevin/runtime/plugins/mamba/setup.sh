308bd609aadb12cbefbb11f15a81fb38c2f953e2
Shimada666 <649940882@qq.com>
2024-06-09 14:44:40 +0800

    update mamba setup shell

--- opendevin/runtime/plugins/mamba/setup.sh
+++ opendevin/runtime/plugins/mamba/setup.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ ! -d "/opendevin/miniforge3" ]; then
-#    wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+    wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
     mkdir -p /opendevin
-    bash /workspace/Miniforge3-$(uname)-$(uname -m).sh -b -p /opendevin/miniforge3
+    bash Miniforge3-$(uname)-$(uname -m).sh -b -p /opendevin/miniforge3
 #    /opendevin/miniforge3/bin/mamba init bash
 fi
 bash -c ". /opendevin/miniforge3/etc/profile.d/conda.sh && conda config --set changeps1 False && conda config --append channels conda-forge"
