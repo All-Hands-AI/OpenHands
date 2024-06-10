0e024b3b244dc29bd8a9fba9ec4b6d393e889458
Shimada666 <649940882@qq.com>
2024-06-10 11:03:19 +0800

    remove export

--- opendevin/runtime/plugins/agent_skills/setup.sh
+++ opendevin/runtime/plugins/agent_skills/setup.sh
@@ -4,11 +4,9 @@ set -e
 
 # add agent_skills to PATH
 echo 'export PATH=/opendevin/plugins/agent_skills:$PATH' >> ~/.bashrc
-export PATH=/opendevin/plugins/agent_skills:$PATH
 
 # add agent_skills to PYTHONPATH
 echo 'export PYTHONPATH=/opendevin/plugins/agent_skills:$PYTHONPATH' >> ~/.bashrc
-export PYTHONPATH=/opendevin/plugins/agent_skills:$PYTHONPATH
 
 source ~/.bashrc
 
