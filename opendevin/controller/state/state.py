8a6c66b1e6abff1ed2699ae5f8637cc5e1d50873
yufansong <yufan@risingwave-labs.com>
2024-06-14 11:02:52 +0800

    fix bug

--- opendevin/controller/state/state.py
+++ opendevin/controller/state/state.py
@@ -30,7 +30,7 @@ class State:
     iteration: int = 0
     max_iterations: int = 100
     max_iterations_per_task: int = 100
-    global_iteration = 0
+    global_iteration: int = 0
     # number of characters we have sent to and received from LLM so far for current task
     num_of_chars: int = 0
     background_commands_obs: list[CmdOutputObservation] = field(default_factory=list)
