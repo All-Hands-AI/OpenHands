97a9193c0231c842374b71863902535cf96297a7
Haofei Yu <haofeiyu@Haofeis-MacBook-Pro.local>
2024-06-17 04:05:22 -0400

    confirm litellm is working

--- litellm_test.py
+++ litellm_test.py
@@ -2,18 +2,13 @@ import litellm
 
 litellm.set_verbose = True
 
-headers = {
-    'Content-Type': 'application/json',
-}
-
 response = litellm.completion(
-    model='openai/./Qwen2-7B-Instruct',
+    model='openai/Qwen2-72B-Instruct',
     messages=[
         {'role': 'system', 'content': 'You are a helpful assistant.'},
         {'role': 'user', 'content': 'Who won the world series in 2020?'},
     ],
-    base_url='http://cccxc710.pok.ibm.com:8000/v1/',
-    headers=headers,
+    base_url='http://cccxc708.pok.ibm.com:8000/v1',
+    api_key='fake',
 )
-
 print(response)
