8dc5afea29f5012e3bb3ecd1650fcec6f8af528b
mingkaid <dengmingkai@gmail.com>
2024-06-17 11:07:54 -0700

    Add agent and API key input to frontend

--- my_frontend.py
+++ my_frontend.py
@@ -169,7 +169,15 @@ def get_status(agent_state):
     return status
 
 
-def get_messages(chat_history, action_messages, browser_history, session, status):
+def get_messages(
+    chat_history,
+    action_messages,
+    browser_history,
+    session,
+    status,
+    agent_selection,
+    api_key,
+):
     print('Get Messages', session.agent_state)
     if len(chat_history) > 0:
         if chat_history[-1][1] is None:
@@ -202,6 +210,9 @@ def get_messages(chat_history, action_messages, browser_history, session, status
     else:
         clear = gr.Button('Clear', interactive=False)
         if session.agent_state not in ['init', 'running', 'pausing', 'resuming']:
+            session.agent = agent_selection
+            print('API Key:', api_key)
+            session.api_key = api_key if len(api_key) > 0 else 'test'
             action_messages = []
             browser_history = browser_history[:1]
             for agent_state in session.initialize(as_generator=True):
@@ -274,65 +285,69 @@ def pause_resume_task(is_paused, session, status):
     return button, is_paused, session, status
 
 
-with gr.Blocks() as demo:
-    with gr.Row(equal_height=True):
-        with gr.Column(scale=1):
-            title = gr.Markdown('# FastAgent')
-            chatbot = gr.Chatbot()
-            with gr.Group():
-                with gr.Row():
-                    msg = gr.Textbox(container=False, show_label=False, scale=7)
-                    submit = gr.Button(
-                        'Submit',
-                        variant='primary',
-                        scale=1,
-                        min_width=150,
+if __name__ == '__main__':
+    default_port = 3000
+    default_agent = 'DummyWebAgent'
+
+    with gr.Blocks() as demo:
+        title = gr.Markdown('# FastAgent')
+        with gr.Row(equal_height=True):
+            with gr.Column(scale=1):
+                with gr.Group():
+                    agent_selection = gr.Dropdown(
+                        ['DummyWebAgent', 'WorldModelAgent'],
+                        value=default_agent,
+                        interactive=True,
+                        label='Agent',
+                        info='Choose your own adventure partner!',
                     )
-                    submit_triggers = [msg.submit, submit.click]
-            with gr.Row():
-                pause_resume = gr.Button('Pause')
-                clear = gr.Button('Clear')
-            status = gr.Markdown('Agent Status: 🔴 Inactive')
+                    api_key = gr.Textbox(label='API Key', placeholder='Your API Key')
+                    chatbot = gr.Chatbot()
+                with gr.Group():
+                    with gr.Row():
+                        msg = gr.Textbox(container=False, show_label=False, scale=7)
+                        submit = gr.Button(
+                            'Submit',
+                            variant='primary',
+                            scale=1,
+                            min_width=150,
+                        )
+                        submit_triggers = [msg.submit, submit.click]
+                with gr.Row():
+                    pause_resume = gr.Button('Pause')
+                    clear = gr.Button('Clear')
 
-        with gr.Column(scale=2):
-            with gr.Group():
-                start_url = 'about:blank'
-                url = gr.Textbox(start_url, label='URL', interactive=False, max_lines=1)
-                blank = Image.new('RGB', (1280, 720), (255, 255, 255))
-                screenshot = gr.Image(blank, interactive=False, label='Webpage')
+                status = gr.Markdown('Agent Status: 🔴 Inactive')
 
-    action_messages = gr.State([])
-    browser_history = gr.State([(blank, start_url)])
-    session = gr.State(OpenDevinSession(agent='DummyWebAgent', port=3000))
-    # session = gr.State(OpenDevinSession(agent='WorldModelAgent', port=3000))
-    is_paused = gr.State(False)
-    # chat_msg = msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False)
-    chat_msg = gr.events.on(
-        submit_triggers, user, [msg, chatbot], [msg, chatbot], queue=False
-    )
-    bot_msg = chat_msg.then(
-        get_messages,
-        [chatbot, action_messages, browser_history, session, status],
-        [
-            chatbot,
-            screenshot,
-            url,
-            action_messages,
-            browser_history,
-            session,
-            status,
-            clear,
-        ],
-    )
-    (
-        pause_resume.click(
-            pause_resume_task,
-            [is_paused, session, status],
-            [pause_resume, is_paused, session, status],
-            queue=False,
-        ).then(
+            with gr.Column(scale=2):
+                with gr.Group():
+                    start_url = 'about:blank'
+                    url = gr.Textbox(
+                        start_url, label='URL', interactive=False, max_lines=1
+                    )
+                    blank = Image.new('RGB', (1280, 720), (255, 255, 255))
+                    screenshot = gr.Image(blank, interactive=False, label='Webpage')
+
+        action_messages = gr.State([])
+        browser_history = gr.State([(blank, start_url)])
+        session = gr.State(OpenDevinSession(agent=default_agent, port=default_port))
+        # session = gr.State(OpenDevinSession(agent='WorldModelAgent', port=3000))
+        is_paused = gr.State(False)
+        # chat_msg = msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False)
+        chat_msg = gr.events.on(
+            submit_triggers, user, [msg, chatbot], [msg, chatbot], queue=False
+        )
+        bot_msg = chat_msg.then(
             get_messages,
-            [chatbot, action_messages, browser_history, session, status],
+            [
+                chatbot,
+                action_messages,
+                browser_history,
+                session,
+                status,
+                agent_selection,
+                api_key,
+            ],
             [
                 chatbot,
                 screenshot,
@@ -344,23 +359,51 @@ with gr.Blocks() as demo:
                 clear,
             ],
         )
-    )
-    clear.click(
-        clear_page,
-        [browser_history, session],
-        [
-            chatbot,
-            pause_resume,
-            is_paused,
-            screenshot,
-            url,
-            action_messages,
-            browser_history,
-            session,
-            status,
-        ],
-        queue=False,
-    )
+        (
+            pause_resume.click(
+                pause_resume_task,
+                [is_paused, session, status],
+                [pause_resume, is_paused, session, status],
+                queue=False,
+            ).then(
+                get_messages,
+                [
+                    chatbot,
+                    action_messages,
+                    browser_history,
+                    session,
+                    status,
+                    agent_selection,
+                    api_key,
+                ],
+                [
+                    chatbot,
+                    screenshot,
+                    url,
+                    action_messages,
+                    browser_history,
+                    session,
+                    status,
+                    clear,
+                ],
+            )
+        )
+        clear.click(
+            clear_page,
+            [browser_history, session],
+            [
+                chatbot,
+                pause_resume,
+                is_paused,
+                screenshot,
+                url,
+                action_messages,
+                browser_history,
+                session,
+                status,
+            ],
+            queue=False,
+        )
 
-demo.queue()
-demo.launch(share=False)
+    demo.queue()
+    demo.launch(share=False)
