7745e03d92bf6d7c3798140fe368b7f3cebe4627
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-06-18 22:43:24 +0530

    lint

--- frontend/src/utils/beep.tsx
+++ frontend/src/utils/beep.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
 const beep = () => {
-  const snd = new Audio('/beep.wav');
-  snd.addEventListener('canplaythrough', () => snd.play());
-  snd.addEventListener('error', (e) => console.error('Audio file could not be loaded', e));
+  const snd = new Audio("/beep.wav");
+  snd.addEventListener("canplaythrough", () => snd.play());
+  snd.addEventListener("error", (e) =>
+    console.error("Audio file could not be loaded", e),
+  );
 };
 
 export default beep;
