e5f652288e9be3afbf72e6bebc65fa2720f31405
Umpire2018 <138990495+Umpire2018@users.noreply.github.com>
2024-06-23 14:33:30 +0000

    fix: sync with commit 868b746
    -  Change to `docusaurus write-translations` to provide translation for JSON files.

--- docs/src/components/Welcome/Welcome.tsx
+++ docs/src/components/Welcome/Welcome.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "../../css/welcome.css";
 import Translate from '@docusaurus/Translate';
 
@@ -8,10 +7,12 @@ export function Welcome() {
       <div className="welcome-container">
         <img src="img/logo.png" className="welcome-logo" />
         <p className="welcome-text">
+          <Translate id="welcome.message">
           Welcome to OpenDevin, an open-source autonomous AI software engineer
           that is capable of executing
           complex engineering tasks and collaborating actively with users on
           software development projects.
+          </Translate>
         </p>
       </div>
     </div>
