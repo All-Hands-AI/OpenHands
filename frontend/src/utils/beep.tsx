877d9ae7945582f9c66372f9ec65435c31aa4c84
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-06-27 06:57:48 +0530

    Added Sound Notification 🎵 (#2203)
    * Added Sound Notification
    
    * prettify
    
    * linted
    
    * disabled by default
    
    * Add dev config for frontend
    
    * add volume icon
    
    * fix typo
    
    * lint
    
    * lint
    
    * Apply suggestions from code review
    
    Co-authored-by: Graham Neubig <neubig@gmail.com>
    
    * Move volume icon to bottom
    
    ---------
    
    Co-authored-by: Graham Neubig <neubig@gmail.com>

---
+++ frontend/src/utils/beep.tsx
@@ -0,0 +1,9 @@
+const beep = () => {
+  const snd = new Audio("/beep.wav");
+  snd.addEventListener("canplaythrough", () => snd.play());
+  snd.addEventListener("error", (e) =>
+    console.error("Audio file could not be loaded", e),
+  );
+};
+
+export default beep;
