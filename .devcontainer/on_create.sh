bc1e3a773bfebebaa801690c71c6ba53cc864c88
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-06-29 14:20:58 +0530

    install netcat

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sudo apt install -y netcat
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt install -y python3.11
 curl -sSL https://install.python-poetry.org | python3.11 -
