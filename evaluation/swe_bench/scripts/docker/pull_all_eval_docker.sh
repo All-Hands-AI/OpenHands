6a0ffc5c619dff80487c40bdf9f5c6ff062cb367
Xingyao Wang <xingyao6@illinois.edu>
2024-07-02 07:58:30 +0800

    [Evaluation] Use the latest official SWE-Bench Dockerization for evaluation (#2728)
    * add newline after patch to fix patch apply
    
    * new swebench wip
    
    * add newline after patch to fix patch apply
    
    * only add newline if not empty
    
    * update swebench source and update
    
    * update gitignore for swebench eval
    
    * update old prep_eval
    
    * update gitignore
    
    * add scripts for push and pull swebench images
    
    * update eval_infer.sh
    
    * update eval_infer for new docker workflow
    
    * update script to create markdown report based on report.json
    
    * update eval infer to use update output
    
    * update readme
    
    * only move result to folder if running whole file
    
    * remove set-x
    
    * update conversion script
    
    * Update evaluation/swe_bench/README.md
    
    * Update evaluation/swe_bench/README.md
    
    * Update evaluation/swe_bench/README.md
    
    * make sure last line end with newline
    
    * switch to an fix attempt branch of swebench
    
    * Update evaluation/swe_bench/README.md
    
    * Update evaluation/swe_bench/README.md
    
    ---------
    
    Co-authored-by: Engel Nyst <enyst@users.noreply.github.com>

--- evaluation/swe_bench/scripts/docker/pull_all_eval_docker.sh
+++ evaluation/swe_bench/scripts/docker/pull_all_eval_docker.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
+set -e
 
-mkdir evaluation/swe_bench/eval_workspace
-pushd evaluation/swe_bench/eval_workspace
-git clone https://github.com/OpenDevin/SWE-bench-docker.git
-cd SWE-bench-docker
-scripts/pull_docker_images.sh docker/ xingyaoww
+LEVEL=$1
+# three levels:
+# - base, keyword "sweb.base"
+# - env, keyword "sweb.env"
+# - instance, keyword "sweb.eval"
+
+if [ -z "$LEVEL" ]; then
+    echo "Usage: $0 <cache_level>"
+    echo "cache_level: base, env, or instance"
+    exit 1
+fi
+
+NAMESPACE=xingyaoww
+IMAGE_FILE="$(dirname "$0")/all-swebench-lite-instance-images.txt"
+
+# Define a pattern based on the level
+case $LEVEL in
+    base)
+        PATTERN="sweb.base"
+        ;;
+    env)
+        PATTERN="sweb.base\|sweb.env"
+        ;;
+    instance)
+        PATTERN="sweb.base\|sweb.env\|sweb.eval"
+        ;;
+    *)
+        echo "Invalid cache level: $LEVEL"
+        echo "Valid levels are: base, env, instance"
+        exit 1
+        ;;
+esac
+
+echo "Pulling docker images for [$LEVEL] level"
+
+echo "Pattern: $PATTERN"
+echo "Image file: $IMAGE_FILE"
+
+# Read each line from the file, filter by pattern, and pull the docker image
+grep "$PATTERN" "$IMAGE_FILE" | while IFS= read -r image; do
+    echo "Pulling $NAMESPACE/$image into $image"
+    docker pull $NAMESPACE/$image
+    docker tag $NAMESPACE/$image $image
+done
