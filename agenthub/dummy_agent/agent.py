83d865cf4b4adcd7c322f498250dd25de67e600e
Engel Nyst <enyst@users.noreply.github.com>
2024-07-03 03:42:12 +0200

    refactor agents

--- agenthub/dummy_agent/agent.py
+++ agenthub/dummy_agent/agent.py
@@ -142,13 +142,19 @@ class DummyAgent(Agent):
 
     def step(self, state: State) -> Action:
         time.sleep(0.1)
+        history = state.history
         if state.iteration > 0:
             prev_step = self.steps[state.iteration - 1]
+
+            # a step is (action, observations list)
             if 'observations' in prev_step:
+                # one obs, or multiple obs if action is a background command
                 expected_observations = prev_step['observations']
-                hist_start = len(state.history) - len(expected_observations)
+                
+                # check if the history matches the expected observations
+                hist_events = history.get_last_events(len(expected_observations))
                 for i in range(len(expected_observations)):
-                    hist_obs = event_to_dict(state.history[hist_start + i][1])
+                    hist_obs = event_to_dict(hist_events[i])
                     expected_obs = event_to_dict(expected_observations[i])
                     if (
                         'command_id' in hist_obs['extras']
