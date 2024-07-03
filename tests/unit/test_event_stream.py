8c1859a1b8f0faeceec68db962791c8fa433b033
Engel Nyst <enyst@users.noreply.github.com>
2024-07-03 06:42:30 +0200

    testing micro agents, event stream

--- tests/unit/test_event_stream.py
+++ tests/unit/test_event_stream.py
@@ -1,25 +1,34 @@
 import json
 
+import pytest
+
 from opendevin.events import EventSource, EventStream
 from opendevin.events.action import NullAction
 from opendevin.events.observation import NullObservation
+
+
+@pytest.fixture
+def event_stream():
+    event_stream = EventStream('abc')
+    yield event_stream
+
+    # clear after each test
+    event_stream.clear()
 
 
 def collect_events(stream):
     return [event for event in stream.get_events()]
 
 
-def test_basic_flow():
-    stream = EventStream('abc')
-    stream.add_event(NullAction(), EventSource.AGENT)
-    assert len(collect_events(stream)) == 1
+def test_basic_flow(event_stream: EventStream):
+    event_stream.add_event(NullAction(), EventSource.AGENT)
+    assert len(collect_events(event_stream)) == 1
 
 
-def test_stream_storage():
-    stream = EventStream('def')
-    stream.add_event(NullObservation(''), EventSource.AGENT)
-    assert len(collect_events(stream)) == 1
-    content = stream._file_store.read('sessions/def/events/0.json')
+def test_stream_storage(event_stream: EventStream):
+    event_stream.add_event(NullObservation(''), EventSource.AGENT)
+    assert len(collect_events(event_stream)) == 1
+    content = event_stream._file_store.read('sessions/def/events/0.json')
     assert content is not None
     data = json.loads(content)
     assert 'timestamp' in data
@@ -34,11 +43,10 @@ def test_stream_storage():
     }
 
 
-def test_rehydration():
-    stream1 = EventStream('es1')
-    stream1.add_event(NullObservation('obs1'), EventSource.AGENT)
-    stream1.add_event(NullObservation('obs2'), EventSource.AGENT)
-    assert len(collect_events(stream1)) == 2
+def test_rehydration(event_stream: EventStream):
+    event_stream.add_event(NullObservation('obs1'), EventSource.AGENT)
+    event_stream.add_event(NullObservation('obs2'), EventSource.AGENT)
+    assert len(collect_events(event_stream)) == 2
 
     stream2 = EventStream('es2')
     assert len(collect_events(stream2)) == 0
