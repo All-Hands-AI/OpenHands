33ca3716b8d55970d600969651bd7f60a8652909
yufansong <yufan@risingwave-labs.com>
2024-07-06 21:37:22 -0700

    add EventStreamRuntime test

--- opendevin/runtime/od-runtime-client/client.py
+++ opendevin/runtime/od-runtime-client/client.py
@@ -2,6 +2,42 @@
 import asyncio
 import websockets
 
+class EventStreamRuntime:
+    uri = 'ws://localhost:8080'
+
+    def __init__(self):
+        self.websocket = None
+        # self.loop = asyncio.get_event_loop()
+        # self.loop.run_until_complete(self._init_websocket_connect())
+
+    # async def _init_websocket_connect(self):
+    #     print("Connecting to WebSocket...")
+    #     self.websocket = await websockets.connect(self.uri)
+    #     print("WebSocket connected.")
+
+    # async def connect(self):
+    #     print("Connecting to WebSocket...")
+    #     if self.websocket is None or self.websocket.closed:
+    #         self.websocket = await websockets.connect(self.uri)
+    #         if self.websocket is None:
+    #             raise Exception("WebSocket is not connected.")
+
+    async def execute(self, command):
+        self.websocket = await websockets.connect(self.uri)
+
+        print(f"Sending command: {command}")
+        await self.websocket.send(command)
+        print("Command sent, waiting for response...")
+        try:
+            output = await asyncio.wait_for(self.websocket.recv(), timeout=10)
+            print("Received output")
+            print(output)
+        except asyncio.TimeoutError:
+            print("No response received within the timeout period.")
+        
+        await self.websocket.close()
+
+
 async def send_command():
     uri = "ws://localhost:8080"
     while True:
@@ -22,6 +58,11 @@ async def send_command():
             print(f"Connection closed, retrying... ({str(e)})")
             await asyncio.sleep(1)
 
-if __name__ == "__main__":
-    asyncio.run(send_command())
+# if __name__ == "__main__":
+#     asyncio.run(send_command())
 
+
+if __name__ == "__main__":
+    runtime = EventStreamRuntime()
+    asyncio.run(runtime.execute('ls -l'))
+    asyncio.run(runtime.execute('pwd'))
