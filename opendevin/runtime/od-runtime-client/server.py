e4b39388a920e018888b8bb46c2bc3ca2013c0bc
yufansong <yufan@risingwave-labs.com>
2024-07-06 21:38:06 -0700

    add echo server test

--- opendevin/runtime/od-runtime-client/server.py
+++ opendevin/runtime/od-runtime-client/server.py
@@ -3,6 +3,7 @@ import asyncio
 import websockets
 import pexpect
 from websockets.exceptions import ConnectionClosed
+import json
 
 async def execute_command(websocket, path):
     shell = pexpect.spawn('/bin/bash', encoding='utf-8')
@@ -23,6 +24,28 @@ async def execute_command(websocket, path):
         print("Connection closed")
     finally:
         shell.close()
+
+
+def is_valid_json(s):
+    try:
+        json.loads(s)
+    except json.JSONDecodeError:
+        return False
+    return True
+
+async def echo(websocket, path):
+    async for message in websocket:
+        if is_valid_json(message):
+            event = json.loads(message)
+            print("Received:", event)
+            # event = "{'message': 'Running command: ls -l', 'action': 'run', 'args': {'command': 'ls -l', 'background': False, 'thought': ''}}"
+            response = json.dumps(event)  
+            await websocket.send(response)
+        else:
+            print("Received:", message)
+            response = f"Echo: {message}"
+            await websocket.send(response)
+
 
 start_server = websockets.serve(execute_command, "0.0.0.0", 8080)
 
