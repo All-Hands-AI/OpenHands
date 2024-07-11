5f61885e44cf1841fe9ec82befd38cf45b13869b
adragos <albastroiudragos@gmail.com>
2024-07-11 13:57:21 +0200

    feat: Implement user confirmation mode, request confirmation when running bash/python code in this mode (#2774)
    * [feat] confirmation mode for bash actions
    
    * feat: Add modal setting for Confirmation Mode
    
    * fix: frontend tests for confirmation mode switch
    
    * fix: add missing CONFIRMATION_MODE value in SettingsModal.test.tsx
    
    * fix: update test to integrate new setting
    
    * feat: Implement user confirmation for running bash/python code
    
    * fix: don't display rejected actions
    
    * fix: linting, rename/refactor based on feedback
    
    * fix: add property only to commands, pass serialization tests
    
    * fix: package-lock.json, lint test_action_serialization.py
    
    * test: add is_confirmed to integration test outputs
    
    ---------
    
    Co-authored-by: Mislav Balunovic <mislav.balunovic@gmail.com>

--- opendevin/server/session/agent.py
+++ opendevin/server/session/agent.py
@@ -91,6 +91,9 @@ class AgentSession:
         model = args.get(ConfigType.LLM_MODEL, llm_config.model)
         api_key = args.get(ConfigType.LLM_API_KEY, llm_config.api_key)
         api_base = llm_config.base_url
+        confirmation_mode = args.get(
+            ConfigType.CONFIRMATION_MODE, config.confirmation_mode
+        )
         max_iterations = args.get(ConfigType.MAX_ITERATIONS, config.max_iterations)
 
         logger.info(f'Creating agent {agent_cls} using LLM {model}')
@@ -110,6 +113,7 @@ class AgentSession:
             event_stream=self.event_stream,
             agent=agent,
             max_iterations=int(max_iterations),
+            confirmation_mode=confirmation_mode,
         )
         try:
             agent_state = State.restore_from_session(self.sid)
