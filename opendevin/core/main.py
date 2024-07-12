7e68de746d4f747053539592fa75b2b136b39dea
Xingyao Wang <xingyao6@illinois.edu>
2024-07-13 04:03:28 +0800

    arch: refractor eventstream into async (#2907)
    * deprecating recall action
    
    * fix integration tests
    
    * fix integration tests
    
    * refractor runtime to use async
    
    * remove search memory
    
    * rename .initialize to .ainit

--- opendevin/core/main.py
+++ opendevin/core/main.py
@@ -81,6 +81,7 @@ async def run_agent_controller(
     # runtime and tools
     runtime_cls = get_runtime_cls(config.runtime)
     runtime = runtime_cls(event_stream=event_stream, sandbox=sandbox)
+    await runtime.ainit()
     runtime.init_sandbox_plugins(controller.agent.sandbox_plugins)
     runtime.init_runtime_tools(
         controller.agent.runtime_tools,
@@ -140,7 +141,7 @@ async def run_agent_controller(
 
     # close when done
     await controller.close()
-    runtime.close()
+    await runtime.close()
     return controller.get_state()
 
 
