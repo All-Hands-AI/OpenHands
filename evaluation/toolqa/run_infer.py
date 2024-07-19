fdf80c4f2fac0f1cccda426f5194bad449323da4
Graham Neubig <neubig@gmail.com>
2024-07-19 14:36:31 -0400

    Remove monologue agent

--- evaluation/toolqa/run_infer.py
+++ evaluation/toolqa/run_infer.py
@@ -10,7 +10,6 @@ from evaluation.utils.shared import (
     EvalMetadata,
     codeact_user_response,
     make_metadata,
-    monologue_user_response,
     prepare_dataset,
     run_evaluation,
 )
@@ -26,7 +25,6 @@ from .utils import download_data, download_tools, encode_question, eval_answer, 
 
 AGENT_CLS_TO_FAKE_USER_RESPONSE_FN = {
     'CodeActAgent': codeact_user_response,
-    'MonologueAgent': monologue_user_response,
 }
 
 AGENT_CLS_TO_INST_SUFFIX = {
