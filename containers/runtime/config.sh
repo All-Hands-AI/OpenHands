1597a6621795eb0f0acc71229b88326f0902cb10
Xingyao Wang <xingyao6@illinois.edu>
2024-07-21 16:04:39 -0400

    update docker base dir for runtime

--- containers/runtime/config.sh
+++ containers/runtime/config.sh
@@ -1,6 +1,6 @@
 DOCKER_REGISTRY=ghcr.io
 DOCKER_ORG=opendevin
-DOCKER_BASE_DIR="."
+DOCKER_BASE_DIR="./containers/runtime"
 # These two variables will be appended by the runtime_build.py script
 # DOCKER_IMAGE=
 # DOCKER_IMAGE_TAG=
