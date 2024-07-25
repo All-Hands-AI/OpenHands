f586911ecf689440d106fd720c418918384113e4
sp.wack <83104063+amanape@users.noreply.github.com>
2024-07-25 18:49:51 +0300

    Refactor and test component (#3108)

--- frontend/src/utils/utils.test.ts
+++ frontend/src/utils/utils.test.ts
@@ -1,6 +1,12 @@
-import { removeApiKey } from "./utils";
+import { getExtension, removeApiKey } from "./utils";
 
 test("removeApiKey", () => {
   const data = [{ args: { LLM_API_KEY: "key", LANGUAGE: "en" } }];
   expect(removeApiKey(data)).toEqual([{ args: { LANGUAGE: "en" } }]);
+});
+
+test("getExtension", () => {
+  expect(getExtension("main.go")).toBe("go");
+  expect(getExtension("get-extension.test.ts")).toBe("ts");
+  expect(getExtension("directory")).toBe("");
 });
