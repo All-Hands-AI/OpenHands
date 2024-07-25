9f125a4d2ea135a5bf06dc5b482774e37f5967d7
tobitege <tobitege@gmx.de>
2024-07-25 05:22:12 +0200

    test_runtime: run tests per runtime, not alternating

--- tests/unit/test_runtime.py
+++ tests/unit/test_runtime.py
@@ -1,5 +1,6 @@
 """Test the EventStreamRuntime, which connects to the RuntimeClient running in the sandbox."""
 
+import asyncio
 import os
 import pathlib
 import tempfile
@@ -24,6 +25,8 @@ from opendevin.runtime.server.runtime import ServerRuntime
 @pytest.fixture(autouse=True)
 def print_method_name(request):
     print('\n########################################################################')
+    print(f'Running test: {request.node.name}')
+    print('########################################################################')
 
 
 @pytest.fixture
@@ -32,6 +35,13 @@ def temp_dir(monkeypatch):
     with tempfile.TemporaryDirectory() as temp_dir:
         pathlib.Path().mkdir(parents=True, exist_ok=True)
         yield temp_dir
+
+
+# This assures that all tests run together for each runtime, not alternating between them,
+# which caused them to fail previously.
+@pytest.fixture(scope='module', params=[EventStreamRuntime, ServerRuntime])
+def box_class(request):
+    return request.param
 
 
 async def _load_runtime(box_class, event_stream, plugins, sid):
@@ -44,7 +54,7 @@ async def _load_runtime(box_class, event_stream, plugins, sid):
     if 'od_runtime' not in container_image:
         container_image = 'ubuntu:22.04'
         logger.warning(
-            f'`sandbox_config.container_image` is not an od_runtime image. Will use `{container_image}` as the container image for testing.'
+            f'`{sandbox_config.container_image}` is not an od_runtime image. Will use `{container_image}` as the container image for testing.'
         )
     if box_class == EventStreamRuntime:
         runtime = EventStreamRuntime(
@@ -73,159 +83,145 @@ async def _load_runtime(box_class, event_stream, plugins, sid):
     return runtime
 
 
-RUNTIME_TO_TEST = [EventStreamRuntime, ServerRuntime]
-
-
 @pytest.mark.asyncio
-async def test_env_vars_os_environ():
+async def test_env_vars_os_environ(box_class):
     with patch.dict(os.environ, {'SANDBOX_ENV_FOOBAR': 'BAZ'}):
         plugins = [JupyterRequirement(), AgentSkillsRequirement()]
         sid = 'test'
         cli_session = 'main_test'
 
-        for box_class in RUNTIME_TO_TEST:
-            event_stream = EventStream(cli_session)
-            runtime = await _load_runtime(box_class, event_stream, plugins, sid)
-
-            obs: CmdOutputObservation = await runtime.run_action(
-                CmdRunAction(command='env')
-            )
-            print(obs)
-
-            obs: CmdOutputObservation = await runtime.run_action(
-                CmdRunAction(command='echo $FOOBAR')
-            )
-            print(obs)
-            assert obs.exit_code == 0, 'The exit code should be 0.'
-            assert (
-                obs.content.strip().split('\n\r')[0].strip() == 'BAZ'
-            ), f'Output: [{obs.content}] for {box_class}'
-
-            await runtime.close()
-
-
-@pytest.mark.asyncio
-async def test_env_vars_runtime_add_env_vars():
-    plugins = [JupyterRequirement(), AgentSkillsRequirement()]
-    sid = 'test'
-    cli_session = 'main_test'
-
-    for box_class in RUNTIME_TO_TEST:
         event_stream = EventStream(cli_session)
         runtime = await _load_runtime(box_class, event_stream, plugins, sid)
-        await runtime.add_env_vars({'QUUX': 'abc"def'})
 
         obs: CmdOutputObservation = await runtime.run_action(
-            CmdRunAction(command='echo $QUUX')
+            CmdRunAction(command='env')
+        )
+        print(obs)
+
+        obs: CmdOutputObservation = await runtime.run_action(
+            CmdRunAction(command='echo $FOOBAR')
         )
         print(obs)
         assert obs.exit_code == 0, 'The exit code should be 0.'
         assert (
-            obs.content.strip().split('\r\n')[0].strip() == 'abc"def'
+            obs.content.strip().split('\n\r')[0].strip() == 'BAZ'
         ), f'Output: [{obs.content}] for {box_class}'
-        await runtime.close()
-
-
-@pytest.mark.asyncio
-async def test_env_vars_runtime_add_empty_dict():
-    plugins = [JupyterRequirement(), AgentSkillsRequirement()]
-    sid = 'test'
-    cli_session = 'main_test'
-
-    for box_class in RUNTIME_TO_TEST:
-        event_stream = EventStream(cli_session)
-        runtime = await _load_runtime(box_class, event_stream, plugins, sid)
-
-        prev_obs = await runtime.run_action(CmdRunAction(command='env'))
-        assert prev_obs.exit_code == 0, 'The exit code should be 0.'
-        print(prev_obs)
-
-        await runtime.add_env_vars({})
-
-        obs = await runtime.run_action(CmdRunAction(command='env'))
-        assert obs.exit_code == 0, 'The exit code should be 0.'
-        print(obs)
-        assert (
-            obs.content == prev_obs.content
-        ), 'The env var content should be the same after adding an empty dict.'
 
         await runtime.close()
+        await asyncio.sleep(1)
 
 
 @pytest.mark.asyncio
-async def test_env_vars_runtime_add_multiple_env_vars():
+async def test_env_vars_runtime_add_env_vars(box_class):
     plugins = [JupyterRequirement(), AgentSkillsRequirement()]
     sid = 'test'
     cli_session = 'main_test'
 
-    for box_class in RUNTIME_TO_TEST:
-        event_stream = EventStream(cli_session)
-        runtime = await _load_runtime(box_class, event_stream, plugins, sid)
-        await runtime.add_env_vars({'QUUX': 'abc"def', 'FOOBAR': 'xyz'})
+    event_stream = EventStream(cli_session)
+    runtime = await _load_runtime(box_class, event_stream, plugins, sid)
+    await runtime.add_env_vars({'QUUX': 'abc"def'})
 
-        obs: CmdOutputObservation = await runtime.run_action(
-            CmdRunAction(command='echo $QUUX $FOOBAR')
-        )
-        print(obs)
-        assert obs.exit_code == 0, 'The exit code should be 0.'
-        assert (
-            obs.content.strip().split('\r\n')[0].strip() == 'abc"def xyz'
-        ), f'Output: [{obs.content}] for {box_class}'
-        await runtime.close()
+    obs: CmdOutputObservation = await runtime.run_action(
+        CmdRunAction(command='echo $QUUX')
+    )
+    print(obs)
+    assert obs.exit_code == 0, 'The exit code should be 0.'
+    assert (
+        obs.content.strip().split('\r\n')[0].strip() == 'abc"def'
+    ), f'Output: [{obs.content}] for {box_class}'
+
+    await runtime.close()
+    await asyncio.sleep(1)
 
 
 @pytest.mark.asyncio
-async def test_env_vars_runtime_add_env_vars_overwrite():
+async def test_env_vars_runtime_add_empty_dict(box_class):
     plugins = [JupyterRequirement(), AgentSkillsRequirement()]
     sid = 'test'
     cli_session = 'main_test'
 
-    for box_class in RUNTIME_TO_TEST:
-        with patch.dict(os.environ, {'SANDBOX_ENV_FOOBAR': 'BAZ'}):
-            event_stream = EventStream(cli_session)
-            runtime = await _load_runtime(box_class, event_stream, plugins, sid)
-            await runtime.add_env_vars({'FOOBAR': 'xyz'})
-
-            obs: CmdOutputObservation = await runtime.run_action(
-                CmdRunAction(command='echo $FOOBAR')
-            )
-            print(obs)
-            assert obs.exit_code == 0, 'The exit code should be 0.'
-            assert (
-                obs.content.strip().split('\r\n')[0].strip() == 'xyz'
-            ), f'Output: [{obs.content}] for {box_class}'
-            await runtime.close()
-
-
-@pytest.mark.asyncio
-async def test_bash_command_pexcept(temp_dir):
-    plugins = [JupyterRequirement(), AgentSkillsRequirement()]
-    sid = 'test'
-    cli_session = 'main_test'
-
-    box_class = EventStreamRuntime
     event_stream = EventStream(cli_session)
     runtime = await _load_runtime(box_class, event_stream, plugins, sid)
 
-    # We set env var PS1="\u@\h:\w $"
-    # and construct the PEXCEPT prompt base on it.
-    # When run `env`, bad implementation of CmdRunAction will be pexcepted by this
-    # and failed to pexcept the right content, causing it fail to get error code.
+    prev_obs = await runtime.run_action(CmdRunAction(command='env'))
+    assert prev_obs.exit_code == 0, 'The exit code should be 0.'
+    print(prev_obs)
+
+    await runtime.add_env_vars({})
+
+    obs = await runtime.run_action(CmdRunAction(command='env'))
+    assert obs.exit_code == 0, 'The exit code should be 0.'
+    print(obs)
+    assert (
+        obs.content == prev_obs.content
+    ), 'The env var content should be the same after adding an empty dict.'
+
+    await runtime.close()
+    await asyncio.sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_env_vars_runtime_add_multiple_env_vars(box_class):
+    plugins = [JupyterRequirement(), AgentSkillsRequirement()]
+    sid = 'test'
+    cli_session = 'main_test'
+
+    event_stream = EventStream(cli_session)
+    runtime = await _load_runtime(box_class, event_stream, plugins, sid)
+    await runtime.add_env_vars({'QUUX': 'abc"def', 'FOOBAR': 'xyz'})
+
+    obs: CmdOutputObservation = await runtime.run_action(
+        CmdRunAction(command='echo $QUUX $FOOBAR')
+    )
+    print(obs)
+    assert obs.exit_code == 0, 'The exit code should be 0.'
+    assert (
+        obs.content.strip().split('\r\n')[0].strip() == 'abc"def xyz'
+    ), f'Output: [{obs.content}] for {box_class}'
+
+    await runtime.close()
+    await asyncio.sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_env_vars_runtime_add_env_vars_overwrite(box_class):
+    plugins = [JupyterRequirement(), AgentSkillsRequirement()]
+    sid = 'test'
+    cli_session = 'main_test'
+
+    with patch.dict(os.environ, {'SANDBOX_ENV_FOOBAR': 'BAZ'}):
+        event_stream = EventStream(cli_session)
+        runtime = await _load_runtime(box_class, event_stream, plugins, sid)
+        await runtime.add_env_vars({'FOOBAR': 'xyz'})
+
+        obs: CmdOutputObservation = await runtime.run_action(
+            CmdRunAction(command='echo $FOOBAR')
+        )
+        print(obs)
+        assert obs.exit_code == 0, 'The exit code should be 0.'
+        assert (
+            obs.content.strip().split('\r\n')[0].strip() == 'xyz'
+        ), f'Output: [{obs.content}] for {box_class}'
+
+        await runtime.close()
+        await asyncio.sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_bash_command_pexcept(temp_dir, box_class):
+    plugins = [JupyterRequirement(), AgentSkillsRequirement()]
+    sid = 'test'
+    cli_session = 'main_test'
+
+    event_stream = EventStream(cli_session)
+    runtime = await _load_runtime(box_class, event_stream, plugins, sid)
+
     obs = await runtime.run_action(CmdRunAction(command='env'))
 
-    # For example:
-    # 02:16:13 - opendevin:DEBUG: client.py:78 - Executing command: env
-    # 02:16:13 - opendevin:DEBUG: client.py:82 - Command output: PYTHONUNBUFFERED=1
-    # CONDA_EXE=/opendevin/miniforge3/bin/conda
-    # [...]
-    # LC_CTYPE=C.UTF-8
-    # PS1=\u@\h:\w $
-    # 02:16:13 - opendevin:DEBUG: client.py:89 - Executing command for exit code: env
-    # 02:16:13 - opendevin:DEBUG: client.py:92 - Exit code Output:
-    # CONDA_DEFAULT_ENV=base
-
-    # As long as the exit code is 0, the test will pass.
     assert isinstance(
         obs, CmdOutputObservation
     ), 'The observation should be a CmdOutputObservation.'
     assert obs.exit_code == 0, 'The exit code should be 0.'
+
+    await runtime.close()
+    await asyncio.sleep(1)
