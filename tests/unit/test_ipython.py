1f6e86c932e16914b94a673f22fc791cd8d4d58c
Xingyao Wang <xingyao6@illinois.edu>
2024-07-26 22:53:01 +0800

    Fix(test,CI): runtime build tests (#3126)
    * fix runtime build test
    
    * get runtime_build test to run in CI
    
    * move test involving docker from `test_ipython` to `test_sandbox`

--- tests/unit/test_ipython.py
+++ tests/unit/test_ipython.py
@@ -7,8 +7,6 @@ import pytest
 from opendevin.core.config import SandboxConfig
 from opendevin.events.action import IPythonRunCellAction
 from opendevin.events.observation import IPythonRunCellObservation
-from opendevin.runtime.docker.ssh_box import DockerSSHBox
-from opendevin.runtime.plugins import JupyterRequirement
 from opendevin.runtime.server.runtime import ServerRuntime
 
 
@@ -75,28 +73,3 @@ async def test_run_python_backticks():
         assert (
             test_code == result.content
         ), f'The output should contain the expected print output, got: {result.content}'
-
-
-def test_sandbox_jupyter_plugin_backticks(temp_dir):
-    box = DockerSSHBox(
-        config=SandboxConfig(),
-        persist_sandbox=False,
-        workspace_mount_path=temp_dir,
-        sandbox_workspace_dir='/workspace',
-        cache_dir='/tmp/cache',
-        run_as_devin=True,
-    )
-    box.init_plugins([JupyterRequirement])
-    test_code = "print('Hello, `World`!')"
-    expected_write_command = (
-        "cat > /tmp/opendevin_jupyter_temp.py <<'EOL'\n" f'{test_code}\n' 'EOL'
-    )
-    expected_execute_command = 'cat /tmp/opendevin_jupyter_temp.py | execute_cli'
-    exit_code, output = box.execute(expected_write_command)
-    exit_code, output = box.execute(expected_execute_command)
-    print(output)
-    assert exit_code == 0, 'The exit code should be 0 for ' + box.__class__.__name__
-    assert output.strip() == 'Hello, `World`!', (
-        'The output should be the same as the input for ' + box.__class__.__name__
-    )
-    box.close()
