d00f54fcf5fc7467d203c02f98a55fcf73be48e8
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-07-29 18:13:46 +0530

    lint

--- frontend/src/services/terminalService.ts
+++ frontend/src/services/terminalService.ts
@@ -2,9 +2,10 @@ import ActionType from "#/types/ActionType";
 import Session from "./session";
 
 export function sendTerminalCommand(command: string): void {
-  // replace END OF TEXT character
-  command = command.replace(/\u0003+/, '');
-  const event = { action: ActionType.RUN, args: { command } };
+  // replace END OF TEXT character copied from terminal
+  // eslint-disable-next-line no-control-regex
+  const cleanedCommand = command.replace(/\u0003+/, "");
+  const event = { action: ActionType.RUN, args: { command: cleanedCommand } };
   const eventString = JSON.stringify(event);
   Session.send(eventString);
 }
