f5bdb32db47c04c31149df9216abf59ddb7def65
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2024-07-29 19:59:12 +0530

    reject empty commands

--- frontend/src/services/terminalService.ts
+++ frontend/src/services/terminalService.ts
@@ -5,6 +5,7 @@ export function sendTerminalCommand(command: string): void {
   // replace END OF TEXT character copied from terminal
   // eslint-disable-next-line no-control-regex
   const cleanedCommand = command.replace(/\u0003+/, "");
+  if (!cleanedCommand) return;
   const event = { action: ActionType.RUN, args: { command: cleanedCommand } };
   const eventString = JSON.stringify(event);
   Session.send(eventString);
