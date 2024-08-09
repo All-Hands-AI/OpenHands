fae49148eb407eca5522d590ebf416af0ca2d0b6
Xingyao Wang <xingyao6@illinois.edu>
2024-08-09 09:33:08 -0700

    fix test

--- tests/unit/test_runtime_build.py
+++ tests/unit/test_runtime_build.py
@@ -182,7 +182,7 @@ def test_get_runtime_image_repo_and_tag_eventstream():
     img_repo, img_tag = get_runtime_image_repo_and_tag(base_image)
     assert (
         img_repo == f'{RUNTIME_IMAGE_REPO}'
-        and img_tag == f'{OD_VERSION}_image_python_tag_3.11-bookworm'
+        and img_tag == f'{OD_VERSION}_image_python-nodejs_tag_python3.11-nodejs22'
     )
 
     base_image = 'ubuntu'
