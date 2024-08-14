7d331acffa83e9bb709299f26c408f3b361b9941
Graham Neubig <neubig@gmail.com>
2024-08-14 09:47:31 -0400

    Handle error observations in codeact (#3383)
    * Handle error observations in codeact
    
    * Remove comments

--- opendevin/events/observation/delegate.py
+++ opendevin/events/observation/delegate.py
@@ -7,7 +7,13 @@ from .observation import Observation
 
 @dataclass
 class AgentDelegateObservation(Observation):
-    """This data class represents the result from delegating to another agent"""
+    """This data class represents the result from delegating to another agent.
+
+    Attributes:
+        content (str): The content of the observation.
+        outputs (dict): The outputs of the delegated agent.
+        observation (str): The type of observation.
+    """
 
     outputs: dict
     observation: str = ObservationType.DELEGATE
