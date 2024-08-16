d5cb287074e66f03237ea7fe1dbb27a579718c2c
Tim O'Farrell <tofarr@gmai.com>
2024-08-16 15:38:49 -0600

    Now adjusting max_iterations or max_budget_per_task as appropriate

--- opendevin/controller/agent_controller.py
+++ opendevin/controller/agent_controller.py
@@ -226,7 +226,10 @@ class AgentController:
             # user intends to interrupt traffic control and let the task resume temporarily
             self.state.traffic_control_state = TrafficControlState.PAUSED
             # User has chosen to deliberately continue - lets double the max iterations
-            self.state.max_iterations *= 2
+            if self.state.iteration == self.state.max_iterations:
+                self.state.max_iterations *= 2
+            if self.state.metrics.accumulated_cost == self.max_budget_per_task:
+                self.max_budget_per_task *= 2
 
         self.state.agent_state = new_state
         if new_state == AgentState.STOPPED or new_state == AgentState.ERROR:
