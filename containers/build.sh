538e58b15b58659c92b185af6fb331a80baeca88
Xingyao Wang <xingyao6@illinois.edu>
2024-08-16 16:13:33 -0500

    remove extra -docker-image

--- containers/build.sh
+++ containers/build.sh
@@ -69,7 +69,7 @@ for tag in "${tags[@]}"; do
   args+=" -t $DOCKER_REPOSITORY:$tag"
 done
 
-output_image="/tmp/${image_name}-${tags[-1]}-docker-image-${platform}.tar"
+output_image="/tmp/${image_name}-${tags[-1]}-${platform}.tar"
 echo "Output image will be saved to: $output_image"
 
 docker buildx build \
