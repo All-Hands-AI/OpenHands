65f7cd353ea4069a427971d8bde381f9b22a6649
Xingyao Wang <xingyao6@illinois.edu>
2024-08-18 12:10:54 -0500

    update testcase for jupyter py interpreter path

--- tests/unit/test_runtime.py
+++ tests/unit/test_runtime.py
@@ -291,9 +291,10 @@ async def test_simple_cmd_ipython_and_fileop(temp_dir, box_class, run_as_devin):
     assert isinstance(obs, IPythonRunCellObservation)
 
     logger.info(obs, extra={'msg_type': 'OBSERVATION'})
-    assert (
-        obs.content.strip()
-        == 'Hello, `World`!\n[Jupyter current working directory: /workspace]'
+    assert obs.content.strip() == (
+        'Hello, `World`!\n'
+        '[Jupyter current working directory: /workspace]\n'
+        '[Jupyter Python interpreter: /opendevin/poetry/opendevin-w8YlaYp7-py3.11/bin/python]'
     )
 
     # Test read file (file should not exist)
@@ -761,7 +762,11 @@ async def test_ipython_multi_user(temp_dir, box_class, run_as_devin):
     logger.info(obs, extra={'msg_type': 'OBSERVATION'})
     assert (
         obs.content.strip()
-        == '/workspace\n[Jupyter current working directory: /workspace]'
+        == (
+            '/workspace\n'
+            '[Jupyter current working directory: /workspace]\n'
+            '[Jupyter Python interpreter: /opendevin/poetry/opendevin-w8YlaYp7-py3.11/bin/python]'
+        ).strip()
     )
 
     # write a file
@@ -773,7 +778,11 @@ async def test_ipython_multi_user(temp_dir, box_class, run_as_devin):
     assert isinstance(obs, IPythonRunCellObservation)
     assert (
         obs.content.strip()
-        == '[Code executed successfully with no output]\n[Jupyter current working directory: /workspace]'
+        == (
+            '[Code executed successfully with no output]\n'
+            '[Jupyter current working directory: /workspace]\n'
+            '[Jupyter Python interpreter: /opendevin/poetry/opendevin-w8YlaYp7-py3.11/bin/python]'
+        ).strip()
     )
 
     # check file owner via bash
@@ -847,7 +856,8 @@ async def _test_ipython_agentskills_fileop_pwd_impl(
         '1|\n'
         '(this is the end of the file)\n'
         '[File hello.py created.]\n'
-        '[Jupyter current working directory: /workspace]'
+        '[Jupyter current working directory: /workspace]\n'
+        '[Jupyter Python interpreter: /opendevin/poetry/opendevin-w8YlaYp7-py3.11/bin/python]'
     ).strip().split('\n')
 
     action = CmdRunAction(command='cd test')
@@ -870,7 +880,8 @@ async def _test_ipython_agentskills_fileop_pwd_impl(
         '1|\n'
         '(this is the end of the file)\n'
         '[File hello.py created.]\n'
-        '[Jupyter current working directory: /workspace/test]'
+        '[Jupyter current working directory: /workspace/test]\n'
+        '[Jupyter Python interpreter: /opendevin/poetry/opendevin-w8YlaYp7-py3.11/bin/python]'
     ).strip().split('\n')
 
     if enable_auto_lint:
@@ -904,6 +915,7 @@ Your changes have NOT been applied. Please fix your edit command and try again.
 You either need to 1) Specify the correct start/end line arguments or 2) Correct your edit code.
 DO NOT re-run the same failed edit command. Running it again will lead to the same error.
 [Jupyter current working directory: /workspace/test]
+[Jupyter Python interpreter: /opendevin/poetry/opendevin-w8YlaYp7-py3.11/bin/python]
 """
         ).strip().split('\n')
 
@@ -923,6 +935,7 @@ DO NOT re-run the same failed edit command. Running it again will lead to the sa
 (this is the end of the file)
 [File updated (edited at line 1). Please review the changes and make sure they are correct (correct indentation, no duplicate lines, etc). Edit the file again if necessary.]
 [Jupyter current working directory: /workspace/test]
+[Jupyter Python interpreter: /opendevin/poetry/opendevin-w8YlaYp7-py3.11/bin/python]
 """
     ).strip().split('\n')
 
@@ -989,7 +1002,8 @@ async def test_ipython_agentskills_fileop_pwd_with_userdir(temp_dir, box_class):
         '1|\n'
         '(this is the end of the file)\n'
         '[File hello.py created.]\n'
-        '[Jupyter current working directory: /root]'
+        '[Jupyter current working directory: /root]\n'
+        '[Jupyter Python interpreter: /opendevin/poetry/opendevin-w8YlaYp7-py3.11/bin/python]'
     ).strip().split('\n')
 
     action = CmdRunAction(command='cd test')
@@ -1012,7 +1026,8 @@ async def test_ipython_agentskills_fileop_pwd_with_userdir(temp_dir, box_class):
         '1|\n'
         '(this is the end of the file)\n'
         '[File hello.py created.]\n'
-        '[Jupyter current working directory: /root/test]'
+        '[Jupyter current working directory: /root/test]\n'
+        '[Jupyter Python interpreter: /opendevin/poetry/opendevin-w8YlaYp7-py3.11/bin/python]'
     ).strip().split('\n')
 
     await runtime.close()
@@ -1046,9 +1061,10 @@ async def test_ipython_package_install(temp_dir, box_class, run_as_devin):
     obs = await runtime.run_action(action)
     logger.info(obs, extra={'msg_type': 'OBSERVATION'})
     # import should not error out
-    assert (
-        obs.content.strip()
-        == '[Code executed successfully with no output]\n[Jupyter current working directory: /workspace]'
+    assert obs.content.strip() == (
+        '[Code executed successfully with no output]\n'
+        '[Jupyter current working directory: /workspace]\n'
+        '[Jupyter Python interpreter: /opendevin/poetry/opendevin-w8YlaYp7-py3.11/bin/python]'
     )
 
     await runtime.close()
