83f36c1d66cebcaf3d5d06ea7f777c0cda8109ba
Xingyao Wang <xingyao6@illinois.edu>
2024-08-19 21:12:00 +0800

    test: build and run runtime tests on different custom docker images (#3324)
    * try to fix pip unavailable
    
    * update test case for pip
    
    * force rebuild in CI
    
    * remove extra symlink
    
    * fix newline
    
    * added semi-colon to line 31
    
    * Dockerfile.j2: activate env at the end
    
    * Revert "Dockerfile.j2: activate env at the end"
    
    This reverts commit cf2f5651021fe80d4ab69a35a85f0a35b29dc3d7.
    
    * cleanup Dockerfile
    
    * switch default python image
    
    * remove image agnostic (no longer used)
    
    * fix tests
    
    * simplify integration tests default image
    
    * add nodejs specific runtime tests
    
    * update tests and workflows
    
    * switch to nikolaik/python-nodejs:python3.11-nodejs22
    
    * update build sh to output image name correctly
    
    * increase custom images to test
    
    * fix test
    
    * fix test
    
    * fix double quote
    
    * try fixing ci
    
    * update ghcr workflow
    
    * fix artifact name
    
    * try to fix ghcr again
    
    * fix workflow
    
    * save built image to correct dir
    
    * remove extra -docker-image
    
    * make last tag to be human readable image tag
    
    * fix hyphen to underscore
    
    * run test runtime on all tags
    
    * revert app build
    
    * separate ghcr workflow
    
    * update dockerfile for eval
    
    * fix tag for test run
    
    * try fix tag
    
    * try fix tag via matrix output
    
    * try workflow again
    
    * update comments
    
    * try fixing test matrix
    
    * fix artifact name
    
    * try fix tag again
    
    * Revert "try fix tag again"
    
    This reverts commit b369badd8cccf4a526e36d27eafb77ea2d32f6be.
    
    * tweak filename
    
    * try different path
    
    * fix filepath
    
    * try fix tag artifact path again
    
    * save json instead of line
    
    * update matrix
    
    * print all tags in workflow
    
    * fix DOCKER_IMAGE to avoid ghcr.io/opendevin/ghcr.io/opendevin/od_runtime
    
    * fix test matrix to only load unique test image tags
    
    * try fix matrix again!!!!!
    
    * add all runtime tests passed
    
    ---------
    
    Co-authored-by: tobitege <tobitege@gmx.de>
    Co-authored-by: Graham Neubig <neubig@gmail.com>
    Co-authored-by: tobitege <10787084+tobitege@users.noreply.github.com>

--- containers/runtime/config.sh
+++ containers/runtime/config.sh
@@ -1,7 +1,7 @@
 DOCKER_REGISTRY=ghcr.io
 DOCKER_ORG=opendevin
 DOCKER_BASE_DIR="./containers/runtime"
+DOCKER_IMAGE=od_runtime
 # These variables will be appended by the runtime_build.py script
-# DOCKER_IMAGE=
 # DOCKER_IMAGE_TAG=
 # DOCKER_IMAGE_HASH_TAG=
