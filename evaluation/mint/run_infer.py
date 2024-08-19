8f0f764a85351da1f32cace9b851c08110d171d9
Xingyao Wang <xingyao6@illinois.edu>
2024-08-20 04:53:28 +0800

    fix: CI docker image push (#3476)
    * fix ghcr app
    
    * fix ghcr runtime push
    
    * rename od_runtime to runtime

--- evaluation/mint/run_infer.py
+++ evaluation/mint/run_infer.py
@@ -104,7 +104,7 @@ def get_config(
             container_image='xingyaoww/od-eval-mint:v1.0',
             enable_auto_lint=True,
             use_host_network=False,
-            od_runtime_extra_deps=f'$OD_INTERPRETER_PATH -m pip install {" ".join(MINT_DEPENDENCIES)}',
+            runtime_extra_deps=f'$OD_INTERPRETER_PATH -m pip install {" ".join(MINT_DEPENDENCIES)}',
         ),
         # do not mount workspace
         workspace_base=None,
