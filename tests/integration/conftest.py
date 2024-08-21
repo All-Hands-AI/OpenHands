370a55494b5f6a04c6be4513d60492f92dabe8e4
tobitege <10787084+tobitege@users.noreply.github.com>
2024-08-21 07:33:41 +0200

    conftest: also mask size=xxx parameter in integration tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -50,6 +50,9 @@ def filter_out_symbols(input):
     # openhands@379c7fce40b4:/workspace $
     input = re.sub(r'(openhands|root)@.*(:/.*)', r'\1[DUMMY_HOSTNAME]\2', input)
 
+    # handle size param
+    input = re.sub(r' size=\d+ ', ' size=[DUMMY_SIZE] ', input)
+
     # handle sha256 hashes
     # sha256=4ecf8be428f55981e2a188f510ba5f9022bed88f5fb404d7d949f44382201e3d
     input = re.sub(r'sha256=[a-z0-9]+', 'sha256=[DUMMY_HASH]', input)
