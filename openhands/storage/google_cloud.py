d8e029bb58f942002dca5250c9ae115bfcc6de45
Tim O'Farrell <tofarr@gmail.com>
2024-08-23 14:48:19 -0600

    Google cloud storage

--- openhands/storage/google_cloud.py
+++ openhands/storage/google_cloud.py
@@ -1,15 +1,48 @@
-import os
+from typing import List
 
-from minio import Minio
+from google.cloud import storage
 
-from openhands.storage.s3 import S3FileStore
-
-GOOGLE_CLOUD_STORAGE_ENDPOINT = 'storage.googleapis.com'
+from openhands.storage.files import FileStore
 
 
-class GoogleCloudFileStore(S3FileStore):
-    def __init__(self, endpoint: str = GOOGLE_CLOUD_STORAGE_ENDPOINT) -> None:
-        access_key = os.getenv('GOOGLE_CLOUD_STORAGE_ACCESS_KEY')
-        secret_key = os.getenv('GOOGLE_CLOUD_STORAGE_SECRET_KEY')
-        self.bucket = os.getenv('GOOGLE_CLOUD_STORAGE_BUCKET')
-        self.client = Minio(endpoint, access_key, secret_key)
+class GoogleCloudFileStore(FileStore):
+    def __init__(self, bucket_name: str) -> None:
+        """
+        Create a new FileStore. If GOOGLE_APPLICATION_CREDENTIALS is defined in the
+        environment it will be used for authentication. Otherwise access will be 
+        anonymous.
+        """
+        self.storage_client = storage.Client()
+        self.bucket = self.storage_client.bucket(bucket_name)
+
+    def write(self, path: str, contents: str | bytes) -> None:
+        blob = self.bucket.blob(path)
+        with blob.open("w") as f:
+            f.write(contents)
+
+    def read(self, path: str) -> str:
+        blob = self.bucket.blob(path)
+        with blob.open("r") as f:
+            return f.read()
+
+    def list(self, path: str) -> List[str]:
+        # The delimiter logic screens out directories, so we can't use it. :(
+        # For example, given a structure:
+        #   foo/bar/zap.txt
+        #   foo/bar/bang.txt
+        # prefix=None, delimiter="/" yields []
+        # prefix="foo", delimiter="/" yields []
+        blobs = set()
+        for blob in self.bucket.list_blobs(prefix=path):
+            name = blob.name
+            try:
+                index = name.index("/", len(path))
+                name = name[:index]
+            except ValueError:
+                pass
+            blobs.add(name)
+        return list(blobs)
+
+
+    def delete(self, path: str) -> None:
+        self.bucket.delete(path)
