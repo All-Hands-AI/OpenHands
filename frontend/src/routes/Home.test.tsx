60a85f6ddc38bf58451a9a9c0f09a3d43b61698d
amanape <83104063+amanape@users.noreply.github.com>
2024-08-26 20:25:33 +0300

    Add test todos

---
+++ frontend/src/routes/Home.test.tsx
@@ -0,0 +1,12 @@
+import { describe, it } from "vitest";
+
+describe("Home", () => {
+  describe("TaskInput", () => {
+    it.todo("should not render the submit button when text is empty");
+    it.todo("should render the submit button when text is not empty");
+    it.todo("should be able to submit the form with the enter key");
+    it.todo(
+      "should not be able to submit the form with the enter key when text is empty",
+    );
+  });
+});
