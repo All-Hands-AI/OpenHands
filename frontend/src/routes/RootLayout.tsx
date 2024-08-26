99468670deccb3e31f3aae4e6b250f7e07a47ffb
amanape <83104063+amanape@users.noreply.github.com>
2024-08-26 20:47:12 +0300

    home link

--- frontend/src/routes/RootLayout.tsx
+++ frontend/src/routes/RootLayout.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import AllHandsLogo from "#/assets/branding/all-hands-logo.svg?react";
 
 function RootLayout() {
   return (
     <div className="bg-root-primary p-3 h-screen flex gap-3">
       <aside className="px-1 flex flex-col gap-[15px]">
-        <AllHandsLogo width={34} height={23} />
+        <Link to="/">
+          <AllHandsLogo width={34} height={23} />
+        </Link>
         <nav className="py-[18px] flex flex-col gap-[18px]">
           <div className="w-8 h-8 rounded-full bg-red-100" />
           <div className="w-8 h-8 rounded-full bg-green-100" />
