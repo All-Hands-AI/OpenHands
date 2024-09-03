d283420ac2ca3d4f98cdb2b32e44b85f919cbf63
Xingyao Wang <xingyao@all-hands.dev>
2024-09-02 19:28:52 -0500

    feat: add SWE-bench fullset support (#3477)
    * feat: add SWE-bench fullset support
    
    * fix instance image list
    
    * update eval script and documentation
    
    * add push script
    
    * handle the case when ret push is an generator
    
    * update pbar

--- evaluation/swe_bench/scripts/docker/pull_all_eval_docker.sh
+++ evaluation/swe_bench/scripts/docker/pull_all_eval_docker.sh
@@ -6,11 +6,20 @@ LEVEL=$1
 # - base, keyword "sweb.base"
 # - env, keyword "sweb.env"
 # - instance, keyword "sweb.eval"
+SET=$2
 
 if [ -z "$LEVEL" ]; then
-    echo "Usage: $0 <cache_level>"
+    echo "Usage: $0 <cache_level> <set>"
     echo "cache_level: base, env, or instance"
+    echo "set: lite, full"
     exit 1
+fi
+
+if [ -z "$SET" ]; then
+    echo "Usage: $0 <cache_level> <set>"
+    echo "cache_level: base, env, or instance"
+    echo "set: lite, full, default is lite"
+    SET="lite"
 fi
 
 NAMESPACE=$2 # xingyaoww
@@ -18,7 +27,12 @@ if [ -z "$NAMESPACE" ]; then
     echo "Default to namespace: xingyaoww"
     NAMESPACE="xingyaoww"
 fi
-IMAGE_FILE="$(dirname "$0")/all-swebench-lite-instance-images.txt"
+
+if [ "$SET" == "lite" ]; then
+    IMAGE_FILE="$(dirname "$0")/all-swebench-lite-instance-images.txt"
+else
+    IMAGE_FILE="$(dirname "$0")/all-swebench-full-instance-images.txt"
+fi
 
 # Define a pattern based on the level
 case $LEVEL in
