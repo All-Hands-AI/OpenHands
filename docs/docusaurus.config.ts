3a202fad2c251679bf7d6faae0d8289c1c4d0d00
Robert Brennan <contact@rbren.io>
2024-09-04 16:18:33 -0400

    fix favicon

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -5,7 +5,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 const config: Config = {
   title: "OpenHands",
   tagline: "Code Less, Make More",
-  favicon: "img/logo.png",
+  favicon: "img/logo-square.png",
 
   // Set the production url of your site here
   url: "https://docs.all-hands.dev",
