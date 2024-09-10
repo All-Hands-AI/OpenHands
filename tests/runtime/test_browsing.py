f3b2085f9b31af0b3f24ee9a3269525c37ff63b3
mamoodi <mamoodiha@gmail.com>
2024-09-10 14:31:17 -0400

    Reduce runtime tests duration by running them across CPUs (#3779)
    * Reduce runtime tests duration by running them across CPUs
    
    * fix hardcoded image name
    
    * test two cpus
    
    * Test folder change
    
    * Up the CPU to 4 again to test
    
    * Change to 3 CPUs
    
    * Down to 2
    
    * Add param to remove all openhands containers
    
    * Add comment
    
    * Add reruns just in case
    
    * Fix ordering of if

--- tests/runtime/test_browsing.py
+++ tests/runtime/test_browsing.py
@@ -66,7 +66,7 @@ def test_simple_browse(temp_dir, box_class, run_as_openhands):
     logger.info(obs, extra={'msg_type': 'OBSERVATION'})
     assert obs.exit_code == 0
 
-    runtime.close()
+    runtime.close(rm_all_containers=False)
     time.sleep(1)
 
 
@@ -111,5 +111,5 @@ def test_browsergym_eval_env(box_class, temp_dir):
     logger.info(obs, extra={'msg_type': 'OBSERVATION'})
     assert json.loads(obs.content) == [0.0]
 
-    runtime.close()
+    runtime.close(rm_all_containers=False)
     time.sleep(1)
