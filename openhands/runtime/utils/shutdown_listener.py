c2439d63dee7a4ea1d10dd7558da9135e474806c
Tim O'Farrell <tofarr@gmail.com>
2024-09-18 05:41:39 -0600

    WIP

--- openhands/runtime/utils/shutdown_listener.py
+++ openhands/runtime/utils/shutdown_listener.py
@@ -1,7 +1,9 @@
 """
 This module monitors the app for shutdown signals
 """
+import asyncio
 import signal
+import time
 from types import FrameType
 
 from uvicorn.server import HANDLED_SIGNALS
@@ -13,8 +15,8 @@ def _register_signal_handler(sig: signal.Signals):
     original_handler = None
 
     def handler(sig_: int, frame: FrameType | None):
-        global _sleep_permitted
-        _sleep_permitted = True
+        global _should_continue
+        _should_continue = False
         if original_handler:
             original_handler(sig_, frame)
 
@@ -25,11 +27,29 @@ def _register_signal_handlers():
     global _should_continue
     if _should_continue is not None:
         return
-    _should_continue = False
+    _should_continue = True
     for sig in HANDLED_SIGNALS:
         _register_signal_handler(sig)
 
 
 def should_continue() -> bool:
     _register_signal_handlers()
-    return not _should_continue
+    return bool(_should_continue)
+
+
+def sleep_if_should_continue(timeout: float):
+    if(timeout <= 1):
+        time.sleep(timeout)
+        return
+    start_time = time.time()
+    while (time.time() - start_time) < timeout and should_continue():
+        time.sleep(1)
+
+
+async def asleep_if_should_continue(timeout: float):
+    if(timeout <= 1):
+        await asyncio.sleep(timeout)
+        return
+    start_time = time.time()
+    while time.time() - start_time < timeout and should_continue():
+        await asyncio.sleep(1)
