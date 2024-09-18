1eb37fcb093b06787fdaea767a32fe91e60fd7f6
amanape <83104063+amanape@users.noreply.github.com>
2024-09-18 20:08:40 +0400

    Update chat message styles and formatTimestamp to include seconds

--- frontend/__tests__/utils/utils.test.ts
+++ frontend/__tests__/utils/utils.test.ts
@@ -1,5 +1,9 @@
 import { test, expect } from "vitest";
-import { getExtension, removeApiKey } from "../../src/utils/utils";
+import {
+  formatTimestamp,
+  getExtension,
+  removeApiKey,
+} from "../../src/utils/utils";
 
 test("removeApiKey", () => {
   const data = [{ args: { LLM_API_KEY: "key", LANGUAGE: "en" } }];
@@ -10,4 +14,12 @@ test("getExtension", () => {
   expect(getExtension("main.go")).toBe("go");
   expect(getExtension("get-extension.test.ts")).toBe("ts");
   expect(getExtension("directory")).toBe("");
+});
+
+test("formatTimestamp", () => {
+  const morningDate = new Date("2021-10-10T10:10:10.000").toISOString();
+  expect(formatTimestamp(morningDate)).toBe("10/10/2021, 10:10:10");
+
+  const eveningDate = new Date("2021-10-10T22:10:10.000").toISOString();
+  expect(formatTimestamp(eveningDate)).toBe("10/10/2021, 22:10:10");
 });
