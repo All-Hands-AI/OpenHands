b6a69381e43c2e76aa4373ae02cdcf7030e86b13
ketan1741 <ramanetiketan.work@gmail.com>
2024-09-22 04:34:56 -0400

    print in browsing for debug

--- agenthub/browsing_agent/browsing_agent.py
+++ agenthub/browsing_agent/browsing_agent.py
@@ -216,9 +216,9 @@ class BrowsingAgent(Agent):
         prompt = get_prompt(error_prefix, cur_url, cur_axtree_txt, prev_action_str)
         messages.append(Message(role='user', content=[TextContent(text=prompt)]))
 
-        flat_messages = self.llm.format_messages_for_llm(messages)
+        # flat_messages = self.llm.format_messages_for_llm(messages)
 
-        print(f'flat_messages: {flat_messages}')
+        # print(f'flat_messages: {flat_messages}')
 
         response = self.llm.completion(
             messages=self.llm.format_messages_for_llm(messages),
