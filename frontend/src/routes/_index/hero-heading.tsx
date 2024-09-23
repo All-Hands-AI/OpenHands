abb1850e496bc2b399d9de59c16e0c17e281f4f2
amanape <83104063+amanape@users.noreply.github.com>
2024-09-23 13:29:15 +0400

    Add padding to hero description

--- frontend/src/routes/_index/hero-heading.tsx
+++ frontend/src/routes/_index/hero-heading.tsx
@@ -7,17 +7,20 @@ export function HeroHeading() {
       <h1 className="text-[38px] leading-[32px] -tracking-[0.02em]">
         Let&apos;s Start Building!
       </h1>
-      <p className="mx-4 text-sm">
+      <p className="mx-4 text-sm flex flex-col gap-2">
         All Hands makes it easy to build and maintain software using a simple
-        prompt. Not sure how to start?{" "}
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://docs.all-hands.dev/modules/usage/agents"
-          className="text-hyperlink underline underline-offset-[3px]"
-        >
-          Read this
-        </a>
+        prompt.{" "}
+        <span className="">
+          Not sure how to start?{" "}
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://docs.all-hands.dev/modules/usage/agents"
+            className="text-hyperlink underline underline-offset-[3px]"
+          >
+            Read this
+          </a>
+        </span>
       </p>
     </div>
   );
