c68efe364c4b8acccb29e0b52c29161661326f8d
Xingyao Wang <xingyao6@illinois.edu>
2024-10-01 21:48:04 -0400

    remove python unbufferred to fix remote runtime

--- openhands/runtime/remote/runtime.py
+++ openhands/runtime/remote/runtime.py
@@ -144,7 +144,7 @@ class RemoteRuntime(Runtime):
             'image': self.container_image,
             'command': (
                 f'/openhands/micromamba/bin/micromamba run -n openhands '
-                'PYTHONUNBUFFERED=1 poetry run '
+                'poetry run '
                 f'python -u -m openhands.runtime.client.client {self.port} '
                 f'--working-dir {self.config.workspace_mount_path_in_sandbox} '
                 f'{plugin_arg}'
