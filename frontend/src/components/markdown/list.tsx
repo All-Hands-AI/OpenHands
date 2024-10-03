630e5f7259cf7c741274968d8f40c1eac1881197
Vaishakh-SM <vaishakhsm@gmail.com>
2024-10-04 01:11:38 +0530

    Tweaking CSS Styles for lists

--- frontend/src/components/markdown/list.tsx
+++ frontend/src/components/markdown/list.tsx
@@ -4,25 +4,19 @@ import { ExtraProps } from "react-markdown";
 // Custom component to render <ul> in markdown
 export function ul({
   children,
-  className,
 }: React.ClassAttributes<HTMLElement> &
   React.HTMLAttributes<HTMLElement> &
   ExtraProps) {
-  return (
-    <ul className={`list-disc ml-5 pl-2 leading-4 ${className}`}>{children}</ul>
-  );
+  return <ul className="list-disc ml-5 pl-2 whitespace-normal">{children}</ul>;
 }
 
 // Custom component to render <ol> in markdown
 export function ol({
   children,
-  className,
 }: React.ClassAttributes<HTMLElement> &
   React.HTMLAttributes<HTMLElement> &
   ExtraProps) {
   return (
-    <ol className={`list-decimal ml-5 pl-2 leading-4 ${className}`}>
-      {children}
-    </ol>
+    <ol className="list-decimal ml-5 pl-2 whitespace-normal">{children}</ol>
   );
 }
