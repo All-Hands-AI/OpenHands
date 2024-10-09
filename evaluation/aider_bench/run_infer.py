d573a5892914070da70b10b6a25401471ec252e7
Xingyao Wang <xingyao6@illinois.edu>
2024-10-09 15:50:48 -0400

    stop set sid in eval

--- evaluation/aider_bench/run_infer.py
+++ evaluation/aider_bench/run_infer.py
@@ -203,7 +203,7 @@ def process_instance(
     # create sandbox and run the agent
     # =============================================
 
-    runtime: Runtime = create_runtime(config, sid=str(instance.instance_id))
+    runtime: Runtime = create_runtime(config)
 
     initialize_runtime(runtime, instance=instance)
 
