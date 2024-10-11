5fd6d1a95df45778182ee0fa371a7257ae9e89dc
Xingyao Wang <xingyao6@illinois.edu>
2024-10-11 14:25:35 -0400

    refactor: re-organize different runtime implementations into an impl folder

--- openhands/runtime/__init__.py
+++ openhands/runtime/__init__.py
@@ -1,18 +1,18 @@
-from openhands.runtime.e2b.sandbox import E2BBox
+from openhands.runtime.impl.e2b.sandbox import E2BBox
 
 
 def get_runtime_cls(name: str):
     # Local imports to avoid circular imports
     if name == 'eventstream':
-        from openhands.runtime.client.runtime import EventStreamRuntime
+        from openhands.runtime.impl.client.runtime import EventStreamRuntime
 
         return EventStreamRuntime
     elif name == 'e2b':
-        from openhands.runtime.e2b.runtime import E2BRuntime
+        from openhands.runtime.impl.e2b.runtime import E2BRuntime
 
         return E2BRuntime
     elif name == 'remote':
-        from openhands.runtime.remote.runtime import RemoteRuntime
+        from openhands.runtime.impl.remote.runtime import RemoteRuntime
 
         return RemoteRuntime
     else:
