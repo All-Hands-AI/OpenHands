0ea2fbe83968806f1004e1f2de5d1e1490eabe32
Tim O'Farrell <tofarr@gmail.com>
2024-10-14 17:14:04 -0600

    Fix loop issue

--- openhands/server/session/agent_session.py
+++ openhands/server/session/agent_session.py
@@ -133,8 +133,7 @@ class AgentSession:
             await self.security_analyzer.close()
 
         if self.loop:
-            self.loop.call_soon_threadsafe(self.loop.stop)
-            self.loop = None
+            self.loop.stop()
 
         self._closed = True
 
