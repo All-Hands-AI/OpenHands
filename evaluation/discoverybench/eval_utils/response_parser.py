682f151dec282196a102b8bf90502af90c5c3a1b
Abhijeetsingh Meena <abhijeet040403@gmail.com>
2024-10-16 10:52:47 +0530

    fix: modify response parser

--- evaluation/discoverybench/eval_utils/response_parser.py
+++ evaluation/discoverybench/eval_utils/response_parser.py
@@ -1,3 +1,27 @@
+workflow_summary_markers = [
+    'WORKFLOW SUMMARY',
+    'WORKFLOW_SUMMARY',
+    'WORKFLOW-SUMMARY',
+    'Workflow Summary',
+]
+
+final_answer_markers = [
+    'FINAL ANSWER',
+    'FINAL_ANSWER',
+    'FINAL-ANSWER',
+    'Final Answer',
+    'Scientific Hypothesis',
+    'Hypothesis',
+]
+
+next_agent_markers = [
+    'NEXT AGENT',
+    'NEXT-AGENT',
+    'NEXT_AGENT',
+    'FEEDBACK',
+]
+
+
 def extract_between(content, start_markers, end_markers=None):
     for marker in start_markers:
         if marker in content:
@@ -15,45 +39,14 @@ def extract_gen_hypo_from_logs(content):
 
     # extract workflow
     gen_workflow = extract_between(
-        content,
-        [
-            'WORKFLOW_SUMMARY:',
-            '**WORKFLOW_SUMMARY**:',
-            "'WORKFLOW_SUMMARY':",
-            'WORKFLOW SUMMARY',
-        ],
-        [
-            'FINAL_ANSWER:',
-            '**FINAL_ANSWER**:',
-            "'FINAL_ANSWER':",
-            'FINAL ANSWER',
-            'Final Answer',
-            'Scientific Hypothesis',
-        ],
+        content, workflow_summary_markers, final_answer_markers
     )
 
     if not gen_workflow:
         error += 'No Workflow Summary found in the line. | '
 
     # extract final answer
-    gen_hypothesis = extract_between(
-        content,
-        [
-            'FINAL_ANSWER:',
-            '**FINAL_ANSWER**:',
-            "'FINAL_ANSWER':",
-            'Final Answer',
-            'Scientific Hypothesis',
-        ],
-        [
-            'NEXT-AGENT:',
-            '**NEXT-AGENT**:',
-            "'NEXT-AGENT':",
-            'FEEDBACK:',
-            '**FEEDBACK**:',
-            "'FEEDBACK':",
-        ],
-    )
+    gen_hypothesis = extract_between(content, final_answer_markers, next_agent_markers)
 
     if not gen_hypothesis:
         error += 'No Final Answer in the line.'
