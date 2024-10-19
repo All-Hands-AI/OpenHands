5cc16cb82a94dfdff519dd8d74c363e7caa94ec4
sp.wack <83104063+amanape@users.noreply.github.com>
2024-10-19 22:20:54 +0400

    fix(frontend): Fix waitlist logic (#4492)

--- frontend/src/utils/user-is-authenticated.ts
+++ frontend/src/utils/user-is-authenticated.ts
@@ -2,7 +2,7 @@ import { retrieveGitHubUser, isGitHubErrorReponse } from "#/api/github";
 import OpenHands from "#/api/open-hands";
 
 export const userIsAuthenticated = async (ghToken: string | null) => {
-  if (window.__APP_MODE__ === "oss") return true;
+  if (window.__APP_MODE__ !== "saas") return true;
 
   let user: GitHubUser | GitHubErrorReponse | null = null;
   if (ghToken) user = await retrieveGitHubUser(ghToken);
