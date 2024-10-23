b5294d1ccea9e81f0e821e60b89f81f2b082a6ee
Tim O'Farrell <tofarr@gmail.com>
2024-10-23 14:42:32 -0600

    Test fix

--- tests/unit/test_observation_serialization.py
+++ tests/unit/test_observation_serialization.py
@@ -52,7 +52,7 @@ def test_observation_event_props_serialization_deserialization():
             'command': 'ls -l',
             'command_id': 3,
             'hidden': False,
-            'meta': '',
+            'interpreter_details': '',
         },
         'content': 'foo.txt',
     }
@@ -67,7 +67,7 @@ def test_command_output_observation_serialization_deserialization():
             'command': 'ls -l',
             'command_id': 3,
             'hidden': False,
-            'meta': '',
+            'interpreter_details': '',
         },
         'message': 'Command `ls -l` executed with exit code 0.',
         'content': 'foo.txt',
