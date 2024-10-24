de3e080740c49bb38b0e9de70ec17b3b57058978
Robert Brennan <contact@rbren.io>
2024-10-24 15:14:16 -0400

    fix modal

--- openhands/runtime/impl/modal/modal_runtime.py
+++ openhands/runtime/impl/modal/modal_runtime.py
@@ -12,7 +12,10 @@ import tenacity
 from openhands.core.config import AppConfig
 from openhands.core.logger import openhands_logger as logger
 from openhands.events import EventStream
-from openhands.runtime.client.runtime import EventStreamRuntime, LogBuffer
+from openhands.runtime.impl.eventstream.eventstream_runtime import (
+    EventStreamRuntime,
+    LogBuffer,
+)
 from openhands.runtime.plugins import PluginRequirement
 from openhands.runtime.utils.runtime_build import (
     prep_build_folder,
