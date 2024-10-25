b943b1974a64beff4a1752e5b7ff627e17b1b747
Robert Brennan <accounts@rbren.io>
2024-10-25 13:17:12 -0400

    Update openhands/server/listen.py

--- openhands/server/listen.py
+++ openhands/server/listen.py
@@ -868,19 +868,3 @@ class SPAStaticFiles(StaticFiles):
 
 
 app.mount('/', SPAStaticFiles(directory='./frontend/build', html=True), name='dist')
-
-
-
-class SPAStaticFiles(StaticFiles):
-    async def get_response(self, path: str, scope):
-        try:
-            return await super().get_response(path, scope)
-        except Exception:
-            # FIXME: just making this HTTPException doesn't work for some reason
-            return await super().get_response('index.html', scope)
-
-
-app.mount('/', SPAStaticFiles(directory='./frontend/build', html=True), name='dist')
-
-
-
