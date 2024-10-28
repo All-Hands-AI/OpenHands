a404f88215f0d9185964f532c4b525cc3ae2648a
amanape <83104063+amanape@users.noreply.github.com>
2024-10-28 19:26:06 +0400

    Initial setup

--- frontend/src/entry.client.tsx
+++ frontend/src/entry.client.tsx
@@ -6,12 +6,24 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import React, { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import posthog from "posthog-js";
 import { SocketProvider } from "./context/socket";
 import "./i18n";
 import store from "./store";
+
+function PosthogInit() {
+  React.useEffect(() => {
+    posthog.init("phc_3ESMmY9SgqEAGBB6sMGK5ayYHkeUuknH2vP6FmWH9RA", {
+      api_host: "https://us.i.posthog.com",
+      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    });
+  }, []);
+
+  return null;
+}
 
 async function prepareApp() {
   if (
