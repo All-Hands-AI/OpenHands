a3aa2496238f680f0becab18601216409acf70d6
Tim O'Farrell <tofarr@gmail.com>
2024-10-29 16:08:29 -0600

    Feature: Allow CORS for servers running on any port on localhost

--- openhands/server/listen.py
+++ openhands/server/listen.py
@@ -13,6 +13,7 @@ from pathspec.patterns import GitWildMatchPattern
 
 from openhands.security.options import SecurityAnalyzers
 from openhands.server.data_models.feedback import FeedbackDataModel, store_feedback
+from openhands.server.middleware import CustomCORSMiddleware, NoCacheMiddleware
 from openhands.storage import get_file_store
 from openhands.utils.async_utils import call_sync_from_async
 
@@ -29,12 +30,10 @@ from fastapi import (
     WebSocket,
     status,
 )
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, StreamingResponse
 from fastapi.security import HTTPBearer
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-from starlette.middleware.base import BaseHTTPMiddleware
 
 import openhands.agenthub  # noqa F401 (we import this to get the agents registered)
 from openhands.controller.agent import Agent
@@ -77,31 +76,8 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['http://localhost:3001', 'http://127.0.0.1:3001'],
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
-)
 
-
-class NoCacheMiddleware(BaseHTTPMiddleware):
-    """
-    Middleware to disable caching for all routes by adding appropriate headers
-    """
-
-    async def dispatch(self, request, call_next):
-        response = await call_next(request)
-        if not request.url.path.startswith('/assets'):
-            response.headers['Cache-Control'] = (
-                'no-cache, no-store, must-revalidate, max-age=0'
-            )
-            response.headers['Pragma'] = 'no-cache'
-            response.headers['Expires'] = '0'
-        return response
-
-
+app.add_middleware(CustomCORSMiddleware)
 app.add_middleware(NoCacheMiddleware)
 
 security_scheme = HTTPBearer()
---
+++ openhands/server/middleware.py
@@ -0,0 +1,42 @@
+from urllib.parse import urlparse
+
+from engineio import ASGIApp
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class CustomCORSMiddleware(CORSMiddleware):
+    """
+    Custom CORS middleware that allows CORS requests from any origin where the domain is localhost
+    (Allowing any port)
+    """
+
+    def __init__(self, app: ASGIApp):
+        super().__init__(
+            app=app,
+            allow_credentials=True,
+            allow_methods=['*'],
+            allow_headers=['*'],
+        )
+
+    def is_allowed_origin(self, origin: str) -> bool:
+        domain = urlparse(origin).netloc.split(':')[0]
+        if domain in ('localhost', '127.0.0.1'):
+            return True
+        return super().is_allowed_origin(origin)
+
+
+class NoCacheMiddleware(BaseHTTPMiddleware):
+    """
+    Middleware to disable caching for all routes by adding appropriate headers
+    """
+
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        if not request.url.path.startswith('/assets'):
+            response.headers['Cache-Control'] = (
+                'no-cache, no-store, must-revalidate, max-age=0'
+            )
+            response.headers['Pragma'] = 'no-cache'
+            response.headers['Expires'] = '0'
+        return response
