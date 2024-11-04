25d8d4931c79bcd367f3d6f662e516fe5392c44a
Xingyao Wang <xingyao@all-hands.dev>
2024-11-04 03:16:23 +0000

    do not skip generic

--- openhands/events/observation/browse.py
+++ openhands/events/observation/browse.py
@@ -71,7 +71,7 @@ class BrowserOutputObservation(Observation):
             self.axtree_object,
             extra_properties=self.extra_element_properties,
             with_clickable=True,
-            skip_generic=True,
+            skip_generic=False,
             filter_visible_only=filter_visible_only,
         )
         self._axtree_str = cur_axtree_txt
