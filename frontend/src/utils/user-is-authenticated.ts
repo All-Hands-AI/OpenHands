10cc3223f84d479c658fbbe8de01aeac8d528043
Robert Brennan <contact@rbren.io>
2024-11-05 16:36:28 -0500

    revert

--- frontend/src/utils/user-is-authenticated.ts
+++ frontend/src/utils/user-is-authenticated.ts
@@ -1,8 +1,6 @@
 import OpenHands from "#/api/open-hands";
 
 export const userIsAuthenticated = async () => {
-  console.log("is auth");
-  console.log(new Error().stack);
   if (window.__APP_MODE__ === "oss") return true;
 
   try {
