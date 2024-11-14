6fc977dcb4c238e36ce00164951fefb05ba0f1c9
Engel Nyst <enyst@users.noreply.github.com>
2024-11-14 18:15:22 +0100

    add a better prefix

--- openhands/agenthub/codeact_agent/function_calling.py
+++ openhands/agenthub/codeact_agent/function_calling.py
@@ -527,5 +527,5 @@ def action_to_str(action: Action) -> str:
     # for example, when the user runs a command from the terminal
     # we want to show the command in the chat history
     if isinstance(action, CmdRunAction) and action.source == 'user':
-        return f'COMMAND:\n{action.command}'
+        return f'User executed the command:\n{action.command}'
     return ''
