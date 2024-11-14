3efe43cb1eaaa5834350c130d7766d8525582805
Ketan <ramanetiketan.work@gmail.com>
2024-11-14 10:43:43 -0500

    increase runtime init timeout

--- evaluation/aider_bench/run_infer.py
+++ evaluation/aider_bench/run_infer.py
@@ -59,6 +59,8 @@ def get_config(
             timeout=100,
             api_key=os.environ.get('ALLHANDS_API_KEY', None),
             remote_runtime_api_url=os.environ.get('SANDBOX_REMOTE_RUNTIME_API_URL'),
+            keep_runtime_alive=False,
+            remote_runtime_init_timeout=1800
         ),
         # do not mount workspace
         workspace_base=None,
