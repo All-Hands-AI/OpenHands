bf5a8e9ffb8504645c605f340cd7f8f1cdb2ac03
Hoang Tran <descience.thh10@gmail.com>
2024-11-15 14:44:02 +0000

    turn off linter in editor by default

--- openhands/runtime/plugins/agent_skills/file_editor/__init__.py
+++ openhands/runtime/plugins/agent_skills/file_editor/__init__.py
@@ -3,13 +3,6 @@ its __call__.
 The implementation of the `EditTool` class can be found at: https://github.com/All-Hands-AI/openhands-aci/.
 """
 
-from openhands_aci.editor import file_editor as _file_editor
-
-
-def file_editor(*args, **kwargs):
-    # Override enable_linting to True by default
-    kwargs['enable_linting'] = True
-    return _file_editor(*args, **kwargs)
-
+from openhands_aci.editor import file_editor
 
 __all__ = ['file_editor']
