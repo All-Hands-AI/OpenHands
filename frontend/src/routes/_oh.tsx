d8c17fc17c1c0a88216b87371e42854d882a74b5
amanape <83104063+amanape@users.noreply.github.com>
2024-11-20 21:07:18 +0400

    Simplify settings form handling

--- frontend/src/routes/_oh.tsx
+++ frontend/src/routes/_oh.tsx
@@ -235,7 +235,9 @@ export default function MainApp() {
                 models={aiConfigOptions.data?.models}
                 agents={aiConfigOptions.data?.agents}
                 securityAnalyzers={aiConfigOptions.data?.securityAnalyzers}
-                onClose={() => setSettingsModalIsOpen(false)}
+                onClose={() => {
+                  setSettingsModalIsOpen(false);
+                }}
               />
             )}
           </div>
