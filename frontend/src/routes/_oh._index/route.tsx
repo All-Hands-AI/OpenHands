933f5b153f238a874666298a8adca4ffb8dd0ebe
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2024-11-21 16:11:03 -0500

    add skip button to hero page

--- frontend/src/routes/_oh._index/route.tsx
+++ frontend/src/routes/_oh._index/route.tsx
@@ -140,6 +140,13 @@ function Home() {
           />
         </div>
       </div>
+      <button
+        className="absolute bottom-4 right-4 text-white px-4 py-2 rounded cursor-pointer border-1 border-neutral-600"
+        onClick={() => (window.location.href = "/app")}
+        type="button"
+      >
+        Skip
+      </button>
     </div>
   );
 }
