07d2f5be3b799decff3c845087c4b5bcdd2ce0ed
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2024-11-21 16:14:00 -0500

    fixing lint

--- frontend/src/routes/_oh._index/route.tsx
+++ frontend/src/routes/_oh._index/route.tsx
@@ -142,7 +142,9 @@ function Home() {
       </div>
       <button
         className="absolute bottom-4 right-4 text-white px-4 py-2 rounded cursor-pointer border-1 border-neutral-600"
-        onClick={() => (window.location.href = "/app")}
+        onClick={() => {
+          window.location.href = "/app";
+        }}
         type="button"
       >
         Skip
