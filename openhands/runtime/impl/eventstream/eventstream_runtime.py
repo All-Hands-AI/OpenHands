24f29c6baef2f063fa7c138f38859b9a0da6bd66
Engel Nyst <enyst@users.noreply.github.com>
2024-11-29 05:21:43 +0100

    Delete openhands/runtime/impl/eventstream/eventstream_runtime.py

--- openhands/runtime/impl/eventstream/eventstream_runtime.py
+++
@@ -1,8 +0,0 @@
-from openhands.runtime.impl.docker.docker_runtime import DockerRuntime
-
-
-class EventStreamRuntime(DockerRuntime):
-    """Default runtime that uses Docker containers for sandboxed execution."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
