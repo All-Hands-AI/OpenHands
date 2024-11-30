0b1e1aa4648e4d83b0021ffa970a1a1277e4821c
Engel Nyst <enyst@users.noreply.github.com>
2024-11-30 02:57:14 +0100

    Update openhands/runtime/__init__.py

--- openhands/runtime/__init__.py
+++ openhands/runtime/__init__.py
@@ -1,5 +1,4 @@
 from openhands.core.logger import openhands_logger as logger
-from openhands.runtime.impl.e2b.sandbox import E2BBox
 from openhands.runtime.impl.docker.docker_runtime import DockerRuntime
 from openhands.runtime.impl.modal.modal_runtime import ModalRuntime
 from openhands.runtime.impl.remote.remote_runtime import RemoteRuntime
