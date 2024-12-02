365a2e17915d7fa95731c19c78e054a5837a8b02
Tim O'Farrell <tofarr@gmail.com>
2024-12-02 12:06:26 -0700

    WIP

--- tests/unit/test_json.py
+++ tests/unit/test_json.py
@@ -18,6 +18,7 @@ def test_event_serialization_deserialization():
         'args': {
             'content': 'This is a test.',
             'image_urls': None,
+            'secondary_id': None,
             'wait_for_response': False,
         },
     }
@@ -25,7 +26,7 @@ def test_event_serialization_deserialization():
 
 
 def test_array_serialization_deserialization():
-    message = MessageAction(content='This is a test.', wait_for_response=False)
+    message = MessageAction(content='This is a test.', wait_for_response=False, secondary_id='unique_id')
     message._id = 42
     message._timestamp = datetime(2020, 1, 1, 0, 0, 0)
     serialized = json.dumps([message])
@@ -39,6 +40,7 @@ def test_array_serialization_deserialization():
             'args': {
                 'content': 'This is a test.',
                 'image_urls': None,
+                'secondary_id': 'unique_id',
                 'wait_for_response': False,
             },
         }
