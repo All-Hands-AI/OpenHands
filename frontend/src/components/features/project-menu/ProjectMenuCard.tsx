ef69f0c0a24a378dc59cd5824e9e3f255e53d8a4
Tim O'Farrell <tofarr@gmail.com>
2024-12-02 11:44:20 -0700

    Test fix

--- frontend/src/components/features/project-menu/ProjectMenuCard.tsx
+++ frontend/src/components/features/project-menu/ProjectMenuCard.tsx
@@ -47,11 +47,13 @@ Please push the changes to GitHub and open a pull request.
 `,
       imageUrls: [],
       timestamp: new Date().toISOString(),
+      secondaryId: crypto.randomUUID(),
     };
     const event = createChatMessage(
       rawEvent.content,
       rawEvent.imageUrls,
       rawEvent.timestamp,
+      rawEvent.secondaryId,
     );
 
     send(event); // send to socket
