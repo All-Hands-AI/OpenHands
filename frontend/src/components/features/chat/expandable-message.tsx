bdc1b941df67f56a04a32d96ac4324730eabba22
Robert Brennan <contact@rbren.io>
2024-12-03 12:26:41 -0500

    better expando ui

--- frontend/src/components/features/chat/expandable-message.tsx
+++ frontend/src/components/features/chat/expandable-message.tsx
@@ -4,6 +4,8 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { code } from "../markdown/code";
 import { ol, ul } from "../markdown/list";
+import ArrowUp from "#/icons/angle-up-solid.svg?react";
+import ArrowDown from "#/icons/angle-down-solid.svg?react";
 
 interface ExpandableMessageProps {
   id?: string;
@@ -30,23 +32,28 @@ export function ExpandableMessage({
 
   const border = type === "error" ? "border-danger" : "border-neutral-300";
   const textColor = type === "error" ? "text-danger" : "text-neutral-300";
+  let arrowClasses = "h-4 w-4 ml-2 inline";
+  if (type === "error") {
+    arrowClasses += " fill-danger";
+  } else {
+    arrowClasses += " fill-neutral-300";
+  }
 
   return (
     <div
       className={`flex gap-2 items-center justify-start border-l-2 pl-2 my-2 py-2 ${border}`}
     >
-      <div className="text-sm leading-4 flex flex-col gap-2">
-        {headline && <p className={`${textColor} font-bold`}>{headline}</p>}
+      <div className="text-sm leading-4 flex flex-col gap-2 max-w-full">
         {headline && (
-          <button
-            type="button"
-            onClick={() => setShowDetails(!showDetails)}
-            className="cursor-pointer text-left"
-          >
-            {showDetails
-              ? t("EXPANDABLE_MESSAGE$HIDE_DETAILS")
-              : t("EXPANDABLE_MESSAGE$SHOW_DETAILS")}
-          </button>
+          <p className={`${textColor} font-bold`}>{headline}
+            <button
+              type="button"
+              onClick={() => setShowDetails(!showDetails)}
+              className="cursor-pointer text-left"
+            >
+              {showDetails ? <ArrowUp className={arrowClasses} /> : <ArrowDown className={arrowClasses} />}
+            </button>
+          </p>
         )}
         {showDetails && (
           <Markdown
