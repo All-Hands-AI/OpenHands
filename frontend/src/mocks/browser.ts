47c5133d906f5da65d290c998e7f5fbd837ae506
amanape <83104063+amanape@users.noreply.github.com>
2024-12-04 14:02:47 +0400

    Start fixing ws mocks

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -1,5 +1,5 @@
 import { setupWorker } from "msw/browser";
-import { handlers as wsHandlers } from "./handlers.ws";
+import { handlers as wsHandlers } from "./handlers.ws copy";
 import { handlers } from "./handlers";
 
 export const worker = setupWorker(...handlers, ...wsHandlers);
