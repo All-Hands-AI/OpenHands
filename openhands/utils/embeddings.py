91bda0aab078016591880994348917cd779d4b78
Engel Nyst <enyst@users.noreply.github.com>
2024-12-12 10:04:20 +0100

    default to code-3

--- openhands/utils/embeddings.py
+++ openhands/utils/embeddings.py
@@ -106,7 +106,7 @@ class EmbeddingsLoader:
             from llama_index.embeddings.voyageai import VoyageEmbedding
 
             return VoyageEmbedding(
-                model_name='voyage-code-2',
+                model_name='voyage-code-3',
             )
         elif (strategy is not None) and (strategy.lower() == 'none'):
             # TODO: this works but is not elegant enough. The incentive is when
