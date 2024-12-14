08d0ce81c8ecbbfc6d93aab74d54f651ced767a9
Tim O'Farrell <tofarr@gmail.com>
2024-12-13 17:30:26 -0700

    Session init updates

--- openhands/storage/file_item_store.py
+++ openhands/storage/file_item_store.py
@@ -1,20 +1,18 @@
 import json
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Type
 
 from openhands.server.session.session_init_data import SessionInitData
+from openhands.server.shared import file_store
 from openhands.storage.files import FileStore
 from openhands.storage.item_store import ItemStore, T
-from openhands.storage.local import LocalFileStore
 
 
 @dataclass
 class FileItemStore(ItemStore[T]):
     type: Type = SessionInitData
-    files: FileStore = field(
-        default_factory=lambda: LocalFileStore('~/.openhands/session_init_data/')
-    )
-    pattern: str = '{id}.json'
+    files: FileStore = file_store
+    pattern: str = 'session_init_data/{id}.json'
 
     def load(self, id: str) -> T | None:
         file_name = self.pattern.format(id=id)
