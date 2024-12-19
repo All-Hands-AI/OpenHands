9fdd3d38f35afec0544951b3c4d9809617f2f0cd
Tim O'Farrell <tofarr@gmail.com>
2024-12-19 12:01:27 -0700

    Loading settings from OpenHands config

--- openhands/server/routes/settings.py
+++ openhands/server/routes/settings.py
@@ -5,13 +5,13 @@ from fastapi.responses import JSONResponse
 
 from openhands.core.logger import openhands_logger as logger
 from openhands.server.settings import Settings
-from openhands.server.shared import config
+from openhands.server.shared import config, openhands_config
 from openhands.storage.settings_store import SettingsStore
 from openhands.utils.import_utils import get_impl
 
 app = APIRouter(prefix='/api')
 
-SettingsStoreImpl = get_impl(SettingsStore, config.settings_store_class)  # type: ignore
+SettingsStoreImpl = get_impl(SettingsStore, openhands_config.settings_store_class)  # type: ignore
 
 
 @app.get('/settings')
@@ -21,6 +21,9 @@ async def load_settings(
     try:
         settings_store = await SettingsStoreImpl.get_instance(config, github_auth)
         settings = await settings_store.load()
+        if settings:
+            # We don't ever send the api key to the client
+            settings.llm_api_key = None
         return settings
     except Exception as e:
         logger.warning(f'Invalid token: {e}')
@@ -37,8 +40,11 @@ async def store_settings(
 ) -> bool:
     try:
         settings_store = await SettingsStoreImpl.get_instance(config, github_auth)
-        settings = await settings_store.store(settings)
-        return True
+        existing_settings = await settings_store.load()
+        if existing_settings:
+            if not settings.llm_api_key:
+                settings.llm_api_key = existing_settings.llm_api_key
+        return await settings_store.store(settings)
     except Exception as e:
         logger.warning(f'Invalid token: {e}')
         return JSONResponse(
