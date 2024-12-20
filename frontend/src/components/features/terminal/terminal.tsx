946e629dd49fe43799d16540d0c162cbbd280eaa
Robert Brennan <contact@rbren.io>
2024-12-20 17:58:05 -0500

    change terminal status

--- frontend/src/components/features/terminal/terminal.tsx
+++ frontend/src/components/features/terminal/terminal.tsx
@@ -2,23 +2,20 @@ import { useSelector } from "react-redux";
 import { RootState } from "#/store";
 import { useTerminal } from "#/hooks/use-terminal";
 import "@xterm/xterm/css/xterm.css";
-import {
-  useWsClient,
-  WsClientProviderStatus,
-} from "#/context/ws-client-provider";
+import { RUNTIME_INACTIVE_STATES } from "#/types/agent-state";
 
 interface TerminalProps {
   secrets: string[];
 }
 
 function Terminal({ secrets }: TerminalProps) {
-  const { status } = useWsClient();
   const { commands } = useSelector((state: RootState) => state.cmd);
+  const { curAgentState } = useSelector((state: RootState) => state.agent);
 
   const ref = useTerminal({
     commands,
     secrets,
-    disabled: status === WsClientProviderStatus.DISCONNECTED,
+    disabled: RUNTIME_INACTIVE_STATES.includes(curAgentState),
   });
 
   return (
