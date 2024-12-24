05dd9d7a0623683d4ad1d11f07d85bccdd68fe78
Robert Brennan <contact@rbren.io>
2024-12-24 14:38:36 -0500

    revert base runtime

--- openhands/runtime/runtime_manager.py
+++ openhands/runtime/runtime_manager.py
@@ -1,4 +1,3 @@
-from abc import abstractmethod
 from typing import Dict, List, Optional
 
 from openhands.core.config import AppConfig
@@ -7,70 +6,18 @@ from openhands.core.logger import openhands_logger as logger
 from openhands.events import EventStream
 from openhands.runtime import get_runtime_cls
 from openhands.runtime.base import Runtime
-from openhands.runtime.container import ContainerInfo
 from openhands.runtime.plugins import PluginRequirement
-from openhands.runtime.utils.log_streamer import LogStreamer
 from openhands.runtime.utils.singleton import Singleton
 
 
 class RuntimeManager(metaclass=Singleton):
     def __init__(self, config: AppConfig):
         self._runtimes: Dict[str, Runtime] = {}
-        self._containers: Dict[str, ContainerInfo] = {}
         self._config = config
 
     @property
     def config(self) -> AppConfig:
         return self._config
-
-    @abstractmethod
-    def initialize_container(
-        self,
-        runtime_container_image: str,
-        sid: str,
-        plugins: Optional[List[PluginRequirement]] = None,
-        env_vars: Optional[Dict[str, str]] = None,
-        status_callback=None,
-    ) -> ContainerInfo:
-        """Initialize a new container for a runtime.
-
-        Args:
-            runtime_container_image: The Docker image to use
-            sid: The session ID that will be used to generate the container name
-            plugins: Optional list of plugins to enable
-            env_vars: Optional environment variables to set
-            status_callback: Optional callback for status updates
-
-        Returns:
-            ContainerInfo object with connection details
-        """
-        pass
-
-    @abstractmethod
-    def attach_to_container(self, sid: str) -> ContainerInfo:
-        """Attach to an existing container.
-
-        Args:
-            sid: The session ID used to generate the container name
-
-        Returns:
-            ContainerInfo object with connection details
-        """
-        pass
-
-    @abstractmethod
-    def wait_until_alive(
-        self,
-        sid: str,
-        log_streamer: Optional[LogStreamer] = None,
-    ):
-        """Wait until a container is ready to accept connections.
-
-        Args:
-            sid: The session ID used to generate the container name
-            log_streamer: Optional log streamer that must be ready
-        """
-        pass
 
     async def create_runtime(
         self,
