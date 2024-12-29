43462c4e88fb4c33d895d9115fdf6dbe81146cd2
Boxuan Li <liboxuan@connect.hku.hk>
2024-12-28 19:57:19 -0800

    Add stress actions

--- tests/runtime/test_stress_eventstream_runtime.py
+++ tests/runtime/test_stress_eventstream_runtime.py
@@ -11,14 +11,25 @@ from openhands.events.action import CmdRunAction
     TEST_IN_CI,
     reason='This test should only be run locally, not in CI.',
 )
-def test_stress_eventstream_runtime(temp_dir, runtime_cls, repeat=100):
+def test_stress_eventstream_runtime(temp_dir, runtime_cls, repeat=10):
     runtime = _load_runtime(temp_dir, runtime_cls)
 
+    action = CmdRunAction(
+        command='sudo apt-get update && sudo apt-get install -y stress-ng'
+    )
+    logger.info(action, extra={'msg_type': 'ACTION'})
+    obs = runtime.run_action(action)
+    logger.info(obs, extra={'msg_type': 'OBSERVATION'})
+    assert obs.exit_code == 0
+
     for _ in range(repeat):
-        action = CmdRunAction(command='sleep 1')
+        action = CmdRunAction(
+            command='stress-ng --vm 4 --vm-bytes 1G --timeout 300s --metrics --verbose'
+        )
+        action.timeout = 600
         logger.info(action, extra={'msg_type': 'ACTION'})
         obs = runtime.run_action(action)
         logger.info(obs, extra={'msg_type': 'OBSERVATION'})
-        assert obs.exit_code == 0
+        # assert obs.exit_code == 0
 
     _close_test_runtime(runtime)
