778831bc873c804df360e87869c316678832e28f
amanape <83104063+amanape@users.noreply.github.com>
2025-01-03 18:03:40 +0400

    Replace deprecated

--- frontend/src/components/features/github/github-repo-selector.tsx
+++ frontend/src/components/features/github/github-repo-selector.tsx
@@ -105,7 +105,7 @@ export function GitHubRepositorySelector({
       }}
       onSelectionChange={(id) => handleRepoSelection(id?.toString() ?? null)}
       onInputChange={(value) => setSearchQuery(value)}
-      clearButtonProps={{ onClick: handleClearSelection }}
+      clearButtonProps={{ onPress: handleClearSelection }}
       listboxProps={{
         emptyContent,
       }}
