a043ae02695a528c8ee6f0bda8aaa7d02782a336
Boxuan Li <liboxuan@connect.hku.hk>
2025-01-04 13:33:59 -0800

    Remove sudo

--- openhands/runtime/builder/docker.py
+++ openhands/runtime/builder/docker.py
@@ -79,17 +79,17 @@ class DockerRuntimeBuilder(RuntimeBuilder):
             # since the official openhands app image is built from ubuntu, we use
             # ubuntu way to install docker binary
             commands = [
-                'sudo apt-get update',
-                'sudo apt-get install ca-certificates curl',
-                'sudo install -m 0755 -d /etc/apt/keyrings',
-                'sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc',
-                'sudo chmod a+r /etc/apt/keyrings/docker.asc',
+                'apt-get update',
+                'apt-get install ca-certificates curl',
+                'install -m 0755 -d /etc/apt/keyrings',
+                'curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc',
+                'chmod a+r /etc/apt/keyrings/docker.asc',
                 'echo \
                   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
                   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-                  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
-                'sudo apt-get update',
-                'sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin',
+                  tee /etc/apt/sources.list.d/docker.list > /dev/null',
+                'apt-get update',
+                'apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin',
             ]
             for cmd in commands:
                 try:
