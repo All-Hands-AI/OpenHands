73f1b73b18d1125da324bfb9b304eff37379b716
amanape <83104063+amanape@users.noreply.github.com>
2025-01-07 23:37:47 +0400

    Init tests

---
+++ frontend/__tests__/components/shared/modals/account-settings/account-settings-modal.test.tsx
@@ -0,0 +1,8 @@
+import { describe, it } from "vitest";
+
+describe("AccountSettingsModal", () => {
+  describe("Billing", () => {
+    it.todo("should show the current balance");
+    it.todo("should not show the current balance if not in saas mode");
+  });
+});
