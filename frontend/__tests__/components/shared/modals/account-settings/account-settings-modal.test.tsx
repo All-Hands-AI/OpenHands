20632c8975b94864dc0e51d9d6f46f7e03609cf3
amanape <83104063+amanape@users.noreply.github.com>
2025-01-08 10:06:58 +0400

    Display hardcoded balance

--- frontend/__tests__/components/shared/modals/account-settings/account-settings-modal.test.tsx
+++ frontend/__tests__/components/shared/modals/account-settings/account-settings-modal.test.tsx
@@ -1,8 +1,39 @@
-import { describe, it } from "vitest";
+import { screen, within } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import { renderWithProviders } from "test-utils";
+import { AccountSettingsModal } from "#/components/shared/modals/account-settings/account-settings-modal";
+import OpenHands from "#/api/open-hands";
 
 describe("AccountSettingsModal", () => {
   describe("Billing", () => {
-    it.todo("should show the current balance");
-    it.todo("should not show the current balance if not in saas mode");
+    it("should show the current balance", async () => {
+      const getConfigSpy = vi.spyOn(OpenHands, "getConfig");
+      getConfigSpy.mockResolvedValue({
+        APP_MODE: "saas",
+        GITHUB_CLIENT_ID: "123",
+        POSTHOG_CLIENT_KEY: "123",
+      });
+
+      renderWithProviders(<AccountSettingsModal onClose={() => {}} />);
+
+      const balance = await screen.findByTestId("current-balance");
+      expect(balance).toBeInTheDocument();
+
+      await within(balance).findByText("$12.34");
+    });
+
+    it("should not show the current balance if not in saas mode", async () => {
+      const getConfigSpy = vi.spyOn(OpenHands, "getConfig");
+      getConfigSpy.mockResolvedValue({
+        APP_MODE: "oss",
+        GITHUB_CLIENT_ID: "123",
+        POSTHOG_CLIENT_KEY: "123",
+      });
+
+      renderWithProviders(<AccountSettingsModal onClose={() => {}} />);
+
+      const balance = screen.queryByTestId("current-balance");
+      expect(balance).not.toBeInTheDocument();
+    });
   });
 });
--- frontend/src/components/shared/modals/account-settings/account-settings-modal.tsx
+++ frontend/src/components/shared/modals/account-settings/account-settings-modal.tsx
@@ -1,7 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
 import { useGitHubUser } from "#/hooks/query/use-github-user";
 import { useSettings } from "#/hooks/query/use-settings";
 import { ModalBackdrop } from "../modal-backdrop";
 import { AccountSettingsForm } from "./account-settings-form";
+import { useConfig } from "#/hooks/query/use-config";
 
 interface AccountSettingsModalProps {
   onClose: () => void;
@@ -9,13 +11,20 @@ interface AccountSettingsModalProps {
 
 export function AccountSettingsModal({ onClose }: AccountSettingsModalProps) {
   const user = useGitHubUser();
+  const { data: config } = useConfig();
   const { data: settings } = useSettings();
+  const { data } = useQuery({
+    queryKey: [user.data?.login, "balance"],
+    queryFn: async () => ({ balance: 12.34 }),
+    enabled: config?.APP_MODE === "saas",
+  });
 
   // FIXME: Bad practice to use localStorage directly
   const analyticsConsent = localStorage.getItem("analytics-consent");
 
   return (
     <ModalBackdrop onClose={onClose}>
+      {data && <span data-testid="current-balance">${data.balance}</span>}
       <AccountSettingsForm
         onClose={onClose}
         selectedLanguage={settings.LANGUAGE}
