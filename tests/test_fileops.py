8d55039f1ec0283d0a015b56bbec50ce76e22fd7
Robert Brennan <contact@rbren.io>
2025-01-09 13:11:55 -0500

    revert files

--- tests/test_fileops.py
+++ tests/test_fileops.py
@@ -10,36 +10,36 @@ WORKSPACE_BASE = 'workspace'
 
 def test_resolve_path():
     assert (
-        files.resolve_path('test.txt', '/workspace', WORKSPACE_BASE, SANDBOX_PATH_PREFIX)
+        files.resolve_path('test.txt', '/workspace')
         == Path(WORKSPACE_BASE) / 'test.txt'
     )
     assert (
-        files.resolve_path('subdir/test.txt', '/workspace', WORKSPACE_BASE, SANDBOX_PATH_PREFIX)
+        files.resolve_path('subdir/test.txt', '/workspace')
         == Path(WORKSPACE_BASE) / 'subdir' / 'test.txt'
     )
     assert (
-        files.resolve_path(Path(SANDBOX_PATH_PREFIX) / 'test.txt', '/workspace', WORKSPACE_BASE, SANDBOX_PATH_PREFIX)
+        files.resolve_path(Path(SANDBOX_PATH_PREFIX) / 'test.txt', '/workspace')
         == Path(WORKSPACE_BASE) / 'test.txt'
     )
     assert (
         files.resolve_path(
-            Path(SANDBOX_PATH_PREFIX) / 'subdir' / 'test.txt', '/workspace', WORKSPACE_BASE, SANDBOX_PATH_PREFIX
+            Path(SANDBOX_PATH_PREFIX) / 'subdir' / 'test.txt', '/workspace'
         )
         == Path(WORKSPACE_BASE) / 'subdir' / 'test.txt'
     )
     assert (
         files.resolve_path(
-            Path(SANDBOX_PATH_PREFIX) / 'subdir' / '..' / 'test.txt', '/workspace', WORKSPACE_BASE, SANDBOX_PATH_PREFIX
+            Path(SANDBOX_PATH_PREFIX) / 'subdir' / '..' / 'test.txt', '/workspace'
         )
         == Path(WORKSPACE_BASE) / 'test.txt'
     )
     with pytest.raises(PermissionError):
-        files.resolve_path(Path(SANDBOX_PATH_PREFIX) / '..' / 'test.txt', '/workspace', WORKSPACE_BASE, SANDBOX_PATH_PREFIX)
+        files.resolve_path(Path(SANDBOX_PATH_PREFIX) / '..' / 'test.txt', '/workspace')
     with pytest.raises(PermissionError):
-        files.resolve_path(Path('..') / 'test.txt', '/workspace', WORKSPACE_BASE, SANDBOX_PATH_PREFIX)
+        files.resolve_path(Path('..') / 'test.txt', '/workspace')
     with pytest.raises(PermissionError):
-        files.resolve_path(Path('/') / 'test.txt', '/workspace', WORKSPACE_BASE, SANDBOX_PATH_PREFIX)
+        files.resolve_path(Path('/') / 'test.txt', '/workspace')
     assert (
-        files.resolve_path('test.txt', '/workspace/test', WORKSPACE_BASE, SANDBOX_PATH_PREFIX)
+        files.resolve_path('test.txt', '/workspace/test')
         == Path(WORKSPACE_BASE) / 'test' / 'test.txt'
     )
