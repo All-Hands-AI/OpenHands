8a2675814c565d2c5f13974632fc08b3c72bf8b3
amanape <83104063+amanape@users.noreply.github.com>
2025-01-13 21:27:19 +0200

    Fetch settings if user is authenticated

--- frontend/src/hooks/query/use-settings.ts
+++ frontend/src/hooks/query/use-settings.ts
@@ -4,6 +4,8 @@ import posthog from "posthog-js";
 import { AxiosError } from "axios";
 import { DEFAULT_SETTINGS, getLocalStorageSettings } from "#/services/settings";
 import OpenHands from "#/api/open-hands";
+import { useAuth } from "#/context/auth-context";
+import { useIsAuthed } from "./use-is-authed";
 
 const getSettingsQueryFn = async () => {
   try {
@@ -36,9 +38,12 @@ const getSettingsQueryFn = async () => {
 };
 
 export const useSettings = () => {
+  const { data: isAuthed } = useIsAuthed();
+
   const query = useQuery({
     queryKey: ["settings"],
     queryFn: getSettingsQueryFn,
+    enabled: !!isAuthed,
   });
 
   React.useEffect(() => {
