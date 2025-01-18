501824a2950f9430c307c63902ae122dd3b6a008
Xingyao Wang <xingyao6@illinois.edu>
2025-01-17 22:04:29 -0500

    simplify remote stress test a little bit

--- tests/runtime/test_stress_remote_runtime.py
+++ tests/runtime/test_stress_remote_runtime.py
@@ -42,9 +42,7 @@ AGENT_CLS_TO_FAKE_USER_RESPONSE_FN = {
 }
 
 
-def get_config(
-    metadata: EvalMetadata,
-) -> AppConfig:
+def get_config() -> AppConfig:
     assert (
         os.environ.get('SANDBOX_REMOTE_RUNTIME_API_URL') is not None
     ), 'SANDBOX_REMOTE_RUNTIME_API_URL must be set.'
@@ -52,9 +50,7 @@ def get_config(
         os.environ.get('ALLHANDS_API_KEY') is not None
     ), 'ALLHANDS_API_KEY must be set.'
     config = AppConfig(
-        default_agent=metadata.agent_class,
         run_as_openhands=False,
-        max_iterations=metadata.max_iterations,
         runtime='remote',
         sandbox=SandboxConfig(
             base_container_image='python:3.11-bookworm',
@@ -65,6 +61,7 @@ def get_config(
             api_key=os.environ.get('ALLHANDS_API_KEY', None),
             remote_runtime_api_url=os.environ.get('SANDBOX_REMOTE_RUNTIME_API_URL'),
             keep_runtime_alive=False,
+            remote_runtime_resource_factor=1,
         ),
         # do not mount workspace
         workspace_base=None,
@@ -79,131 +76,129 @@ def get_config(
     return config
 
 
-def initialize_runtime(
-    runtime: Runtime,
-):
-    """Initialize the runtime for the agent.
-
-    This function is called before the runtime is used to run the agent.
-    """
-    logger.info('-' * 30)
-    logger.info('BEGIN Runtime Initialization Fn')
-    logger.info('-' * 30)
-    obs: CmdOutputObservation
-
-    action = CmdRunAction(command="""export USER=$(whoami); echo USER=${USER} """)
-    action.set_hard_timeout(600)
-    logger.info(action, extra={'msg_type': 'ACTION'})
-    obs = runtime.run_action(action)
-    logger.info(obs, extra={'msg_type': 'OBSERVATION'})
-    assert_and_raise(obs.exit_code == 0, f'Failed to export USER: {str(obs)}')
-
-    action = CmdRunAction(command='mkdir -p /dummy_dir')
-    action.set_hard_timeout(600)
-    logger.info(action, extra={'msg_type': 'ACTION'})
-    obs = runtime.run_action(action)
-    logger.info(obs, extra={'msg_type': 'OBSERVATION'})
-    assert_and_raise(
-        obs.exit_code == 0,
-        f'Failed to create /dummy_dir: {str(obs)}',
-    )
-
-    with tempfile.TemporaryDirectory() as temp_dir:
-        # Construct the full path for the desired file name within the temporary directory
-        temp_file_path = os.path.join(temp_dir, 'dummy_file')
-        # Write to the file with the desired name within the temporary directory
-        with open(temp_file_path, 'w') as f:
-            f.write('dummy content')
-
-        # Copy the file to the desired location
-        runtime.copy_to(temp_file_path, '/dummy_dir/')
-
-    logger.info('-' * 30)
-    logger.info('END Runtime Initialization Fn')
-    logger.info('-' * 30)
-
-
-def process_instance(
-    instance: pd.Series,
-    metadata: EvalMetadata,
-    reset_logger: bool = True,
-) -> EvalOutput:
-    config = get_config(metadata)
-
-    # Setup the logger properly, so you can run multi-processing to parallelize the evaluation
-    if reset_logger:
-        log_dir = os.path.join(metadata.eval_output_dir, 'infer_logs')
-        reset_logger_for_multiprocessing(logger, instance.instance_id, log_dir)
-    else:
-        logger.info(f'Starting evaluation for instance {instance.instance_id}.')
-
-    runtime = create_runtime(config, headless_mode=False)
-    call_async_from_sync(runtime.connect)
-
-    try:
-        initialize_runtime(runtime)
-
-        instruction = 'dummy instruction'
-        agent = Agent.get_cls(metadata.agent_class)(
-            llm=LLM(config=metadata.llm_config),
-            config=config.get_agent_config(metadata.agent_class),
-        )
-
-        def next_command(*args, **kwargs):
-            return CmdRunAction(command='ls -lah')
-
-        agent.step = MagicMock(side_effect=next_command)
-
-        # Here's how you can run the agent (similar to the `main` function) and get the final task state
-        state: State | None = asyncio.run(
-            run_controller(
-                config=config,
-                initial_user_action=MessageAction(content=instruction),
-                runtime=runtime,
-                fake_user_response_fn=AGENT_CLS_TO_FAKE_USER_RESPONSE_FN[
-                    metadata.agent_class
-                ],
-                agent=agent,
-            )
-        )
-
-        # if fatal error, throw EvalError to trigger re-run
-        if (
-            state.last_error
-            and 'fatal error during agent execution' in state.last_error
-            and 'stuck in a loop' not in state.last_error
-        ):
-            raise EvalException('Fatal error detected: ' + state.last_error)
-
-    finally:
-        runtime.close()
-
-    test_result = {}
-    if state is None:
-        raise ValueError('State should not be None.')
-    histories = [event_to_dict(event) for event in state.history]
-    metrics = state.metrics.get() if state.metrics else None
-
-    # Save the output
-    output = EvalOutput(
-        instance_id=instance.instance_id,
-        instruction=instruction,
-        instance=instance.to_dict(),  # SWE Bench specific
-        test_result=test_result,
-        metadata=metadata,
-        history=histories,
-        metrics=metrics,
-        error=state.last_error if state and state.last_error else None,
-    )
-    return output
-
-
 @pytest.mark.skipif(
     TEST_IN_CI,
     reason='This test should only be run locally, not in CI.',
 )
-def test_stress_remote_runtime(n_eval_workers: int = 64):
+def test_stress_remote_runtime_eval(n_eval_workers: int = 64):
     """Mimic evaluation setting to test remote runtime in a multi-processing setting."""
+
+    def _initialize_runtime(
+        runtime: Runtime,
+    ):
+        """Initialize the runtime for the agent.
+
+        This function is called before the runtime is used to run the agent.
+        """
+        logger.info('-' * 30)
+        logger.info('BEGIN Runtime Initialization Fn')
+        logger.info('-' * 30)
+        obs: CmdOutputObservation
+
+        action = CmdRunAction(command="""export USER=$(whoami); echo USER=${USER} """)
+        action.set_hard_timeout(600)
+        logger.info(action, extra={'msg_type': 'ACTION'})
+        obs = runtime.run_action(action)
+        logger.info(obs, extra={'msg_type': 'OBSERVATION'})
+        assert_and_raise(obs.exit_code == 0, f'Failed to export USER: {str(obs)}')
+
+        action = CmdRunAction(command='mkdir -p /dummy_dir')
+        action.set_hard_timeout(600)
+        logger.info(action, extra={'msg_type': 'ACTION'})
+        obs = runtime.run_action(action)
+        logger.info(obs, extra={'msg_type': 'OBSERVATION'})
+        assert_and_raise(
+            obs.exit_code == 0,
+            f'Failed to create /dummy_dir: {str(obs)}',
+        )
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # Construct the full path for the desired file name within the temporary directory
+            temp_file_path = os.path.join(temp_dir, 'dummy_file')
+            # Write to the file with the desired name within the temporary directory
+            with open(temp_file_path, 'w') as f:
+                f.write('dummy content')
+
+            # Copy the file to the desired location
+            runtime.copy_to(temp_file_path, '/dummy_dir/')
+
+        logger.info('-' * 30)
+        logger.info('END Runtime Initialization Fn')
+        logger.info('-' * 30)
+
+    def _process_instance(
+        instance: pd.Series,
+        metadata: EvalMetadata,
+        reset_logger: bool = True,
+    ) -> EvalOutput:
+        config = get_config()
+
+        # Setup the logger properly, so you can run multi-processing to parallelize the evaluation
+        if reset_logger:
+            log_dir = os.path.join(metadata.eval_output_dir, 'infer_logs')
+            reset_logger_for_multiprocessing(logger, instance.instance_id, log_dir)
+        else:
+            logger.info(f'Starting evaluation for instance {instance.instance_id}.')
+
+        runtime = create_runtime(config, headless_mode=False)
+        call_async_from_sync(runtime.connect)
+
+        try:
+            _initialize_runtime(runtime)
+
+            instruction = 'dummy instruction'
+            agent = Agent.get_cls(metadata.agent_class)(
+                llm=LLM(config=metadata.llm_config),
+                config=config.get_agent_config(metadata.agent_class),
+            )
+
+            def next_command(*args, **kwargs):
+                return CmdRunAction(command='ls -lah')
+
+            agent.step = MagicMock(side_effect=next_command)
+
+            # Here's how you can run the agent (similar to the `main` function) and get the final task state
+            state: State | None = asyncio.run(
+                run_controller(
+                    config=config,
+                    initial_user_action=MessageAction(content=instruction),
+                    runtime=runtime,
+                    fake_user_response_fn=AGENT_CLS_TO_FAKE_USER_RESPONSE_FN[
+                        metadata.agent_class
+                    ],
+                    agent=agent,
+                )
+            )
+
+            # if fatal error, throw EvalError to trigger re-run
+            if (
+                state.last_error
+                and 'fatal error during agent execution' in state.last_error
+                and 'stuck in a loop' not in state.last_error
+            ):
+                raise EvalException('Fatal error detected: ' + state.last_error)
+
+        finally:
+            runtime.close()
+
+        test_result = {}
+        if state is None:
+            raise ValueError('State should not be None.')
+        histories = [event_to_dict(event) for event in state.history]
+        metrics = state.metrics.get() if state.metrics else None
+
+        # Save the output
+        output = EvalOutput(
+            instance_id=instance.instance_id,
+            instruction=instruction,
+            instance=instance.to_dict(),  # SWE Bench specific
+            test_result=test_result,
+            metadata=metadata,
+            history=histories,
+            metrics=metrics,
+            error=state.last_error if state and state.last_error else None,
+        )
+        return output
 
     llm_config = LLMConfig()
     metadata = make_metadata(
@@ -228,4 +223,4 @@ def test_stress_remote_runtime(n_eval_workers: int = 64):
         dummy_instance, output_file, eval_n_limit=len(dummy_instance)
     )
 
-    run_evaluation(instances, metadata, output_file, n_eval_workers, process_instance)
+    run_evaluation(instances, metadata, output_file, n_eval_workers, _process_instance)
