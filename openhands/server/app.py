c7f1229b5f6a549c26ed4398a9e3b9a272b3db11
Tim O'Farrell <tofarr@gmail.com>
2025-01-20 09:37:14 -0700

    Feat: Better mechanism for attaching middleware
    Different environments will need different middleware. This PR moves the code for attaching middleware into the OpenhandsConfig so that it may be configured in an environment specific manner. This will also allow us to move middleware specific to SAAS out of the OSS repo.

--- openhands/server/app.py
+++ openhands/server/app.py
@@ -9,13 +9,6 @@ from fastapi import (
 )
 
 import openhands.agenthub  # noqa F401 (we import this to get the agents registered)
-from openhands.server.middleware import (
-    AttachConversationMiddleware,
-    InMemoryRateLimiter,
-    LocalhostCORSMiddleware,
-    NoCacheMiddleware,
-    RateLimitMiddleware,
-)
 from openhands.server.routes.conversation import app as conversation_api_router
 from openhands.server.routes.feedback import app as feedback_api_router
 from openhands.server.routes.files import app as files_api_router
@@ -27,7 +20,6 @@ from openhands.server.routes.public import app as public_api_router
 from openhands.server.routes.security import app as security_api_router
 from openhands.server.routes.settings import app as settings_router
 from openhands.server.shared import openhands_config, session_manager
-from openhands.utils.import_utils import get_impl
 
 
 @asynccontextmanager
@@ -37,17 +29,7 @@ async def _lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=_lifespan)
-app.add_middleware(
-    LocalhostCORSMiddleware,
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
-)
-
-app.add_middleware(NoCacheMiddleware)
-app.add_middleware(
-    RateLimitMiddleware, rate_limiter=InMemoryRateLimiter(requests=10, seconds=1)
-)
+openhands_config.attach_middleware(app)
 
 
 @app.get('/health')
@@ -63,8 +45,3 @@ app.include_router(conversation_api_router)
 app.include_router(manage_conversation_api_router)
 app.include_router(settings_router)
 app.include_router(github_api_router)
-
-AttachConversationMiddlewareImpl = get_impl(
-    AttachConversationMiddleware, openhands_config.attach_conversation_middleware_path
-)
-app.middleware('http')(AttachConversationMiddlewareImpl(app))
