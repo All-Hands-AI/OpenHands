27429280fd91641720e8ba3241aaebfd02d7b1ac
Tim O'Farrell <tofarr@gmail.com>
2025-01-21 08:02:49 -0700

    Fixed version

--- openhands/server/app.py
+++ openhands/server/app.py
@@ -6,10 +6,10 @@ with warnings.catch_warnings():
 
 from fastapi import (
     FastAPI,
-    __version__,
 )
 
 import openhands.agenthub  # noqa F401 (we import this to get the agents registered)
+from openhands import __version__
 from openhands.server.routes.conversation import app as conversation_api_router
 from openhands.server.routes.feedback import app as feedback_api_router
 from openhands.server.routes.files import app as files_api_router
