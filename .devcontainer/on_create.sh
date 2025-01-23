3de310cc7a227a3865c56b8b6f0f38fc22c91076
Christopher Pereira <kripper@imatronix.com>
2025-01-23 19:36:47 -0300

    Dev Containers support

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -12,3 +12,7 @@ curl -sSL https://install.python-poetry.org | python3.12 -
 
 # See: https://github.com/SmartManoj/Kevin/issues/122#issuecomment-2540482254
 git config --global --add safe.directory /workspaces/OpenHands
+
+# Global .gitignore for VS Code
+echo ".history/" > ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
