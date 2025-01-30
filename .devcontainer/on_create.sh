6fb7631c399d77be5bd95ecf48bcebe7e63bc5a7
Christopher Pereira <kripper@imatronix.com>
2025-01-30 18:27:43 -0300

    Removed comments

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -4,12 +4,6 @@ sudo apt install -y netcat
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 curl -sSL https://install.python-poetry.org | python3.12 -
 
-# WAS working:
-#sudo add-apt-repository -y ppa:deadsnakes/ppa \
-#    && apt-get update \
-#    && apt-get install -y python3.12 python3.12-venv python3.12-dev python3-pip \
-#    && ln -s /usr/bin/python3.12 /usr/bin/python
-
 # See: https://github.com/SmartManoj/Kevin/issues/122#issuecomment-2540482254
 git config --global --add safe.directory /workspaces/OpenHands
 
