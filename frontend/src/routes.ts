7ff53df572a2a5dada6149fb9a21f165a9d9b8c3
amanape <83104063+amanape@users.noreply.github.com>
2025-01-30 19:13:28 +0400

    Setup setting route

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -8,6 +8,7 @@ import {
 export default [
   layout("routes/_oh/route.tsx", [
     index("routes/_oh._index/route.tsx"),
+    route("settings", "routes/settings.tsx"),
     route("conversations/:conversationId", "routes/_oh.app/route.tsx", [
       index("routes/_oh.app._index/route.tsx"),
       route("browser", "routes/_oh.app.browser.tsx"),
