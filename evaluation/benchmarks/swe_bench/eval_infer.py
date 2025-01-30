8dea7085fb4b3c2dc41e2b32bc05d067c9341600
Xingyao Wang <xingyao@all-hands.dev>
2025-01-30 01:17:29 +0000

    add api timeout

--- evaluation/benchmarks/swe_bench/eval_infer.py
+++ evaluation/benchmarks/swe_bench/eval_infer.py
@@ -90,6 +90,7 @@ def get_config(metadata: EvalMetadata, instance: pd.Series) -> AppConfig:
             api_key=os.environ.get('ALLHANDS_API_KEY', None),
             remote_runtime_api_url=os.environ.get('SANDBOX_REMOTE_RUNTIME_API_URL'),
             remote_runtime_init_timeout=3600,
+            remote_runtime_api_timeout=120,
             remote_runtime_resource_factor=get_instance_resource_factor(
                 dataset_name=metadata.dataset,
                 instance_id=instance['instance_id'],
