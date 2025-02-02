7bb44615dc1815318d8853b7f7a1996e9261d633
Boxuan Li <liboxuan@connect.hku.hk>
2025-02-02 11:01:55 -0800

    Enable enable_som_visual_browsing in TAC evaluation harness

--- evaluation/benchmarks/the_agent_company/run_infer.py
+++ evaluation/benchmarks/the_agent_company/run_infer.py
@@ -21,6 +21,7 @@ from openhands.core.config import (
     get_llm_config_arg,
     get_parser,
 )
+from openhands.core.config.agent_config import AgentConfig
 from openhands.core.logger import openhands_logger as logger
 from openhands.core.main import create_runtime, run_controller
 from openhands.events.action import CmdRunAction, MessageAction
@@ -34,6 +35,7 @@ def get_config(
     task_short_name: str,
     mount_path_on_host: str,
     llm_config: LLMConfig,
+    agent_config: AgentConfig,
 ) -> AppConfig:
     config = AppConfig(
         run_as_openhands=False,
@@ -57,6 +59,7 @@ def get_config(
         workspace_mount_path_in_sandbox='/outputs',
     )
     config.set_llm_config(llm_config)
+    config.set_agent_config(agent_config)
     return config
 
 
@@ -214,6 +217,13 @@ if __name__ == '__main__':
     )
     args, _ = parser.parse_known_args()
 
+    #### TODO: parse this from cli and toml ####
+    agent_config: AgentConfig | None = None
+    agent_config = AgentConfig(
+        enable_som_visual_browsing=True,
+    )
+    ############################################
+
     agent_llm_config: LLMConfig | None = None
     if args.agent_llm_config:
         agent_llm_config = get_llm_config_arg(args.agent_llm_config)
@@ -254,7 +264,7 @@ if __name__ == '__main__':
     else:
         temp_dir = tempfile.mkdtemp()
     config: AppConfig = get_config(
-        args.task_image_name, task_short_name, temp_dir, agent_llm_config
+        args.task_image_name, task_short_name, temp_dir, agent_llm_config, agent_config
     )
     runtime: Runtime = create_runtime(config)
     call_async_from_sync(runtime.connect)
