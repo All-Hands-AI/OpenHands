0f039a7dc342e29d9e4431302d0898330df12e33
Tim O'Farrell <tofarr@gmail.com>
2025-02-03 12:08:51 -0700

    Middleware fix

--- openhands/server/listen.py
+++ openhands/server/listen.py
@@ -8,7 +8,7 @@ from openhands.server.middleware import (
     InMemoryRateLimiter,
     LocalhostCORSMiddleware,
     RateLimitMiddleware,
-    SetGitHubUserMiddleware,
+    GitHubTokenMiddleware,
 )
 from openhands.server.static import SPAStaticFiles
 
@@ -29,6 +29,6 @@ base_app.add_middleware(
     rate_limiter=InMemoryRateLimiter(requests=10, seconds=1),
 )
 base_app.middleware('http')(AttachConversationMiddleware(base_app))
-base_app.middleware('http')(SetGitHubUserMiddleware())
+base_app.middleware('http')(GitHubTokenMiddleware(base_app))
 
 app = socketio.ASGIApp(sio, other_asgi_app=base_app)
