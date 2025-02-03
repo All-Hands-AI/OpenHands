aa87fe28bbd10dcc1eb8166d315d5a98bee7eb24
Xingyao Wang <xingyao6@illinois.edu>
2025-02-03 10:05:38 -0500

    add enable retries to all benchmark scripts

--- evaluation/benchmarks/scienceagentbench/run_infer.py
+++ evaluation/benchmarks/scienceagentbench/run_infer.py
@@ -73,6 +73,7 @@ def get_config(
             api_key=os.environ.get('ALLHANDS_API_KEY', None),
             remote_runtime_api_url=os.environ.get('SANDBOX_REMOTE_RUNTIME_API_URL'),
             keep_runtime_alive=False,
+            remote_runtime_enable_retries=True,
         ),
         # do not mount workspace
         workspace_base=None,
