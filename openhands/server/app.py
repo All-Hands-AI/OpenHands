0e5c2756b87619fb2313f88f9163cc40bb5581cb
WTiger <wtiger9218@gmail.com>
2025-02-04 21:01:21 -0800

    Comment openhand route for gitlab

--- openhands/server/app.py
+++ openhands/server/app.py
@@ -14,7 +14,6 @@ from openhands.server.routes.conversation import app as conversation_api_router
 from openhands.server.routes.feedback import app as feedback_api_router
 from openhands.server.routes.files import app as files_api_router
 from openhands.server.routes.github import app as github_api_router
-from openhands.server.routes.gitlab import app as gitlab_api_router
 from openhands.server.routes.manage_conversations import (
     app as manage_conversation_api_router,
 )
@@ -52,5 +51,6 @@ app.include_router(conversation_api_router)
 app.include_router(manage_conversation_api_router)
 app.include_router(settings_router)
 app.include_router(github_api_router)
-app.include_router(gitlab_api_router)
+# TODO for example
+# app.include_router(gitlab_api_router)
 app.include_router(trajectory_router)
