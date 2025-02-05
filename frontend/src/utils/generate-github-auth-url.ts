96575998ece0909da325190e1519b43ce9f4570d
Chuck Butkus <chuck@all-hands.dev>
2025-02-05 13:54:19 -0500

    Force build

--- frontend/src/utils/generate-github-auth-url.ts
+++ frontend/src/utils/generate-github-auth-url.ts
@@ -8,5 +8,6 @@ export const generateGitHubAuthUrl = (clientId: string, requestUrl: URL) => {
   const redirectUri = `${requestUrl.origin}/oauth/github/callback`;
   const baseUrl = `${requestUrl.origin}`.replace("https://", "").replace("http://", "")
   const scope = "openid email profile";
+  const force_build = true
   return `https://auth.${baseUrl}/realms/allhands/protocol/openid-connect/auth?client_id=github&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&state=some-state-value&nonce=222`;
 };
