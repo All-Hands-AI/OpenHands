978f0757a2f1f2fb6466b92bc5d460ccd0cc2795
Tim O'Farrell <tofarr@gmail.com>
2025-02-06 13:36:43 +0000

    Update active conditions

--- frontend/src/hooks/query/use-balance.ts
+++ frontend/src/hooks/query/use-balance.ts
@@ -8,7 +8,6 @@ export const useBalance = (user: string) => {
   return useQuery({
     queryKey: [user, "balance"],
     queryFn: OpenHands.getBalance,
-    // TODO: Should be saas-only and if user is
-    enabled: config?.APP_MODE !== "saas",
+    enabled: config?.APP_MODE === "saas",
   });
 };
