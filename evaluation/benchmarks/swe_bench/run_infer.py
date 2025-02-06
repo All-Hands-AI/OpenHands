a70e97906c97bddb7945856c81c8c19d62127823
Hoang Tran <descience.thh10@gmail.com>
2025-02-06 04:36:27 +0000

    working eval

--- evaluation/benchmarks/swe_bench/run_infer.py
+++ evaluation/benchmarks/swe_bench/run_infer.py
@@ -1,4 +1,5 @@
 import asyncio
+import copy
 import json
 import os
 import tempfile
@@ -33,6 +34,7 @@ from openhands.core.config import (
     SandboxConfig,
     get_llm_config_arg,
     get_parser,
+    load_from_toml,
 )
 from openhands.core.logger import openhands_logger as logger
 from openhands.core.main import create_runtime, run_controller
@@ -155,15 +157,19 @@ def get_config(
             metadata.llm_config, metadata.eval_output_dir, instance['instance_id']
         )
     )
+    config_copy = copy.deepcopy(config)
+    load_from_toml(config_copy)
     agent_config = AgentConfig(
         codeact_enable_jupyter=False,
         codeact_enable_browsing=RUN_WITH_BROWSING,
         codeact_enable_llm_editor=False,
         condenser=metadata.condenser_config,
         enable_prompt_extensions=False,
-        # enable_plan_routing=True,
+        enable_plan_routing=config_copy.get_agent_config().enable_plan_routing,
     )
     config.set_agent_config(agent_config)
+    config.routing_llms = config_copy.routing_llms
+    config.model_routing = config_copy.model_routing
     return config
 
 
