006a6922df07d979eb8b49c408a759fdefa46733
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-02-05 21:23:42 -0500

    relocate filestore init

--- openhands/server/config/config_init.py
+++ openhands/server/config/config_init.py
@@ -1,5 +1,7 @@
 from openhands.core.config.utils import load_app_config
 from openhands.server.config.server_config import load_server_config
+from openhands.storage import get_file_store
 
 config = load_app_config()
 server_config = load_server_config()
+file_store = get_file_store(config.file_store, config.file_store_path)
--- openhands/server/shared.py
+++ openhands/server/shared.py
@@ -1,19 +1,14 @@
 import os
 
 import socketio
-from dotenv import load_dotenv
 
-from openhands.server.config.config_init import config, server_config
+from openhands.server.config.config_init import config, file_store, server_config
 from openhands.server.conversation_manager.conversation_manager import (
     ConversationManager,
 )
-from openhands.storage import get_file_store
 from openhands.storage.conversation.conversation_store import ConversationStore
 from openhands.storage.settings.settings_store import SettingsStore
 from openhands.utils.import_utils import get_impl
-
-load_dotenv()
-file_store = get_file_store(config.file_store, config.file_store_path)
 
 client_manager = None
 redis_host = os.environ.get('REDIS_HOST')
