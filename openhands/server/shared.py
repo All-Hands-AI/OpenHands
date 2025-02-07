bb8dfbdf1dd6eaf5e0c6783f60c91eb76f0a6a10
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-02-07 13:35:50 -0500

    rm config init

--- openhands/server/shared.py
+++ openhands/server/shared.py
@@ -1,14 +1,23 @@
 import os
 
 import socketio
+from dotenv import load_dotenv
 
-from openhands.server.config.config_init import config, file_store, server_config
+from openhands.core.config.utils import load_app_config
+from openhands.server.config.server_config import load_server_config
 from openhands.server.conversation_manager.conversation_manager import (
     ConversationManager,
 )
+from openhands.storage import get_file_store
 from openhands.storage.conversation.conversation_store import ConversationStore
 from openhands.storage.settings.settings_store import SettingsStore
 from openhands.utils.import_utils import get_impl
+
+load_dotenv()
+
+config = load_app_config()
+server_config = load_server_config()
+file_store = get_file_store(config.file_store, config.file_store_path)
 
 client_manager = None
 redis_host = os.environ.get('REDIS_HOST')
