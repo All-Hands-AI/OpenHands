4c8dc8d60303fc5c6018a3e4897d7409f585e5eb
Graham Neubig <neubig@gmail.com>
2025-02-10 14:47:19 -0500

    Apply suggestions from code review

--- openhands/server/auth.py
+++ openhands/server/auth.py
@@ -1,9 +1,5 @@
-import jwt
 from fastapi import Request
-from jwt.exceptions import InvalidTokenError
 from pydantic import SecretStr
-
-from openhands.core.logger import openhands_logger as logger
 
 
 def get_github_token(request: Request) -> SecretStr | None:
