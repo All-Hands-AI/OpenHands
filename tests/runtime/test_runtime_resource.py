2988b10d0088964cdfc5cb5013594683cd55ef3f
Xingyao Wang <xingyao@all-hands.dev>
2025-02-17 12:53:02 -0500

    fix: disable prlimit since limiting --vm breaks nodejs (#6765)

--- tests/runtime/test_runtime_resource.py
+++ tests/runtime/test_runtime_resource.py
@@ -36,78 +36,78 @@ def test_stress_docker_runtime(temp_dir, runtime_cls, repeat=1):
     _close_test_runtime(runtime)
 
 
-def test_stress_docker_runtime_hit_memory_limits(temp_dir, runtime_cls):
-    """Test runtime behavior under resource constraints."""
-    runtime, config = _load_runtime(
-        temp_dir,
-        runtime_cls,
-        docker_runtime_kwargs={
-            'cpu_period': 100000,  # 100ms
-            'cpu_quota': 100000,  # Can use 100ms out of each 100ms period (1 CPU)
-            'mem_limit': '4G',  # 4 GB of memory
-            'memswap_limit': '0',  # No swap
-            'mem_swappiness': 0,  # Disable swapping
-            'oom_kill_disable': False,  # Enable OOM killer
-        },
-        runtime_startup_env_vars={
-            'RUNTIME_MAX_MEMORY_GB': '3',
-        },
-    )
+# def test_stress_docker_runtime_hit_memory_limits(temp_dir, runtime_cls):
+#     """Test runtime behavior under resource constraints."""
+#     runtime, config = _load_runtime(
+#         temp_dir,
+#         runtime_cls,
+#         docker_runtime_kwargs={
+#             'cpu_period': 100000,  # 100ms
+#             'cpu_quota': 100000,  # Can use 100ms out of each 100ms period (1 CPU)
+#             'mem_limit': '4G',  # 4 GB of memory
+#             'memswap_limit': '0',  # No swap
+#             'mem_swappiness': 0,  # Disable swapping
+#             'oom_kill_disable': False,  # Enable OOM killer
+#         },
+#         runtime_startup_env_vars={
+#             'RUNTIME_MAX_MEMORY_GB': '3',
+#         },
+#     )
 
-    action = CmdRunAction(
-        command='sudo apt-get update && sudo apt-get install -y stress-ng'
-    )
-    logger.info(action, extra={'msg_type': 'ACTION'})
-    obs = runtime.run_action(action)
-    logger.info(obs, extra={'msg_type': 'OBSERVATION'})
-    assert obs.exit_code == 0
+#     action = CmdRunAction(
+#         command='sudo apt-get update && sudo apt-get install -y stress-ng'
+#     )
+#     logger.info(action, extra={'msg_type': 'ACTION'})
+#     obs = runtime.run_action(action)
+#     logger.info(obs, extra={'msg_type': 'OBSERVATION'})
+#     assert obs.exit_code == 0
 
-    action = CmdRunAction(
-        command='stress-ng --vm 1 --vm-bytes 6G --timeout 30s --metrics'
-    )
-    action.set_hard_timeout(120)
-    logger.info(action, extra={'msg_type': 'ACTION'})
-    obs = runtime.run_action(action)
-    logger.info(obs, extra={'msg_type': 'OBSERVATION'})
-    assert 'aborted early, out of system resources' in obs.content
-    assert obs.exit_code == 3  # OOM killed!
+#     action = CmdRunAction(
+#         command='stress-ng --vm 1 --vm-bytes 6G --timeout 30s --metrics'
+#     )
+#     action.set_hard_timeout(120)
+#     logger.info(action, extra={'msg_type': 'ACTION'})
+#     obs = runtime.run_action(action)
+#     logger.info(obs, extra={'msg_type': 'OBSERVATION'})
+#     assert 'aborted early, out of system resources' in obs.content
+#     assert obs.exit_code == 3  # OOM killed!
 
-    _close_test_runtime(runtime)
+#     _close_test_runtime(runtime)
 
 
-def test_stress_docker_runtime_within_memory_limits(temp_dir, runtime_cls):
-    """Test runtime behavior under resource constraints."""
-    runtime, config = _load_runtime(
-        temp_dir,
-        runtime_cls,
-        docker_runtime_kwargs={
-            'cpu_period': 100000,  # 100ms
-            'cpu_quota': 100000,  # Can use 100ms out of each 100ms period (1 CPU)
-            'mem_limit': '4G',  # 4 GB of memory
-            'memswap_limit': '0',  # No swap
-            'mem_swappiness': 0,  # Disable swapping
-            'oom_kill_disable': False,  # Enable OOM killer
-        },
-        runtime_startup_env_vars={
-            'RUNTIME_MAX_MEMORY_GB': '7',
-        },
-    )
+# def test_stress_docker_runtime_within_memory_limits(temp_dir, runtime_cls):
+#     """Test runtime behavior under resource constraints."""
+#     runtime, config = _load_runtime(
+#         temp_dir,
+#         runtime_cls,
+#         docker_runtime_kwargs={
+#             'cpu_period': 100000,  # 100ms
+#             'cpu_quota': 100000,  # Can use 100ms out of each 100ms period (1 CPU)
+#             'mem_limit': '4G',  # 4 GB of memory
+#             'memswap_limit': '0',  # No swap
+#             'mem_swappiness': 0,  # Disable swapping
+#             'oom_kill_disable': False,  # Enable OOM killer
+#         },
+#         runtime_startup_env_vars={
+#             'RUNTIME_MAX_MEMORY_GB': '7',
+#         },
+#     )
 
-    action = CmdRunAction(
-        command='sudo apt-get update && sudo apt-get install -y stress-ng'
-    )
-    logger.info(action, extra={'msg_type': 'ACTION'})
-    obs = runtime.run_action(action)
-    logger.info(obs, extra={'msg_type': 'OBSERVATION'})
-    assert obs.exit_code == 0
+#     action = CmdRunAction(
+#         command='sudo apt-get update && sudo apt-get install -y stress-ng'
+#     )
+#     logger.info(action, extra={'msg_type': 'ACTION'})
+#     obs = runtime.run_action(action)
+#     logger.info(obs, extra={'msg_type': 'OBSERVATION'})
+#     assert obs.exit_code == 0
 
-    action = CmdRunAction(
-        command='stress-ng --vm 1 --vm-bytes 6G --timeout 30s --metrics'
-    )
-    action.set_hard_timeout(120)
-    logger.info(action, extra={'msg_type': 'ACTION'})
-    obs = runtime.run_action(action)
-    logger.info(obs, extra={'msg_type': 'OBSERVATION'})
-    assert obs.exit_code == 0
+#     action = CmdRunAction(
+#         command='stress-ng --vm 1 --vm-bytes 6G --timeout 30s --metrics'
+#     )
+#     action.set_hard_timeout(120)
+#     logger.info(action, extra={'msg_type': 'ACTION'})
+#     obs = runtime.run_action(action)
+#     logger.info(obs, extra={'msg_type': 'OBSERVATION'})
+#     assert obs.exit_code == 0
 
-    _close_test_runtime(runtime)
+#     _close_test_runtime(runtime)
