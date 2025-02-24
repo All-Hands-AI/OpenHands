f4c5bbda19f08d19ba4bf139367e5cee0c2b271a
tofarr <tofarr@gmail.com>
2025-02-24 18:47:13 +0000

    Revert "Fix file descriptor leak (#6897)" (#6921)

--- openhands/llm/llm.py
+++ openhands/llm/llm.py
@@ -8,7 +8,6 @@ from typing import Any, Callable
 import requests
 
 from openhands.core.config import LLMConfig
-from openhands.utils.ensure_httpx_close import EnsureHttpxClose
 
 with warnings.catch_warnings():
     warnings.simplefilter('ignore')
@@ -231,9 +230,9 @@ class LLM(RetryMixin, DebugMixin):
 
             # Record start time for latency measurement
             start_time = time.time()
-            with EnsureHttpxClose():
-                # we don't support streaming here, thus we get a ModelResponse
-                resp: ModelResponse = self._completion_unwrapped(*args, **kwargs)
+
+            # we don't support streaming here, thus we get a ModelResponse
+            resp: ModelResponse = self._completion_unwrapped(*args, **kwargs)
 
             # Calculate and record latency
             latency = time.time() - start_time
@@ -288,11 +287,7 @@ class LLM(RetryMixin, DebugMixin):
                     'messages': messages,
                     'response': resp,
                     'args': args,
-                    'kwargs': {
-                        k: v
-                        for k, v in kwargs.items()
-                        if k not in ('messages', 'client')
-                    },
+                    'kwargs': {k: v for k, v in kwargs.items() if k != 'messages'},
                     'timestamp': time.time(),
                     'cost': cost,
                 }
