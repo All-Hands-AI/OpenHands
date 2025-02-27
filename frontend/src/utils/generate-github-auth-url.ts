e6471c1c9a6eb5b082317e608fc38d36d41669da
Chuck Butkus <chuck@all-hands.dev>
2025-02-26 23:23:29 -0500

    Cleanup

--- frontend/src/utils/generate-github-auth-url.ts
+++ frontend/src/utils/generate-github-auth-url.ts
@@ -13,6 +13,5 @@ export const generateGitHubAuthUrl = (clientId: string, requestUrl: URL) => {
     .replace(/(^|\.)staging\.all-hands\.dev$/, ".auth.staging.all-hands.dev")
     .replace(/(^|\.)app\.all-hands\.dev$/, "auth.app.all-hands.dev");
   const scope = "openid email profile";
-  const force_build = true;
-  return `https://${authUrl}/realms/allhands/protocol/openid-connect/auth?client_id=github&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&state=some-state-value&nonce=222`;
+  return `https://${authUrl}/realms/allhands/protocol/openid-connect/auth?client_id=github&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
 };
