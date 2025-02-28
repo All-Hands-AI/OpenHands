987ecd34aacf1d992727a86069774617538d18d8
Christopher Pereira <kripper@imatronix.com>
2025-02-28 16:57:53 -0300

    Removed DevContainer support
    It's working fine (highly recommended).
    Just removed because other devs are not using it officially.

--- .devcontainer/on_run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#USE_HOST_NETWORK=True nohup bash -c '(litellm --config my-configs/litellm.yaml &) ; make run' &> output.log &
-export USE_HOST_NETWORK=True
-bash -c '(nohup bash -c "make run") &> output.log'
