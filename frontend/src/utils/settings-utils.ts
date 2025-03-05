8d0e9469dc6709ec2a41f4795a615f52a4b2c98c
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-03-05 04:45:15 -0500

    Revert "fix: rename provider_tokens to providerTokens to follow camelCase convention"
    This reverts commit 0321a43c4bb76f73d4ea399bf9dad01ca47a47cc.

--- frontend/src/utils/settings-utils.ts
+++ frontend/src/utils/settings-utils.ts
@@ -80,6 +80,6 @@ export const extractSettings = (formData: FormData): Partial<Settings> => {
     CONFIRMATION_MODE,
     SECURITY_ANALYZER,
     ENABLE_DEFAULT_CONDENSER,
-    providerTokens,
+    provider_tokens: providerTokens,
   };
 };
