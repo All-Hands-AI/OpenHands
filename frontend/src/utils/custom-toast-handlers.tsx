82b62488a662209cad8f4111496a894cc5c1b4f1
amanape <83104063+amanape@users.noreply.github.com>
2025-03-07 23:03:35 +0400

    Update styles

--- frontend/src/utils/custom-toast-handlers.tsx
+++ frontend/src/utils/custom-toast-handlers.tsx
@@ -1,46 +1,22 @@
-import toast, { Toast } from "react-hot-toast";
-import CloseIcon from "#/icons/close.svg?react";
-import { cn } from "./utils";
+import { CSSProperties } from "react";
+import toast, { ToastOptions } from "react-hot-toast";
 
-interface CustomToastProps {
-  toast: Toast;
-  variant: "success" | "error";
-}
+const TOAST_STYLE: CSSProperties = {
+  background: "#454545",
+  border: "1px solid #717888",
+  color: "#fff",
+  borderRadius: "4px",
+};
 
-function CustomToast({
-  toast: t,
-  variant,
-  children,
-}: React.PropsWithChildren<CustomToastProps>) {
-  return (
-    <div
-      className={cn(
-        "w-[448px] border-l-4 pl-4 pr-3 py-3 rounded-lg flex gap-3 items-start",
-        t.visible ? "animate-enter" : "animate-leave",
-        variant === "error" && "bg-[#D0493D] border-[#A92418]",
-        variant === "success" && "bg-[#44671F] border-[#5F8F2B]",
-      )}
-    >
-      <p className="grow">{children}</p>
-      <button type="button" onClick={() => toast.dismiss(t.id)}>
-        <CloseIcon />
-      </button>
-    </div>
-  );
-}
+const TOAST_OPTIONS: ToastOptions = {
+  position: "top-right",
+  style: TOAST_STYLE,
+};
 
 export const displayErrorToast = (error: string) => {
-  toast.custom((t) => (
-    <CustomToast toast={t} variant="error">
-      {error}
-    </CustomToast>
-  ));
+  toast.error(error, TOAST_OPTIONS);
 };
 
 export const displaySuccessToast = (message: string) => {
-  toast.custom((t) => (
-    <CustomToast toast={t} variant="success">
-      {message}
-    </CustomToast>
-  ));
+  toast.success(message, TOAST_OPTIONS);
 };
