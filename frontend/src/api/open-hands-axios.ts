c32407ec48f83402febf71da20e32629a65a0d70
marco.dallalba <marco.dalalba@dbserver.com.br>
2025-03-18 20:28:58 -0300

    fix: adding window.location.protocol

--- frontend/src/api/open-hands-axios.ts
+++ frontend/src/api/open-hands-axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const openHands = axios.create({
-  baseURL: `http://${import.meta.env.VITE_BACKEND_BASE_URL || window?.location.host}`,
+  baseURL: `${window.location.protocol}//${import.meta.env.VITE_BACKEND_BASE_URL || window?.location.host}`,
 });
 export const setAuthTokenHeader = (token: string) => {
   openHands.defaults.headers.common.Authorization = `Bearer ${token}`;
