51472715dbc916a3b41670d51990c67dc32a215f
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-03-21 02:40:06 -0400

    fix init auth state

--- frontend/test-utils.tsx
+++ frontend/test-utils.tsx
@@ -66,7 +66,7 @@ export function renderWithProviders(
   function Wrapper({ children }: PropsWithChildren) {
     return (
       <Provider store={store}>
-        <AuthProvider initialProviderTokens={{ github: false, gitlab: false }}>
+        <AuthProvider initialProviderTokens={[]}>
           <QueryClientProvider
             client={
               new QueryClient({
