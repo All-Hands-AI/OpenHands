cc51c742ab7929efada24c6ad6582a2675ffc51c
Chuck Butkus <chuck@all-hands.dev>
2025-03-22 15:20:03 -0400

    Don't display Keycloak error page, just force a new login

--- frontend/src/utils/generate-github-auth-url.ts
+++ frontend/src/utils/generate-github-auth-url.ts
@@ -12,5 +12,10 @@ export const generateGitHubAuthUrl = (clientId: string, requestUrl: URL) => {
     .replace(/(^|\.)app\.all-hands\.dev$/, "auth.app.all-hands.dev")
     .replace(/(^|\.)localhost$/, "auth.staging.all-hands.dev");
   const scope = "openid email profile";
-  return `https://${authUrl}/realms/allhands/protocol/openid-connect/auth?client_id=github&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
+  return (
+    `https://${authUrl}/realms/allhands/protocol/openid-connect/auth?` +
+    `client_id=github&response_type=code&` +
+    `redirect_uri=${encodeURIComponent(redirectUri)}&` +
+    `scope=${encodeURIComponent(scope)}&kc_action=login`
+  );
 };
