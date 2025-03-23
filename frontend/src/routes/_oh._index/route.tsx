6011505e4ad40ce19de2488e10abd3bf8fe14c66
Robert Brennan <contact@rbren.io>
2025-03-22 20:57:06 -0400

    better styles

--- frontend/src/routes/_oh._index/route.tsx
+++ frontend/src/routes/_oh._index/route.tsx
@@ -36,7 +36,7 @@ function Home() {
             user={user || null}
           />
         </div>
-        <div className="w-full flex justify-start">
+        <div className="w-full flex justify-start text-xs">
           <CodeNotInGitHubLink />
         </div>
       </div>
