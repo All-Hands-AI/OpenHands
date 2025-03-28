2f4431b4f4a2c18bd831ab3979df8d265f449255
Tetsu-is <ca02481@cartahd.com>
2025-03-28 16:13:10 +0900

    force using cache

--- openhands/runtime/builder/docker.py
+++ openhands/runtime/builder/docker.py
@@ -141,7 +141,22 @@ class DockerRuntimeBuilder(RuntimeBuilder):
         if platform:
             buildx_cmd.append(f'--platform={platform}')
 
+        # cursor >
+        # # ローカルのイメージをキャッシュとして使用
+        # if target_image_repo:
+        #     try:
+        #         self.docker_client.images.get(target_image_repo)
+        #         buildx_cmd.append(f'--cache-from={target_image_repo}')
+        #         logger.debug(f'Using local image {target_image_repo} as cache')
+        #     except docker.errors.ImageNotFound:
+        #         logger.debug(f'Local image {target_image_repo} not found, skipping cache')
+        # < cursor
+
+        # TODO: (Tetsu-is) fix later. Hard-coded for now.
+        use_local_cache = True
+
         cache_dir = '/tmp/.buildx-cache'
+        logger.info(f'[LOG] is_cache_usable: {self._is_cache_usable(cache_dir)}')
         if use_local_cache and self._is_cache_usable(cache_dir):
             buildx_cmd.extend(
                 [
