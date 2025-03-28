32252a667caf4b065d245067712502cda8233cc5
Tetsu-is <ca02481@cartahd.com>
2025-03-28 13:57:08 +0900

    set None if args not given

--- openhands/resolver/resolve_issue.py
+++ openhands/resolver/resolve_issue.py
@@ -13,7 +13,6 @@ from uuid import uuid4
 from pydantic import SecretStr
 from termcolor import colored
 
-import openhands
 from openhands.controller.state.state import State
 from openhands.core.config import AgentConfig, AppConfig, LLMConfig, SandboxConfig
 from openhands.core.logger import openhands_logger as logger
@@ -646,9 +645,13 @@ def main() -> None:
 
     runtime_container_image = my_args.runtime_container_image
     if runtime_container_image is None and not my_args.is_experimental:
-        runtime_container_image = (
-            f'ghcr.io/all-hands-ai/runtime:{openhands.__version__}-nikolaik'
+        # runtime_container_image = (
+        #     f'ghcr.io/all-hands-ai/runtime:{openhands.__version__}-nikolaik'
+        # )
+        logger.info(
+            'runtime_container_image is None and not my_args.is_experimental then intentionally set to None'
         )
+        runtime_container_image = None
 
     parts = my_args.selected_repo.rsplit('/', 1)
     if len(parts) < 2:
