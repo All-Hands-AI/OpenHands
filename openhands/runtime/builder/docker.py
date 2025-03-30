3abdee0e42a20f29d70a094fdfb896b1ce0235c6
Tetsu-is <hap30305@kwansei.ac.jp>
2025-03-30 20:33:16 +0900

    check local images before build

--- openhands/runtime/builder/docker.py
+++ openhands/runtime/builder/docker.py
@@ -127,6 +127,10 @@ class DockerRuntimeBuilder(RuntimeBuilder):
         target_image_repo, target_image_source_tag = target_image_hash_name.split(':')
         target_image_tag = tags[1].split(':')[1] if len(tags) > 1 else None
 
+
+        # docker image lsで手元のイメージを確認
+        logger.info(f'[LOG] docker image ls: {self.docker_client.images.list()}')
+
         # docker buildxのコマンドを作成
         buildx_cmd = [
             'docker' if not self.is_podman else 'podman',
