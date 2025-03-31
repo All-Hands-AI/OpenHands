18cd5b3d1af00302a0c802fe0643a846aeb39628
Tetsu-is <ca02481@cartahd.com>
2025-03-31 15:04:15 +0900

    print buildx instance

--- openhands/runtime/builder/docker.py
+++ openhands/runtime/builder/docker.py
@@ -205,6 +205,25 @@ class DockerRuntimeBuilder(RuntimeBuilder):
 
         # << subprocess上でdocker image lsを実行する
 
+        # docker buildxのinstanceを確認する
+        print_instance_cmd = ['docker', 'buildx', 'ls']
+        print_instance_process = subprocess.Popen(
+            print_instance_cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+        )
+
+        if print_instance_process.stdout:
+            for line in iter(print_instance_process.stdout.readline, ''):
+                line = line.strip()
+                if line:
+                    stdout_lines.append(line)
+                    logger.info(f'[LOG] docker buildx ls: {line}')
+        else:
+            logger.warning('[LOG] No stdout available from docker buildx ls command')
+
+        # << docker buildxのinstanceを確認する
         # subprocess上でbuildコマンドを実行する >>
         # logger.info('[LOG] build in subprocess for testing')
         # # use docker build not buildx
