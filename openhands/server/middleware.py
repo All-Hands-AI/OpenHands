a7d6b80b13fe2bc16ed21d9eb20fa8de615f7de2
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-04-01 12:16:17 -0400

    rename github refs

--- openhands/server/middleware.py
+++ openhands/server/middleware.py
@@ -185,7 +185,7 @@ class AttachConversationMiddleware(SessionMiddlewareInterface):
         return response
 
 
-class GitHubTokenMiddleware(SessionMiddlewareInterface):
+class ProviderTokenMiddleware(SessionMiddlewareInterface):
     def __init__(self, app):
         self.app = app
 
