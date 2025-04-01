859503bd5cb6b2170bdc4d8a6d8aa28635732874
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-04-01 12:04:11 -0400

    rm unused token header

--- frontend/src/api/open-hands-axios.ts
+++ frontend/src/api/open-hands-axios.ts
@@ -3,12 +3,3 @@ import axios from "axios";
 export const openHands = axios.create({
   baseURL: `${window.location.protocol}//${import.meta.env.VITE_BACKEND_BASE_URL || window?.location.host}`,
 });
-export const setAuthTokenHeader = (token: string) => {
-  openHands.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
-
-export const removeAuthTokenHeader = () => {
-  if (openHands.defaults.headers.common.Authorization) {
-    delete openHands.defaults.headers.common.Authorization;
-  }
-};
