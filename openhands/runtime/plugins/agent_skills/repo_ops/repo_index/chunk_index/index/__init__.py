d2e153d81b93839d38528c6b8d0d96d105ac5776
Hoang Tran <descience.thh10@gmail.com>
2025-04-09 10:40:30 +0000

    use openhands-aci for repo ops

--- openhands/runtime/plugins/agent_skills/repo_ops/repo_index/chunk_index/index/__init__.py
+++
@@ -1,7 +0,0 @@
-# from .code_index import CodeIndex
-from .settings import IndexSettings
-
-# from .simple_faiss import SimpleFaissVectorStore
-
-# __all__ = ['CodeIndex', 'IndexSettings', 'SimpleFaissVectorStore']
-__all__ = ['IndexSettings']
