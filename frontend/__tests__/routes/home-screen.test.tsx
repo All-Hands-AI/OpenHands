6c37d4549bb3481dc081779484545b4b16acec10
amanape <83104063+amanape@users.noreply.github.com>
2025-04-10 18:06:34 +0400

    Pass test

--- frontend/__tests__/routes/home-screen.test.tsx
+++ frontend/__tests__/routes/home-screen.test.tsx
@@ -1,10 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it } from "vitest";
 import HomeScreen from "#/routes/new-home";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 describe("HomeScreen", () => {
   it("should render", () => {
-    render(<HomeScreen />);
+    render(<HomeScreen />, {
+      wrapper: ({ children }) => (
+        <QueryClientProvider client={new QueryClient()}>
+          {children}
+        </QueryClientProvider>
+      ),
+    });
     screen.getByTestId("home-screen");
   });
 });
