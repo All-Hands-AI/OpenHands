515157161a9706294ac5d22ef050dce38e34f1ad
NguyenTheQuang <60279723+quangdz1704@users.noreply.github.com>
2025-04-16 18:36:12 +0700

    feat: add origin (#65)

--- openhands/server/listen.py
+++ openhands/server/listen.py
@@ -1,5 +1,5 @@
 import socketio
-
+import os
 from openhands.server.app import app as base_app
 from openhands.server.listen_socket import sio
 from openhands.server.middleware import (
@@ -13,6 +13,7 @@ from openhands.server.middleware import (
     CheckUserActivationMiddleware,
 )
 from openhands.server.static import SPAStaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 base_app.mount(
     '/', SPAStaticFiles(directory='./frontend/build', html=True), name='dist'
@@ -24,11 +25,19 @@ base_app.middleware('http')(AttachConversationMiddleware(base_app))
 base_app.add_middleware(CheckUserActivationMiddleware)
 base_app.add_middleware(JWTAuthMiddleware)
 
+
+origin_str=os.getenv('ALLOW_ORIGIN')
+if (not origin_str) : 
+    origin_str="*"
+
+origins = [i for i in origin_str.split(",")]
+print('origins', origins)
 base_app.add_middleware(
-    LocalhostCORSMiddleware,
+    CORSMiddleware,
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 base_app.add_middleware(CacheControlMiddleware)
