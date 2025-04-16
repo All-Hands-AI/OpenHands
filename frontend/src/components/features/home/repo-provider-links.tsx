c9af0b89dcb0aa447f8fdf402c84fdfb88859ac4
amanape <83104063+amanape@users.noreply.github.com>
2025-04-16 20:17:31 +0400

    Add installation url

--- frontend/src/components/features/home/repo-provider-links.tsx
+++ frontend/src/components/features/home/repo-provider-links.tsx
@@ -1,10 +1,21 @@
+import { useConfig } from "#/hooks/query/use-config";
+
 export function RepoProviderLinks() {
+  const { data: config } = useConfig();
+
+  const githubHref = config
+    ? `https://github.com/apps/${config.APP_SLUG}/installations/new`
+    : "";
+  const gitlabHref = config
+    ? `https://github.com/apps/${config.APP_SLUG}/installations/new`
+    : "";
+
   return (
     <div className="flex flex-col text-sm underline underline-offset-2 text-content-2 gap-4 w-fit">
-      <a href="http://" target="_blank" rel="noopener noreferrer">
+      <a href={githubHref} target="_blank" rel="noopener noreferrer">
         Add GitHub repos
       </a>
-      <a href="http://" target="_blank" rel="noopener noreferrer">
+      <a href={gitlabHref} target="_blank" rel="noopener noreferrer">
         Add GitLab repos
       </a>
     </div>
