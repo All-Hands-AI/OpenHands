733f5172b7f3ad75b696938633c53a0046166de1
Robert Brennan <contact@rbren.io>
2025-04-21 17:10:50 -0400

    Revert "Fix sidebar.ts format to match Docusaurus requirements"
    This reverts commit 1fb9a194a07ee30d4cecb96168d3feef75409200.

--- docs/api/sidebar.ts
+++ docs/api/sidebar.ts
@@ -1,19 +1,14 @@
-import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 import openApiSidebar from './openapi/sidebar';
 
-const sidebars: SidebarsConfig = {
-  apiSidebar: [
-    {
-      type: "doc",
-      id: "index",
-      label: "API Overview",
-    },
-    {
-      type: "category",
-      label: "REST API Reference",
-      items: openApiSidebar,
-    },
-  ],
-};
-
-export default sidebars;
+export default [
+  {
+    type: "doc",
+    id: "index",
+    label: "API Overview",
+  },
+  {
+    type: "category",
+    label: "REST API Reference",
+    items: openApiSidebar,
+  },
+];
