71bdbf626e6e1e162ad23b19959305961f8882b0
amanape <83104063+amanape@users.noreply.github.com>
2025-04-21 17:34:24 +0400

    Form tests

--- frontend/__tests__/routes/app-settings.test.tsx
+++ frontend/__tests__/routes/app-settings.test.tsx
@@ -1,10 +1,12 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import userEvent from "@testing-library/user-event";
 import AppSettingsScreen from "#/routes/app-settings";
 import OpenHands from "#/api/open-hands";
 import { MOCK_DEFAULT_USER_SETTINGS } from "#/mocks/handlers";
 import { AuthProvider } from "#/context/auth-context";
+import { AvailableLanguages } from "#/i18n";
 
 const renderAppSettingsScreen = () =>
   render(<AppSettingsScreen />, {
@@ -51,5 +53,81 @@ describe("Content", () => {
       expect(analytics).toBeChecked();
       expect(sound).toBeChecked();
     });
+  });
+
+  it("should render the language options", async () => {
+    renderAppSettingsScreen();
+
+    const language = await screen.findByTestId("language-input");
+    await userEvent.click(language);
+
+    AvailableLanguages.forEach((lang) => {
+      const option = screen.getByText(lang.label);
+      expect(option).toBeInTheDocument();
+    });
+  });
+});
+
+describe("Form submission", () => {
+  it("should submit the form with the correct values", async () => {
+    const saveSettingsSpy = vi.spyOn(OpenHands, "saveSettings");
+    const getSettingsSpy = vi.spyOn(OpenHands, "getSettings");
+    getSettingsSpy.mockResolvedValue(MOCK_DEFAULT_USER_SETTINGS);
+
+    renderAppSettingsScreen();
+
+    const language = await screen.findByTestId("language-input");
+    const analytics = await screen.findByTestId("enable-analytics-switch");
+    const sound = await screen.findByTestId(
+      "enable-sound-notifications-switch",
+    );
+
+    expect(language).toHaveValue("English");
+    expect(analytics).not.toBeChecked();
+    expect(sound).not.toBeChecked();
+
+    // change language
+    await userEvent.click(language);
+    const norsk = screen.getByText("Norsk");
+    await userEvent.click(norsk);
+    expect(language).toHaveValue("Norsk");
+
+    // toggle options
+    await userEvent.click(analytics);
+    expect(analytics).toBeChecked();
+    await userEvent.click(sound);
+    expect(sound).toBeChecked();
+
+    // submit the form
+    const submit = await screen.findByTestId("submit-button");
+    await userEvent.click(submit);
+    expect(saveSettingsSpy).toHaveBeenCalledWith(
+      expect.objectContaining({
+        language: "no",
+        user_consents_to_analytics: true,
+        enable_sound_notifications: true,
+      }),
+    );
+  });
+
+  it("should only enable the submit button when there are changes", async () => {
+    const getSettingsSpy = vi.spyOn(OpenHands, "getSettings");
+    getSettingsSpy.mockResolvedValue(MOCK_DEFAULT_USER_SETTINGS);
+
+    renderAppSettingsScreen();
+
+    const submit = await screen.findByTestId("submit-button");
+    expect(submit).toBeDisabled();
+
+    const language = await screen.findByTestId("language-input");
+    await userEvent.click(language);
+    const norsk = screen.getByText("Norsk");
+    await userEvent.click(norsk);
+    expect(submit).not.toBeDisabled();
+
+    await userEvent.click(language);
+    const english = screen.getByText("English");
+    await userEvent.click(english);
+    expect(submit).toBeDisabled();
   });
 });
--- frontend/src/routes/app-settings.tsx
+++ frontend/src/routes/app-settings.tsx
@@ -1,12 +1,34 @@
 import { SettingsDropdownInput } from "#/components/features/settings/settings-dropdown-input";
+import { useSaveSettings } from "#/hooks/mutation/use-save-settings";
 import { useSettings } from "#/hooks/query/use-settings";
 import { AvailableLanguages } from "#/i18n";
+import { DEFAULT_SETTINGS } from "#/services/settings";
 
 function AppSettingsScreen() {
+  const { mutate: saveSettings } = useSaveSettings();
   const { data: settings } = useSettings();
 
+  const formAction = (formData: FormData) => {
+    const languageLabel = formData.get("language-input")?.toString();
+    const languageValue = AvailableLanguages.find(
+      ({ label }) => label === languageLabel,
+    )?.value;
+    const language = languageValue || DEFAULT_SETTINGS.LANGUAGE;
+
+    const enableAnalytics =
+      formData.get("enable-analytics-switch")?.toString() === "on";
+    const enableSoundNotifications =
+      formData.get("enable-sound-notifications-switch")?.toString() === "on";
+
+    saveSettings({
+      LANGUAGE: language,
+      user_consents_to_analytics: enableAnalytics,
+      ENABLE_SOUND_NOTIFICATIONS: enableSoundNotifications,
+    });
+  };
+
   return (
-    <div data-testid="app-settings-screen">
+    <form data-testid="app-settings-screen" action={formAction}>
       {settings && (
         <SettingsDropdownInput
           testId="language-input"
@@ -25,6 +47,7 @@ function AppSettingsScreen() {
         <input
           type="checkbox"
           data-testid="enable-analytics-switch"
+          name="enable-analytics-switch"
           defaultChecked={!!settings.USER_CONSENTS_TO_ANALYTICS}
         />
       )}
@@ -33,10 +56,15 @@ function AppSettingsScreen() {
         <input
           type="checkbox"
           data-testid="enable-sound-notifications-switch"
+          name="enable-sound-notifications-switch"
           defaultChecked={!!settings.ENABLE_SOUND_NOTIFICATIONS}
         />
       )}
-    </div>
+
+      <button data-testid="submit-button" type="submit">
+        Submit
+      </button>
+    </form>
   );
 }
 
