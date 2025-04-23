9a8b44378b4cb659de565ce4fb582692e5716967
Xingyao Wang <xingyao@all-hands.dev>
2025-04-22 19:38:43 -0500

    add missing imports

--- openhands/events/observation/__init__.py
+++ openhands/events/observation/__init__.py
@@ -24,6 +24,7 @@ from openhands.events.observation.files import (
 from openhands.events.observation.observation import Observation
 from openhands.events.observation.reject import UserRejectObservation
 from openhands.events.observation.success import SuccessObservation
+from openhands.events.observation.mcp import MCPObservation
 
 __all__ = [
     'Observation',
