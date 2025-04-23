411d7e999c1847b1646fc1ebbe94c81e75d6ab8f
Tim O'Farrell <tofarr@gmail.com>
2025-04-23 09:51:07 -0600

    WIP

--- openhands/server/utils.py
+++ openhands/server/utils.py
@@ -3,6 +3,8 @@ from fastapi import Request
 
 from openhands.storage.conversation.conversation_store import ConversationStore
 from openhands.server.shared import ConversationStoreImpl, config
+from openhands.server.user_auth import get_user_auth
+from openhands.integrations.provider import ProviderType
 
 
 async def get_conversation_store(request: Request) -> ConversationStore:
@@ -11,7 +13,9 @@ async def get_conversation_store(request: Request) -> ConversationStore:
         return conversation_store
     user_auth = await get_user_auth(request)
     user_id = await user_auth.get_user_id()
-    github_user_id = await user_auth.get_provider_tokens().get(ProviderType.GITHUB)
+    provider_tokens = await user_auth.get_provider_tokens()
+    github_token = provider_tokens.get(ProviderType.GITHUB)
+    github_user_id = github_token.token.get_secret_value() if github_token else None
     conversation_store = await ConversationStoreImpl.get_instance(
         config, user_id, github_user_id
     )
