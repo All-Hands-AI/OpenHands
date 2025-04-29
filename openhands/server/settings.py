8e8f6ddf93520be27cdb9101b4a90a07a59879ce
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-04-28 21:42:59 -0400

    use updated model

--- openhands/server/settings.py
+++ openhands/server/settings.py
@@ -5,6 +5,8 @@ from pydantic import (
     SecretStr,
 )
 
+from openhands.integrations.provider import ProviderToken
+from openhands.integrations.service_types import ProviderType
 from openhands.storage.data_models.settings import Settings
 
 
@@ -13,7 +15,7 @@ class POSTSettingsModel(Settings):
     Settings for POST requests
     """
 
-    provider_tokens: dict[str, str] = {}
+    provider_tokens: dict[ProviderType, ProviderToken] = {}
 
 
 class POSTSettingsCustomSecrets(BaseModel):
