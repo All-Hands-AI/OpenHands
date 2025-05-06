abdf8ced0f3ddfa6f1475a6d72d57ed4ea387862
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-05-06 17:36:00 -0400

    add new trigger type from chrome extension

--- openhands/storage/data_models/conversation_metadata.py
+++ openhands/storage/data_models/conversation_metadata.py
@@ -8,6 +8,7 @@ class ConversationTrigger(Enum):
     GUI = 'gui'
     SUGGESTED_TASK = 'suggested_task'
     REMOTE_API_KEY = 'openhands_api'
+    CHROME_EXTENSION = 'chrome_extension'
 
 
 @dataclass
