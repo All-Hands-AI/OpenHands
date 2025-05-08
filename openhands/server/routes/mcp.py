88e23b7460aa8341345ee8eec62326902c822dab
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-05-08 13:29:27 -0400

    create pr tool template

--- openhands/server/routes/mcp.py
+++ openhands/server/routes/mcp.py
@@ -1,19 +1,20 @@
-from mcp.server.fastmcp import FastMCP
-from mcp.server.fastmcp import Context
+from fastmcp import FastMCP, Context
+from typing import Annotated
+from pydantic import Field
 
 
 mcp_server = FastMCP("mcp")
 
 
 @mcp_server.tool()
-def add(a: int, b: int) -> int:
-    """Adds two numbers."""
-    return a + b
-
-
-
-@mcp_server.tool()
-def create_pr(source_branch: str, target_branch: str, ctx: Context):
+def create_pr(
+    repo_name: Annotated[str, Field(description="GitHub repository ({{owner}}/{{repo}})")], 
+    source_branch: Annotated[str, Field(description="Source branch on repo")], 
+    target_branch: Annotated[str, Field(description="Target branch on repo")],
+    ctx: Context
+) -> str:
     """Open a PR in GitHub"""
+    request = ctx.get_http_request()
+    print(request)
 
-    pass
+    return "pr was created successfully"
