292a024ecf2d646d7f52afac695af863393097ce
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-05-08 12:58:09 -0400

    add fastmcp library

--- openhands/server/routes/mcp.py
+++ openhands/server/routes/mcp.py
@@ -1,4 +1,5 @@
 from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp import Context
 
 
 mcp_server = FastMCP("mcp")
@@ -8,3 +9,11 @@ mcp_server = FastMCP("mcp")
 def add(a: int, b: int) -> int:
     """Adds two numbers."""
     return a + b
+
+
+
+@mcp_server.tool()
+def create_pr(source_branch: str, target_branch: str, ctx: Context):
+    """Open a PR in GitHub"""
+
+    pass
