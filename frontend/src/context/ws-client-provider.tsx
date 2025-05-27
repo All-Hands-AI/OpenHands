0dcd5e9d306a674895fb6c1cbf017120836bd5d4
sp.wack <83104063+amanape@users.noreply.github.com>
2025-05-27 18:12:56 +0400

    hotfix(frontend): Clear error message on WS connect (#8725)

--- frontend/src/context/ws-client-provider.tsx
+++ frontend/src/context/ws-client-provider.tsx
@@ -162,6 +162,7 @@ export function WsClientProvider({
 
   function handleConnect() {
     setStatus(WsClientProviderStatus.CONNECTED);
+    removeErrorMessage();
   }
 
   function handleMessage(event: Record<string, unknown>) {
