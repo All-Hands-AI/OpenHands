170c9ae1292f2aaaa2c9ae12880cf3e24382e920
Tim O'Farrell <tofarr@gmail.com>
2025-06-01 08:03:12 -0600

    Reduce the noise from loggers

--- openhands/core/logger.py
+++ openhands/core/logger.py
@@ -385,10 +385,20 @@ if LOG_TO_FILE:
     )  # default log to project root
     openhands_logger.debug(f'Logging to file in: {LOG_DIR}')
 
-# Exclude LiteLLM from logging output
-logging.getLogger('LiteLLM').disabled = True
-logging.getLogger('LiteLLM Router').disabled = True
-logging.getLogger('LiteLLM Proxy').disabled = True
+# Exclude loquacious loggers
+LOQUACIOUS_LOGGERS = [
+    'engineio',
+    'engineio.server',
+    'LiteLLM',
+    'LiteLLM Router',
+    'LiteLLM Proxy',
+    'socketio',
+    'socketio.client',
+    'socketio.server',
+]
+
+for logger_name in LOQUACIOUS_LOGGERS:
+    logging.getLogger(logger_name).setLevel('WARNING')
 
 
 class LlmFileHandler(logging.FileHandler):
