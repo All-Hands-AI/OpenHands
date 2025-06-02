531f91ae21451f40d67b19c0362b76b6f5a5b4cd
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-06-02 10:38:43 +0530

    use string for consistency

--- openhands/runtime/action_execution_server.py
+++ openhands/runtime/action_execution_server.py
@@ -284,7 +284,7 @@ class ActionExecutor:
 
         await wait_all(
             (self._init_plugin(plugin) for plugin in self.plugins_to_load),
-            timeout=int(os.environ.get('INIT_PLUGIN_TIMEOUT', 120)),
+            timeout=int(os.environ.get('INIT_PLUGIN_TIMEOUT', '120')),
         )
         logger.debug('All plugins initialized')
 
