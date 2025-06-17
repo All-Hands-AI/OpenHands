f4d34e53fbe77ee56d9667d391a710fcc6ea81a7
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-06-17 13:55:53 -0400

    Revert "Fix unit test: ensure RuntimeError is raised with correct message for authentication failures"
    This reverts commit 6a6e79d95f9c0105e6cd252e682ffcd8093dc7da.

--- openhands/runtime/base.py
+++ openhands/runtime/base.py
@@ -609,7 +609,7 @@ fi
             )
             repository = await provider_handler.verify_repo_provider(repo_name)
         except AuthenticationError:
-            raise RuntimeError('Git provider authentication issue when cloning repo')
+            raise Exception('Git provider authentication issue when getting remote URL')
 
         provider = repository.git_provider
         repo_name = repository.full_name
