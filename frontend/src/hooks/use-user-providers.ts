6a87aab5d6d8901b64758d25934ef2a5b383d572
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-06-19 00:44:57 -0400

    memoize providers

--- frontend/src/hooks/use-user-providers.ts
+++ frontend/src/hooks/use-user-providers.ts
@@ -1,8 +1,15 @@
 import { convertRawProvidersToList } from "#/utils/convert-raw-providers-to-list";
+import React from "react";
 import { useSettings } from "./query/use-settings";
 
 export const useUserProviders = () => {
   const { data: settings } = useSettings();
+
+  const providers = React.useMemo(
+    () => convertRawProvidersToList(settings?.PROVIDER_TOKENS_SET),
+    [settings?.PROVIDER_TOKENS_SET],
+  );
+
   return {
     providers: convertRawProvidersToList(settings?.PROVIDER_TOKENS_SET),
   };
