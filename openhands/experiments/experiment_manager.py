12367ac5db205a263fee9c94a6169f3c731ffd57
Valerie Chen <valerie@all-hands.dev>
2025-07-09 15:41:46 -0400

    Update experiment_manager.py

--- openhands/experiments/experiment_manager.py
+++ openhands/experiments/experiment_manager.py
@@ -1,25 +1,22 @@
 import os
 
 from openhands.core.config.agent_config import AgentConfig
+from openhands.server.session.conversation_init_data import ConversationInitData
 from openhands.utils.import_utils import get_impl
 
 
 class ExperimentManager:
     @staticmethod
     def run_conversation_variant_test(
-        user_id, conversation_id, conversation_settings
-    ):
+         user_id: str, conversation_id: str, conversation_settings: ConversationInitData
+    ) -> ConversationInitData:
         return conversation_settings
 
     @staticmethod
     def run_agent_config_variant_test(
-        user_id, conversation_id, agent_config
+        user_id: str, conversation_id: str, agent_config: AgentConfig
     ):
-        modified_config = agent_config.model_copy()
-        custom_system_prompt = os.environ.get('OPENHANDS_SYSTEM_PROMPT_FILENAME')
-        if custom_system_prompt:
-            modified_config.system_prompt_filename = custom_system_prompt
-        return modified_config
+        return agent_config
 
 
 experiment_manager_cls = os.environ.get(
