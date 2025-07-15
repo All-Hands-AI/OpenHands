03c547dcd93da0bc0fea60dc44db8297f8292e56
Tim O'Farrell <tofarr@gmail.com>
2025-07-15 11:44:57 -0600

    Test fix

--- openhands/events/observation/agent.py
+++ openhands/events/observation/agent.py
@@ -76,7 +76,6 @@ class RecallObservation(Observation):
     date: str = ''
     custom_secrets_descriptions: dict[str, str] = field(default_factory=dict)
     conversation_instructions: str = ''
-    cwd: str = ''
 
     # knowledge
     microagent_knowledge: list[MicroagentKnowledge] = field(default_factory=list)
