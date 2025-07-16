387318385ce3a47527d28f838ae2efe426ccb35d
Mislav Lukach <mislavlukach@gmail.com>
2025-07-16 18:38:51 +0200

    feat(ui): tab component (#9673)
    Co-authored-by: amanape <83104063+amanape@users.noreply.github.com>

--- openhands-ui/index.ts
+++ openhands-ui/index.ts
@@ -12,6 +12,7 @@ export { Scrollable } from "./components/scrollable/Scrollable";
 export { ToastManager } from "./components/toast/ToastManager";
 export { toasterMessages } from "./components/toast/Toast";
 export { Toggle } from "./components/toggle/Toggle";
+export { Tabs } from "./components/tabs/Tabs";
 export { Tooltip } from "./components/tooltip/Tooltip";
 export { Typography } from "./components/typography/Typography";
 
