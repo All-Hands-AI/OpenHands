b3d81bc7b8ef0f6c9cecfe9a8cc5c0df5fa0816d
amanape <83104063+amanape@users.noreply.github.com>
2025-07-16 20:12:53 +0400

    Export toaster messages and manager

--- openhands-ui/index.ts
+++ openhands-ui/index.ts
@@ -9,6 +9,8 @@ export { InteractiveChip } from "./components/interactive-chip/InteractiveChip";
 export { RadioGroup } from "./components/radio-group/RadioGroup";
 export { RadioOption } from "./components/radio-group/RadioOption";
 export { Scrollable } from "./components/scrollable/Scrollable";
+export { ToastManager } from "./components/toast/ToastManager";
+export { toasterMessages } from "./components/toast/Toast";
 export { Toggle } from "./components/toggle/Toggle";
 export { Tooltip } from "./components/tooltip/Tooltip";
 export { Typography } from "./components/typography/Typography";
