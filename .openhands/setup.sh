fa689f7b0967169d5a2cd04ebc9ef07009a22f9e
Carlos Freund <carlosfreund@gmail.com>
2025-07-29 20:41:48 +0100

    trigger pre-commit on setup.

--- .openhands/setup.sh
+++ .openhands/setup.sh
@@ -9,3 +9,9 @@ if ! command -v docker >/dev/null 2>&1; then
 fi
 
 make build
+
+#run a minimal pre-commit script to trigger its setup. Otherwhise it will cause
+#the first commit to time-out.
+poetry run pre-commit run --files ./README.md --config ./dev_config/python/.pre-commit-config.yaml
+
+
