1d207c8cd790f8a5a67236a5ec9d2dd6bb6d961c
Hiep Le <69354317+hieptl@users.noreply.github.com>
2025-07-30 21:33:02 +0700

    feat: the action panel / canvas can be toggled on the conversation page. (#9992)

---
+++ frontend/src/components/features/chat/chat-actions.tsx
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import BlockDrawerLeftIcon from "#/icons/block-drawer-left.svg?react";
+import { setIsRightPanelShown } from "#/state/conversation-slice";
+import { RootState } from "#/store";
+import { cn } from "#/utils/utils";
+
+export function ChatActions() {
+  const isRightPanelShown = useSelector(
+    (state: RootState) => state.conversation.isRightPanelShown,
+  );
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex items-center justify-end">
+      <button
+        type="button"
+        className={cn(
+          "flex items-center justify-center w-[26px] h-[26px] rounded-lg cursor-pointer",
+          isRightPanelShown && "bg-[#25272D] hover:bg-tertiary",
+        )}
+        onClick={() => {
+          dispatch(setIsRightPanelShown(!isRightPanelShown));
+        }}
+      >
+        <BlockDrawerLeftIcon
+          width={18}
+          height={18}
+          className={cn(
+            "text-white",
+            !isRightPanelShown && "text-[#9299AA] hover:text-white",
+          )}
+        />
+      </button>
+    </div>
+  );
+}
