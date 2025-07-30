32b3f53ac9cc75eb7962aa8e9ff0abbca613901e
Carlos Freund <carlosfreund@gmail.com>
2025-07-30 06:38:36 +0100

    blank lines

--- .openhands/setup.sh
+++ .openhands/setup.sh
@@ -13,5 +13,3 @@ make build
 #run a minimal pre-commit script to trigger its setup. Otherwhise it will cause
 #the first commit to time-out.
 poetry run pre-commit run --files ./README.md --config ./dev_config/python/.pre-commit-config.yaml
-
-
