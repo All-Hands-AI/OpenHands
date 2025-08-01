51501e34e1096f1ade304c1828cca9ab9077632c
Xingyao Wang <xingyao@all-hands.dev>
2025-08-01 17:17:59 -0400

    revert builder

--- openhands/runtime/builder/docker.py
+++ openhands/runtime/builder/docker.py
@@ -98,22 +98,16 @@ class DockerRuntimeBuilder(RuntimeBuilder):
             logger.info(
                 'No docker binary available inside openhands-app container, trying to download online...'
             )
-            # Following official Docker installation documentation for Debian
-            # https://docs.docker.com/engine/install/debian/
             commands = [
                 'apt-get update',
-                'apt-get install -y ca-certificates curl',
+                'apt-get install -y ca-certificates curl gnupg',
                 'install -m 0755 -d /etc/apt/keyrings',
                 'curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc',
                 'chmod a+r /etc/apt/keyrings/docker.asc',
-                'CODENAME=$(. /etc/os-release && echo "$VERSION_CODENAME")',
-                'if [ "$CODENAME" = "noble" ]; then \
-                    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-                    bookworm stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null; \
-                else \
-                    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-                    $CODENAME stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null; \
-                fi',
+                'echo \
+                  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+                  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                  tee /etc/apt/sources.list.d/docker.list > /dev/null',
                 'apt-get update',
                 'apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin',
             ]
