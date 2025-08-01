60ccdc120c475a2b0e11703ce1a95172962fd6da
hieptl <hieptl.developer@gmail.com>
2025-08-01 17:17:06 +0700

    feat: display task suggestions

--- frontend/src/routes/conversation.tsx
+++ frontend/src/routes/conversation.tsx
@@ -90,7 +90,7 @@ function AppContent() {
     if (width <= 1024) {
       return (
         <div className="flex flex-col gap-3 overflow-auto w-full">
-          <div className="rounded-xl overflow-hidden border border-neutral-600 w-full bg-base-secondary min-h-[494px]">
+          <div className="rounded-xl overflow-hidden border border-neutral-600 w-full bg-base min-h-[494px]">
             <ChatInterface />
           </div>
           <div className="h-full w-full min-h-[494px]">
@@ -104,7 +104,7 @@ function AppContent() {
         orientation={Orientation.HORIZONTAL}
         className="grow h-full min-h-0 min-w-0"
         initialSize={500}
-        firstClassName="rounded-xl overflow-hidden border border-neutral-600 bg-base-secondary"
+        firstClassName="rounded-xl overflow-hidden border border-neutral-600 bg-base"
         secondClassName="flex flex-col overflow-hidden"
         firstChild={<ChatInterface />}
         secondChild={<ConversationTabs />}
