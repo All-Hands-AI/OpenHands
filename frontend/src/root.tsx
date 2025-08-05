6c65da01c0a833bc3657394a9b307ac5fb34fd72
Jacob Ambroz <codenamespivey@sixtyoneeightyai.com>
2025-08-05 11:06:14 -0500

    c

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -31,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export const meta: MetaFunction = () => [
-  { title: "OpenHands" },
+  { title: "MojoCode" },
   { name: "description", content: "Make Cool Shit!!" },
 ];
 
