4421fb166cb36ab74eac9692b0e912d15341b1f1
Hiep Le <69354317+hieptl@users.noreply.github.com>
2025-08-06 20:28:03 +0700

    feat: redesign the home page. (#9899)

---
+++ frontend/src/components/features/home/recent-conversations/conversation-status-indicator.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from "react";
+import { ConversationStatus } from "#/types/conversation-status";
+import { cn } from "#/utils/utils";
+
+interface ConversationStatusIndicatorProps {
+  conversationStatus: ConversationStatus;
+}
+
+export function ConversationStatusIndicator({
+  conversationStatus,
+}: ConversationStatusIndicatorProps) {
+  const conversationStatusBackgroundColor = useMemo(() => {
+    switch (conversationStatus) {
+      case "STOPPED":
+        return "bg-[#FF684E]";
+      case "RUNNING":
+        return "bg-[#1FBD53]";
+      case "STARTING":
+        return "bg-[#ffffff]";
+      default:
+        return "bg-[#ffffff]";
+    }
+  }, [conversationStatus]);
+
+  return (
+    <div
+      className={cn(
+        "w-1.5 h-1.5 rounded-full",
+        conversationStatusBackgroundColor,
+      )}
+    />
+  );
+}
