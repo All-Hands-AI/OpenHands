aa9ba846bc668035cee53622c7d442495703633b
hieptl <hieptl.developer@gmail.com>
2025-08-06 23:48:05 +0700

    refactor: background color

--- frontend/src/routes/root-layout.tsx
+++ frontend/src/routes/root-layout.tsx
@@ -198,7 +198,7 @@ export default function MainApp() {
   return (
     <div
       data-testid="root-layout"
-      className="h-screen p-3 pl-0 lg:min-w-[1024px] flex flex-col md:flex-row"
+      className="h-screen p-3 pl-0 lg:min-w-[1024px] flex flex-col md:flex-row bg-base"
     >
       <Sidebar />
 
