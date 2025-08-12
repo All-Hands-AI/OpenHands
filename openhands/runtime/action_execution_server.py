868f434f979dc9b439aa843024ea1ff4ef7102c5
Chuck Butkus <chuck@all-hands.dev>
2025-08-12 12:49:30 -0400

    Fix logging

--- openhands/runtime/action_execution_server.py
+++ openhands/runtime/action_execution_server.py
@@ -675,10 +675,10 @@ class ActionExecutor:
 
 
 if __name__ == '__main__':
-    logger.warning('Starting Action Execution Server')
-    logger.warning('Arguments passed to script:')
+    logger.debug('Starting Action Execution Server')
+    logger.debug('Arguments passed to script:')
     for i, arg in enumerate(sys.argv):
-        logger.warning(f'Argument {i}: {arg}')
+        logger.debug(f'Argument {i}: {arg}')
     parser = argparse.ArgumentParser()
     parser.add_argument('port', type=int, help='Port to listen on')
     parser.add_argument('--working-dir', type=str, help='Working directory')
