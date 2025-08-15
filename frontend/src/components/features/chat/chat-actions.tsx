2b60c3e96ee07a412de773b469d6a1985d3c708b
sp.wack <83104063+amanape@users.noreply.github.com>
2025-08-15 17:49:49 +0400

    Revert "feat(ui): run web server (#10328)"
    This reverts commit da1f867a37e08def11701f4db70057414f1106aa.

--- frontend/src/components/features/chat/chat-actions.tsx
+++ frontend/src/components/features/chat/chat-actions.tsx
@@ -1,16 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useTranslation } from "react-i18next";
 import BlockDrawerLeftIcon from "#/icons/block-drawer-left.svg?react";
-import PlayIcon from "#/icons/play-solid.svg?react";
-import {
-  setIsRightPanelShown,
-  setMessageToSend,
-} from "#/state/conversation-slice";
+import { setIsRightPanelShown } from "#/state/conversation-slice";
 import { RootState } from "#/store";
 import { cn } from "#/utils/utils";
 import { ChatActionTooltip } from "./chat-action-tooltip";
-import { I18nKey } from "#/i18n/declaration";
-import { RUN_SERVER_SUGGESTION } from "#/utils/suggestions/repo-suggestions";
 
 export function ChatActions() {
   const isRightPanelShown = useSelector(
@@ -22,14 +15,9 @@ export function ChatActions() {
   );
 
   const dispatch = useDispatch();
-  const { t } = useTranslation();
-
-  const onRunClick = () => {
-    dispatch(setMessageToSend(RUN_SERVER_SUGGESTION));
-  };
 
   return (
-    <div className="flex items-center justify-end gap-x-4">
+    <div className="flex items-center justify-end">
       <ChatActionTooltip tooltip="Drawer" ariaLabel="Drawer">
         <button
           type="button"
@@ -55,20 +43,6 @@ export function ChatActions() {
           />
         </button>
       </ChatActionTooltip>
-      <button
-        type="button"
-        onClick={onRunClick}
-        className={cn(
-          "bg-white py-0.75 pl-2 pr-3.5 rounded-full cursor-pointer",
-          `flex flex-row items-center`,
-          "text-black",
-        )}
-      >
-        <PlayIcon className="w-4.5 h-4.5 text-inherit" />
-        <span className="text-inherit text-sm font-medium">
-          {t(I18nKey.CONVERSATION$RUN)}
-        </span>
-      </button>
     </div>
   );
 }
