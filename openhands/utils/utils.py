3bca13ecf63142d0d546be823f48482880ffecd0
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-08-18 13:05:14 -0700

    fix filestore init

--- openhands/utils/utils.py
+++ openhands/utils/utils.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 from openhands.core.config.openhands_config import OpenHandsConfig
 from openhands.llm.llm_registry import LLMRegistry
 from openhands.server.services.conversation_stats import ConversationStats
-from openhands.server.shared import file_store
+from openhands.storage import get_file_store
 from openhands.storage.data_models.settings import Settings
 
 
@@ -31,6 +31,13 @@ def create_registry_and_convo_stats(
 
     agent_cls = user_settings.agent if user_settings else None
     llm_registry = LLMRegistry(user_config, agent_cls)
+    file_store = get_file_store(
+        file_store_type=config.file_store,
+        file_store_path=config.file_store_path,
+        file_store_web_hook_url=config.file_store_web_hook_url,
+        file_store_web_hook_headers=config.file_store_web_hook_headers,
+        file_store_web_hook_batch=config.file_store_web_hook_batch,
+    )
     convo_stats = ConversationStats(file_store, sid, user_id)
     llm_registry.subscribe(convo_stats.register_llm)
     return llm_registry, convo_stats, user_config
