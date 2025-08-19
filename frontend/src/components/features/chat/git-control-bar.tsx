8634c2249974d5372cd9fcdda33d8ccd521c397c
hieptl <hieptl.developer@gmail.com>
2025-08-19 20:36:56 +0700

    feat: add the scroll buttons to scroll the git control bar

--- frontend/src/components/features/chat/git-control-bar.tsx
+++ frontend/src/components/features/chat/git-control-bar.tsx
@@ -5,10 +5,14 @@ import { GitControlBarBranchButton } from "./git-control-bar-branch-button";
 import { GitControlBarPullButton } from "./git-control-bar-pull-button";
 import { GitControlBarPushButton } from "./git-control-bar-push-button";
 import { GitControlBarPrButton } from "./git-control-bar-pr-button";
+import { GitScrollButton } from "./git-scroll-button";
 import { useActiveConversation } from "#/hooks/query/use-active-conversation";
+import { useHorizontalScroll } from "#/hooks/use-horizontal-scroll";
 import { Provider } from "#/types/settings";
 import { I18nKey } from "#/i18n/declaration";
 import { GitControlBarTooltipWrapper } from "./git-control-bar-tooltip-wrapper";
+import ChevronLeftSmallIcon from "#/icons/chevron-left-small.svg?react";
+import ChevronRightSmallIcon from "#/icons/chevron-right-small.svg?react";
 
 interface GitControlBarProps {
   onSuggestionsClick: (value: string) => void;
@@ -24,6 +28,8 @@ export function GitControlBar({
   optimisticUserMessage,
 }: GitControlBarProps) {
   const { t } = useTranslation();
+  const { scrollContainerRef, canScrollLeft, canScrollRight, scroll } =
+    useHorizontalScroll();
 
   const { data: conversation } = useActiveConversation();
 
@@ -74,66 +80,94 @@ export function GitControlBar({
   const shouldShowTooltipForGitActions = !!tooltipMessage;
 
   return (
-    <div className="flex flex-row gap-2.5 items-center flex-wrap">
-      <GitControlBarTooltipWrapper
-        tooltipMessage={t(I18nKey.COMMON$GIT_TOOLS_DISABLED_CONTENT)}
-        testId="git-control-bar-repo-button-tooltip"
-        shouldShowTooltip={!hasRepository}
-      >
-        <GitControlBarRepoButton
-          selectedRepository={selectedRepository}
-          gitProvider={gitProvider}
-        />
-      </GitControlBarTooltipWrapper>
+    <div className="flex flex-row items-center">
+      {/* Left Arrow */}
+      {canScrollLeft && (
+        <GitScrollButton
+          direction="left"
+          onClick={() => scroll("left")}
+          ariaLabel="Scroll left"
+        >
+          <ChevronLeftSmallIcon width={24} height={24} color="#A3A3A3" />
+        </GitScrollButton>
+      )}
 
-      <GitControlBarTooltipWrapper
-        tooltipMessage={t(I18nKey.COMMON$GIT_TOOLS_DISABLED_CONTENT)}
-        testId="git-control-bar-branch-button-tooltip"
-        shouldShowTooltip={!hasRepository}
+      {/* Scrollable Container */}
+      <div
+        ref={scrollContainerRef}
+        className="flex flex-row gap-2.5 items-center overflow-x-auto flex-nowrap relative scrollbar-hide"
       >
-        <GitControlBarBranchButton
-          selectedBranch={selectedBranch}
-          selectedRepository={selectedRepository}
-          gitProvider={gitProvider}
-        />
-      </GitControlBarTooltipWrapper>
+        <GitControlBarTooltipWrapper
+          tooltipMessage={t(I18nKey.COMMON$GIT_TOOLS_DISABLED_CONTENT)}
+          testId="git-control-bar-repo-button-tooltip"
+          shouldShowTooltip={!hasRepository}
+        >
+          <GitControlBarRepoButton
+            selectedRepository={selectedRepository}
+            gitProvider={gitProvider}
+          />
+        </GitControlBarTooltipWrapper>
 
-      <GitControlBarTooltipWrapper
-        tooltipMessage={tooltipMessage}
-        testId="git-control-bar-pull-button-tooltip"
-        shouldShowTooltip={shouldShowTooltipForGitActions}
-      >
-        <GitControlBarPullButton
-          onSuggestionsClick={onSuggestionsClick}
-          isEnabled={isButtonEnabled}
-        />
-      </GitControlBarTooltipWrapper>
+        <GitControlBarTooltipWrapper
+          tooltipMessage={t(I18nKey.COMMON$GIT_TOOLS_DISABLED_CONTENT)}
+          testId="git-control-bar-branch-button-tooltip"
+          shouldShowTooltip={!hasRepository}
+        >
+          <GitControlBarBranchButton
+            selectedBranch={selectedBranch}
+            selectedRepository={selectedRepository}
+            gitProvider={gitProvider}
+          />
+        </GitControlBarTooltipWrapper>
 
-      <GitControlBarTooltipWrapper
-        tooltipMessage={tooltipMessage}
-        testId="git-control-bar-push-button-tooltip"
-        shouldShowTooltip={shouldShowTooltipForGitActions}
-      >
-        <GitControlBarPushButton
-          onSuggestionsClick={onSuggestionsClick}
-          isEnabled={isButtonEnabled}
-          hasRepository={hasRepository}
-          currentGitProvider={gitProvider}
-        />
-      </GitControlBarTooltipWrapper>
+        <GitControlBarTooltipWrapper
+          tooltipMessage={tooltipMessage}
+          testId="git-control-bar-pull-button-tooltip"
+          shouldShowTooltip={shouldShowTooltipForGitActions}
+        >
+          <GitControlBarPullButton
+            onSuggestionsClick={onSuggestionsClick}
+            isEnabled={isButtonEnabled}
+          />
+        </GitControlBarTooltipWrapper>
 
-      <GitControlBarTooltipWrapper
-        tooltipMessage={tooltipMessage}
-        testId="git-control-bar-pr-button-tooltip"
-        shouldShowTooltip={shouldShowTooltipForGitActions}
-      >
-        <GitControlBarPrButton
-          onSuggestionsClick={onSuggestionsClick}
-          isEnabled={isButtonEnabled}
-          hasRepository={hasRepository}
-          currentGitProvider={gitProvider}
-        />
-      </GitControlBarTooltipWrapper>
+        <GitControlBarTooltipWrapper
+          tooltipMessage={tooltipMessage}
+          testId="git-control-bar-push-button-tooltip"
+          shouldShowTooltip={shouldShowTooltipForGitActions}
+        >
+          <GitControlBarPushButton
+            onSuggestionsClick={onSuggestionsClick}
+            isEnabled={isButtonEnabled}
+            hasRepository={hasRepository}
+            currentGitProvider={gitProvider}
+          />
+        </GitControlBarTooltipWrapper>
+
+        <GitControlBarTooltipWrapper
+          tooltipMessage={tooltipMessage}
+          testId="git-control-bar-pr-button-tooltip"
+          shouldShowTooltip={shouldShowTooltipForGitActions}
+        >
+          <GitControlBarPrButton
+            onSuggestionsClick={onSuggestionsClick}
+            isEnabled={isButtonEnabled}
+            hasRepository={hasRepository}
+            currentGitProvider={gitProvider}
+          />
+        </GitControlBarTooltipWrapper>
+      </div>
+
+      {/* Right Arrow */}
+      {canScrollRight && (
+        <GitScrollButton
+          direction="right"
+          onClick={() => scroll("right")}
+          ariaLabel="Scroll right"
+        >
+          <ChevronRightSmallIcon width={24} height={24} color="#B1B9D3" />
+        </GitScrollButton>
+      )}
     </div>
   );
 }
