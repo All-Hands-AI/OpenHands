aabe1d957abddcf1d6f89229ea090e0d71d029ee
hieptl <hieptl.developer@gmail.com>
2025-08-28 16:25:06 +0700

    refactor: user-actions.test.tsx

--- frontend/__tests__/components/user-actions.test.tsx
+++ frontend/__tests__/components/user-actions.test.tsx
@@ -67,28 +67,6 @@ describe("UserActions", () => {
     expect(screen.getByTestId("user-avatar")).toBeInTheDocument();
   });
 
-  it("should toggle the user menu when the user avatar is clicked", async () => {
-    renderWithRouter(
-      <UserActions
-        onLogout={onLogoutMock}
-        user={{ avatar_url: "https://example.com/avatar.png" }}
-      />,
-    );
-
-    const userAvatar = screen.getByTestId("user-avatar");
-    await user.click(userAvatar);
-
-    expect(
-      screen.getByTestId("account-settings-context-menu"),
-    ).toBeInTheDocument();
-
-    await user.click(userAvatar);
-
-    expect(
-      screen.queryByTestId("account-settings-context-menu"),
-    ).not.toBeInTheDocument();
-  });
-
   it("should call onLogout and close the menu when the logout option is clicked", async () => {
     renderWithRouter(
       <UserActions
@@ -104,9 +82,6 @@ describe("UserActions", () => {
     await user.click(logoutOption);
 
     expect(onLogoutMock).toHaveBeenCalledOnce();
-    expect(
-      screen.queryByTestId("account-settings-context-menu"),
-    ).not.toBeInTheDocument();
   });
 
   it("should NOT show context menu when user is not authenticated and avatar is clicked", async () => {
