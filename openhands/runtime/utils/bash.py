45986887e62a1b6378aa3b5ec056a0fe742ddabf
Chuck Butkus <chuck@all-hands.dev>
2025-09-09 23:04:35 -0400

    more logging

--- openhands/runtime/utils/bash.py
+++ openhands/runtime/utils/bash.py
@@ -239,7 +239,9 @@ class BashSession:
         self.pane = self.window.active_pane
         self.pane.send_keys(f'cd {self.work_dir}', enter=True)
         time.sleep(0.1)  # Wait for command to take effect
-        recent_output = self.pane.capture_pane(start=-20, end=-1, join=True)
+        recent_output = recent_output = '\n'.join(
+            self.pane.capture_pane(start=-20, end=-1)
+        )
         logger.info(f'cd output: {recent_output}')
         cwd = self.pane.get('pane_current_path')
         logger.info(f'cwd0: {cwd}')
