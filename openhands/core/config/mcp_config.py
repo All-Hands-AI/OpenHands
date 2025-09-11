15f0469c69abbe709304fe707c548404799e2178
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-09-10 17:05:26 -0700

    Create mcp_config.py

---
+++ openhands/core/config/mcp_config.py
@@ -0,0 +1,11 @@
+import os
+from openhands.utils.import_utils import get_impl
+from openhands_configuration import OpenHandsMCPConfig
+
+
+openhands_mcp_config_cls = os.environ.get(
+    'OPENHANDS_MCP_CONFIG_CLS',
+    'opehands_configuration.OpenHandsMCPConfig',
+)
+
+OpenHandsMCPConfigImpl = get_impl(OpenHandsMCPConfig, openhands_mcp_config_cls)
