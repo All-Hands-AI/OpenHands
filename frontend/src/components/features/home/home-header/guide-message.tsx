d5cd886f234ec294a38bc9bbaad83b707c2ae4f4
hieptl <hieptl.developer@gmail.com>
2025-09-11 21:48:56 +0700

    refactor: guide-message

--- frontend/src/components/features/home/home-header/guide-message.tsx
+++ frontend/src/components/features/home/home-header/guide-message.tsx
@@ -4,19 +4,15 @@ export function GuideMessage() {
   const { t } = useTranslation();
 
   return (
-    <div className="w-full flex items-center justify-center">
-      <div className="w-fit flex items-center justify-center gap-1 px-[15px] rounded-[12px] bg-[#454545] leading-5 text-white text-[15px] font-normal h-[38px] m-1">
-        <div>
-          <span className="">{t("HOME$GUIDE_MESSAGE_TITLE")} </span>
-          <a
-            href="https://docs.all-hands.dev/usage/getting-started"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span className="underline">{t("COMMON$CLICK_HERE")}</span>
-          </a>
-        </div>
-      </div>
+    <div className="w-fit flex flex-col md:flex-row items-start md:items-center justify-center gap-1 rounded-[12px] bg-[#454545] leading-5 text-white text-[15px] font-normal m-1 md:h-9.5 px-4 pb-1 md:px-[15px] md:py-0">
+      <span className="">{t("HOME$GUIDE_MESSAGE_TITLE")} </span>
+      <a
+        href="https://docs.all-hands.dev/usage/getting-started"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="underline">{t("COMMON$CLICK_HERE")}</span>
+      </a>
     </div>
   );
 }
