6802011adbb6fdb9e81dc6a541e6fba0bec589ac
hieptl <hieptl.developer@gmail.com>
2025-09-22 18:53:06 +0700

    refactor: remove file-state-slice.ts file

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,7 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import agentReducer from "./state/agent-slice";
 import browserReducer from "./state/browser-slice";
-import fileStateReducer from "./state/file-state-slice";
 import { jupyterReducer } from "./state/jupyter-slice";
 import securityAnalyzerReducer from "./state/security-analyzer-slice";
 import microagentManagementReducer from "./state/microagent-management-slice";
@@ -9,7 +8,6 @@ import conversationReducer from "./state/conversation-slice";
 import eventMessageReducer from "./state/event-message-slice";
 
 export const rootReducer = combineReducers({
-  fileState: fileStateReducer,
   browser: browserReducer,
   agent: agentReducer,
   jupyter: jupyterReducer,
