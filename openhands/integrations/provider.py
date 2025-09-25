2ac557c86190acb552083736a3b5c98d0d3814b4
johba <admin@noreply.localhost>
2025-09-25 12:33:30 +0200

    Add Forgejo provider wiring and resolver handlers

--- openhands/integrations/provider.py
+++ openhands/integrations/provider.py
@@ -19,6 +19,7 @@ from openhands.events.action.commands import CmdRunAction
 from openhands.events.stream import EventStream
 from openhands.integrations.bitbucket.bitbucket_service import BitBucketServiceImpl
 from openhands.integrations.github.github_service import GithubServiceImpl
+from openhands.integrations.forgejo.forgejo_service import ForgejoServiceImpl
 from openhands.integrations.gitlab.gitlab_service import GitLabServiceImpl
 from openhands.integrations.service_types import (
     AuthenticationError,
@@ -108,6 +109,7 @@ class ProviderHandler:
         ProviderType.GITHUB: 'github.com',
         ProviderType.GITLAB: 'gitlab.com',
         ProviderType.BITBUCKET: 'bitbucket.org',
+        ProviderType.FORGEJO: 'codeberg.org',
     }
 
     def __init__(
@@ -128,6 +130,7 @@ class ProviderHandler:
             ProviderType.GITHUB: GithubServiceImpl,
             ProviderType.GITLAB: GitLabServiceImpl,
             ProviderType.BITBUCKET: BitBucketServiceImpl,
+            ProviderType.FORGEJO: ForgejoServiceImpl,
         }
 
         self.external_auth_id = external_auth_id
