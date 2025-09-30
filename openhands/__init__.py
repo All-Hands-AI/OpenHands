aae0681fabae56b4dc52c7229d2bb52c7a04264b
Tim O'Farrell <tofarr@gmail.com>
2025-09-30 08:51:42 -0600

    added docs

--- openhands/__init__.py
+++ openhands/__init__.py
@@ -1,4 +1,6 @@
 # This is a namespace package - extend the path to include installed packages
+# (We need to do this to support dependencies openhands-sdk, openhands-tools and openhands-agent-server
+# which all have a top level `openhands`` package.)
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 # Import version information for backward compatibility
