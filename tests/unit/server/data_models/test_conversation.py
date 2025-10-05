d1c000d23a474a772fd2f31018603cf4992fbfb2
Tim O'Farrell <tofarr@gmail.com>
2025-10-05 08:09:25 -0600

    Lint fixes

--- tests/unit/server/data_models/test_conversation.py
+++ tests/unit/server/data_models/test_conversation.py
@@ -158,7 +158,9 @@ async def test_search_conversations():
                     )
 
                     mock_app_conversation_service = AsyncMock()
-                    mock_app_conversation_service.search_app_conversations.return_value=AppConversationPage(items=[])
+                    mock_app_conversation_service.search_app_conversations.return_value=AppConversationPage(
+                        items=[]
+                    )
 
                     result_set = await search_conversations(
                         page_id=None,
@@ -166,7 +168,7 @@ async def test_search_conversations():
                         selected_repository=None,
                         conversation_trigger=None,
                         conversation_store=mock_store,
-                        app_conversation_service = mock_app_conversation_service
+                        app_conversation_service = mock_app_conversation_service,
                     )
 
                     expected = ConversationInfoResultSet(
