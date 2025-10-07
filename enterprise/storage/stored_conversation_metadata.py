df465b03fce47925a3e60a84919fe1d864d1d728
Engel Nyst <enyst@users.noreply.github.com>
2025-10-07 10:26:07 +0200

    Update enterprise/storage/stored_conversation_metadata.py

--- enterprise/storage/stored_conversation_metadata.py
+++ enterprise/storage/stored_conversation_metadata.py
@@ -1,4 +1,4 @@
 from openhands.app_server.app_conversation.sql_app_conversation_info_service import (
-    StoredConversationMetadata as OSSStoredConversationMetadata,
+    StoredConversationMetadata,
 )
 
