41d1ce33bea792a0dea32632a33612978e69b533
Tim O'Farrell <tofarr@gmail.com>
2025-10-07 11:34:26 -0600

    Prevent breaking changes

--- enterprise/storage/stored_conversation_metadata.py
+++ enterprise/storage/stored_conversation_metadata.py
@@ -1,10 +1,1 @@
-import uuid
-from datetime import UTC, datetime
-
-from sqlalchemy import JSON, Column, DateTime, Float, Integer, String
-from storage.base import Base
-
-from openhands.app_server.app_conversation.sql_app_conversation_info_service import StoredAppConversationInfo
-
-
-StoredConversationMetadata = StoredAppConversationInfo
+from openhands.app_server.app_conversation.sql_app_conversation_info_service import StoredConversationMetadata
