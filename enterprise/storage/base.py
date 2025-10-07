a6a1229573ecd2adff9d6be406cff5b2032084a8
Tim O'Farrell <tofarr@gmail.com>
2025-10-07 15:44:26 -0600

    Enterprise test fixes

--- enterprise/storage/base.py
+++ enterprise/storage/base.py
@@ -1,7 +1,6 @@
 """
 Unified SQLAlchemy declarative base for all models.
 """
+from openhands.app_server.utils.sql_utils import Base
 
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
+__all__ = ['Base']
