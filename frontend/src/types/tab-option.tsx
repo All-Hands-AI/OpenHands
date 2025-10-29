fab48fe864a1b0504dd71d3fdca50e7bafa92103
sp.wack <83104063+amanape@users.noreply.github.com>
2025-10-29 21:57:48 +0400

    chore(frontend): Remove Jupyter tab and features (#11563)

--- frontend/src/types/tab-option.tsx
+++ frontend/src/types/tab-option.tsx
@@ -1,21 +1,10 @@
 enum TabOption {
   PLANNER = "planner",
   BROWSER = "browser",
-  JUPYTER = "jupyter",
   VSCODE = "vscode",
 }
 
-type TabType =
-  | TabOption.PLANNER
-  | TabOption.BROWSER
-  | TabOption.JUPYTER
-  | TabOption.VSCODE;
-
-const AllTabs = [
-  TabOption.VSCODE,
-  TabOption.BROWSER,
-  TabOption.PLANNER,
-  TabOption.JUPYTER,
-];
+type TabType = TabOption.PLANNER | TabOption.BROWSER | TabOption.VSCODE;
+const AllTabs = [TabOption.VSCODE, TabOption.BROWSER, TabOption.PLANNER];
 
 export { AllTabs, TabOption, type TabType };
