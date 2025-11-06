305922a298dc0fa075de8821c317bf605b7d354f
Tim O'Farrell <tofarr@gmail.com>
2025-11-06 09:12:39 -0700

    Lint fixes

--- enterprise/migrations/versions/080_add_status_and_updated_at_to_callback.py
+++ enterprise/migrations/versions/080_add_status_and_updated_at_to_callback.py
@@ -6,11 +6,10 @@ Create Date: 2025-11-05 00:00:00.000000
 
 """
 
-from enum import Enum
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '080'
@@ -21,11 +20,21 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.add_column('event_callback', sa.Column('status', sa.String, nullable=False, server_default='ACTIVE'))
-    op.add_column('event_callback', sa.Column('updated_at', sa.DateTime, nullable=False, server_default=sa.func.now()))
-    op.drop_index("ix_event_callback_result_event_id")
+    op.add_column(
+        'event_callback',
+        sa.Column('status', sa.String, nullable=False, server_default='ACTIVE'),
+    )
+    op.add_column(
+        'event_callback',
+        sa.Column(
+            'updated_at', sa.DateTime, nullable=False, server_default=sa.func.now()
+        ),
+    )
+    op.drop_index('ix_event_callback_result_event_id')
     op.drop_column('event_callback_result', 'event_id')
-    op.add_column('event_callback_result', sa.Column('event_id', sa.String, nullable=True))
+    op.add_column(
+        'event_callback_result', sa.Column('event_id', sa.String, nullable=True)
+    )
     op.create_index(
         op.f('ix_event_callback_result_event_id'),
         'event_callback_result',
@@ -38,9 +47,11 @@ def downgrade() -> None:
     """Downgrade schema."""
     op.drop_column('event_callback', 'status')
     op.drop_column('event_callback', 'updated_at')
-    op.drop_index("ix_event_callback_result_event_id")
+    op.drop_index('ix_event_callback_result_event_id')
     op.drop_column('event_callback_result', 'event_id')
-    op.add_column('event_callback_result', sa.Column('event_id', sa.UUID, nullable=True))
+    op.add_column(
+        'event_callback_result', sa.Column('event_id', sa.UUID, nullable=True)
+    )
     op.create_index(
         op.f('ix_event_callback_result_event_id'),
         'event_callback_result',
