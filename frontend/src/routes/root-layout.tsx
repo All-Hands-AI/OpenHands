3a12900bea917c5cf78a4c105460c087f19d3d73
vladimirovmm <vladimirov.m.m@mail.ru>
2025-11-13 11:11:08 +0700

    feat: connect button

--- frontend/src/routes/root-layout.tsx
+++ frontend/src/routes/root-layout.tsx
@@ -29,6 +29,8 @@ import { LOCAL_STORAGE_KEYS } from "#/utils/local-storage";
 import { EmailVerificationGuard } from "#/components/features/guards/email-verification-guard";
 import { MaintenanceBanner } from "#/components/features/maintenance/maintenance-banner";
 import { cn, isMobileDevice } from "#/utils/utils";
+import { WalletButton } from "#/components/wallet/wallet";
+import { useAuthWallet } from "#/hooks/use-auth";
 
 export function ErrorBoundary() {
   const error = useRouteError();
@@ -210,6 +212,7 @@ export default function MainApp() {
         isMobileDevice() && "overflow-hidden",
       )}
     >
+      <WalletButton />
       <Sidebar />
 
       <div className="flex flex-col w-full h-[calc(100%-50px)] md:h-full gap-3">
