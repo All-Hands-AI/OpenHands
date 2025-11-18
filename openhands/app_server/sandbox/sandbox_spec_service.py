efa664300f6fcd9fae2959d37b248eecda534293
Tim O'Farrell <tofarr@gmail.com>
2025-11-18 12:44:39 +0000

    Removed architecture

--- openhands/app_server/sandbox/sandbox_spec_service.py
+++ openhands/app_server/sandbox/sandbox_spec_service.py
@@ -11,7 +11,7 @@ from openhands.sdk.utils.models import DiscriminatedUnionMixin
 
 # The version of the agent server to use for deployments.
 # Typically this will be the same as the values from the pyproject.toml
-AGENT_SERVER_IMAGE = 'ghcr.io/openhands/agent-server:88cc031-python-arm64'
+AGENT_SERVER_IMAGE = 'ghcr.io/openhands/agent-server:88cc031-python'
 
 
 class SandboxSpecService(ABC):
