a767b9f5ee07c7d850a2c1aeb44f72a0f6ebe4a2
Tim O'Farrell <tofarr@gmail.com>
2025-11-21 08:45:31 +0000

    Bump SDK

--- openhands/app_server/sandbox/sandbox_spec_service.py
+++ openhands/app_server/sandbox/sandbox_spec_service.py
@@ -11,7 +11,7 @@ from openhands.sdk.utils.models import DiscriminatedUnionMixin
 
 # The version of the agent server to use for deployments.
 # Typically this will be the same as the values from the pyproject.toml
-AGENT_SERVER_IMAGE = 'ghcr.io/openhands/agent-server:15f565b-python'
+AGENT_SERVER_IMAGE = 'ghcr.io/openhands/agent-server:ed9f804-python'
 
 
 class SandboxSpecService(ABC):
