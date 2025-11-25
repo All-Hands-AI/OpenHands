e98dbdb43f923d2b847acee62e8ee5ac32894c6f
rohitvinodmalhotra@gmail.com <rohitvinodmalhotra@gmail.com>
2025-11-24 19:10:30 -0500

    update agent server

--- openhands/app_server/sandbox/sandbox_spec_service.py
+++ openhands/app_server/sandbox/sandbox_spec_service.py
@@ -11,7 +11,7 @@ from openhands.sdk.utils.models import DiscriminatedUnionMixin
 
 # The version of the agent server to use for deployments.
 # Typically this will be the same as the values from the pyproject.toml
-AGENT_SERVER_IMAGE = 'ghcr.io/openhands/agent-server:f8bdd81-python'
+AGENT_SERVER_IMAGE = 'ghcr.io/openhands/agent-server:cc09302-python'
 
 
 class SandboxSpecService(ABC):
